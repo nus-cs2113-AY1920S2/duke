6931033813b76777819c9a9eb10ea06ab0b1d4b0
alwayshuizhen <alwayshuizhen@gmail.com>
2020-01-15 22:55:50 +0800

    Level-1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+                + "|____/ \\__,_|_|\\_\\___| ";
+        System.out.println(logo + " says hello :)");
+        System.out.println("\nWhat can i do for you?");
+        Scanner myObj = new Scanner(System.in);
+        String cmd;
+        do {
+            cmd = myObj.nextLine();
+            if (!cmd.equals("bye")){
+                System.out.println(cmd);
+            } else {
+                System.out.println("Bye bye! Talk to me again soon!");
+            }
+        } while (!cmd.equals("bye"));
     }
 }
