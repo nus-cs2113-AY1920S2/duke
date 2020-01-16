8a08adab8424b8394640d615614df70809cdbee9
RenzoTsai <cairunze17@mails.ucas.ac.cn>
2020-01-16 14:00:12 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,50 @@
+import java.awt.desktop.SystemSleepEvent;
+
 public class Duke {
+    public static void greet(){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("\tHello! I'm Renzo");
+        System.out.println("\tWhat can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void list(){
+        System.out.println("\n" +
+                "    ____________________________________________________________");
+        System.out.println("\tlist");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void bye(){
+        System.out.println("\n" +
+                "    ____________________________________________________________");
+        System.out.println("\tBye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void echo(String command){
+        System.out.println("\n" +
+                "    ____________________________________________________________");
+        System.out.println("\t"+command);
+        System.out.println("    ____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _____    _____   __   _   ______  _____  \n"
+                +"|  _  \\  | ____| |  \\ | | |___  / /  _  \\ \n"
+                +"| |_| |  | |__   |   \\| |    / /  | | | | \n"
+                +"|  _  /  |  __|  | |\\   |   / /   | | | | \n"
+                +"| | \\ \\  | |___  | | \\  |  / /__  | |_| | \n"
+                +"|_|  \\_\\ |_____| |_|  \\_| /_____| \\_____/ \n";
+        System.out.println(logo);
+        greet();
+        String[] commands = {"list","blah","blah","bye"};
+        int i = 0;
+        String command = commands[i++];
+        while (!command.equals("bye")){
+            echo(command);
+            command = commands[i++];
+        }
+        bye();
     }
 }
