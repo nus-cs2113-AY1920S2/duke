8b514ac0159d8eaece6ebbeb71a809a49147eeb7
kokjoon97 <e0309136@u.nus.edu>
2020-01-16 17:27:21 +0800

    Import Scanner, add greet, echo and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "Kok Joon";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm KJ\n" + "How can I help you today?");
+        String cmd;
+        boolean isAsking = true;
+        Scanner input = new Scanner(System.in);
+        do {
+            cmd = input.nextLine();
+            if (cmd.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                isAsking = false;
+            } else {
+                System.out.println(cmd);
+            }
+        } while (isAsking);
     }
 }
