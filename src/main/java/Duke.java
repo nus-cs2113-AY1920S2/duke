2e8cfa84fca56a86f0b2128ab0387aff40a9447d
DDzuikeai <yanchenghao17@gmail.com>
2020-01-17 01:47:38 +0800

    week1,version1

---
+++ src/main/java/Duke.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("________________________________");
+        System.out.println("Hello,I'm Duke.");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        Scanner in = new Scanner(System.in);
+        String input = in.next();
+        while(!(input.equals("Bye"))){
+            System.out.println("________________________________");
+            System.out.println(input);
+            System.out.println("________________________________");
+            input = in.next();
+        }
+        System.out.println("________________________________");
+        System.out.println("Bye.Hope to see you again soon!");
+        System.out.println("________________________________");
+    }
+}
