59f52edc9ea56f18fe898f57a8a97fbee86b9698
andy-aw-why <andd9912529@gmail.com>
2020-01-17 01:53:05 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner myInput = new Scanner(System.in);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String userInput = myInput.nextLine();
+        while (!userInput.equals("bye")) {
+            System.out.println(userInput);
+            userInput = myInput.nextLine();
+        }
+
+
+        System.out.println("Bye. Hope to see you again soon!");
+        myInput.close();
     }
 }
