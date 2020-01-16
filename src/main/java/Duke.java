04919985b90781d8569be451cda0075e4cc5d72b
quinnyyy <quinnyyy@bu.edu>
2020-01-16 20:47:54 +0800

    Add greet, echo, and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static void printMessagePretty(String message) {
+        String lineSeparator = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+
+        System.out.println("\n" + lineSeparator);
+        System.out.println(message);
+        System.out.println(lineSeparator + "\n");
+    }
+
+    private static void readAndRepeatUserInput() {
+        Scanner scannerObject = new Scanner(System.in);
+        final String exitKeyword = "bye";
+        String userInput = "";
+
+        while (true) {
+            userInput = scannerObject.nextLine();
+            if (userInput.equals(exitKeyword)) {
+                break;
+            }
+            printMessagePretty("You typed: " + userInput);
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String greeting = "Hello! This is Quinn's chat bot.\nType something and I will repeat it\n" +
+                "Say \"bye\" to quit";
+        printMessagePretty(greeting);
+
+        readAndRepeatUserInput();
+
+        final String byeMessage = "Leaving so soon? :(";
+        printMessagePretty(byeMessage);
+
     }
 }
