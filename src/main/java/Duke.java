c2d67093895861ec9d96778bbc15ba945f913695
katelo731 <xe870731@gmail.com>
2020-01-17 01:08:09 +0800

    add greet function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+    public static void greet(String string){
+        System.out.println("____________________________________________________________");
+        System.out.println(string);
+        System.out.println("Hello! I'm your chatbot - Huei.");
+        System.out.println("What can I do for you? Type 'bye' to exit.");
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  _   _    _   _ U _____ u             \n"
+                    + " |'| |'|U |\"|u| |\\| ___\"|/    ___      \n"
+                    + "/| |_| |\\\\| |\\| | |  _|\"     |_\"_|     \n"
+                    + "U|  _  |u | |_| | | |___      | |      \n"
+                    + " |_| |_| <<\\___/  |_____|   U/| |\\u    \n"
+                    + " //   \\\\(__) )(   <<   >>.-,_|___|_,-. \n"
+                    + "(_\") (\"_)   (__) (__) (__)\\_)-' '-(_/  \n";
+        greet(logo);
+
     }
 }
