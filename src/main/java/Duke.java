6f54b9ae2580b7776c84f3606235ed39ea1e0ba7
Jeremy Cheung <z5142254@ad.unsw.edu.au>
2020-01-16 18:00:52 +1100

    upload chat method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greeting();
+        chat();
+    }
+
+    public static void greeting() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you");
+        System.out.println("____________________________________________________________");
+
+    }
+
+    public static void  chat() throws IOException {
+        System.out.println("____________________________________________________________");
+        String str = readCommand();
+        System.out.println("____________________________________________________________");
+
+        if(str.equals("bye")){
+            System.out.println("____________________________________________________________");
+            System.out.println("bye");
+            System.out.println("____________________________________________________________");
+        }else {
+            System.out.println(str);
+            System.out.println("____________________________________________________________");
+            chat();
+
+        }
+    }
+
+    private static String readCommand() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        System.out.println("Type something");
+        str = br.readLine();
+        return str;
     }
 }
+
+
+
+
