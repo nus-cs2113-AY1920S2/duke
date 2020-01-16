b9b10f0d7849eb589e2da9369f8766c397ed04be
RenzoTsai <cairunze17@mails.ucas.ac.cn>
2020-01-16 14:22:25 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,32 +1,33 @@
 import java.awt.desktop.SystemSleepEvent;
+import java.util.Scanner;
 
 public class Duke {
     public static void greet(){
         System.out.println("    ____________________________________________________________");
         System.out.println("\tHello! I'm Renzo");
-        System.out.println("\tWhat can I do for you?");
+        System.out.println("\tWhat can I do for you? Please enter your command:");
         System.out.println("    ____________________________________________________________");
+        System.out.println("\nPlease enter your command or enter \"bye\" to exit:");
     }
 
     public static void list(){
-        System.out.println("\n" +
-                "    ____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
         System.out.println("\tlist");
         System.out.println("    ____________________________________________________________");
+        System.out.println("\nPlease enter your command or enter \"bye\" to exit:");
     }
 
     public static void bye(){
-        System.out.println("\n" +
-                "    ____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
         System.out.println("\tBye. Hope to see you again soon!");
         System.out.println("    ____________________________________________________________");
     }
 
     public static void echo(String command){
-        System.out.println("\n" +
-                "    ____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
         System.out.println("\t"+command);
         System.out.println("    ____________________________________________________________");
+        System.out.println("\nPlease enter your command or enter \"bye\" to exit:");
     }
 
     public static void main(String[] args) {
@@ -38,13 +39,16 @@ public class Duke {
                 +"|_|  \\_\\ |_____| |_|  \\_| /_____| \\_____/ \n";
         System.out.println(logo);
         greet();
-        String[] commands = {"list","blah","blah","bye"};
-        int i = 0;
-        String command = commands[i++];
+
+        /* Enable to get command from Command Line */
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+
         while (!command.equals("bye")){
             echo(command);
-            command = commands[i++];
+            command = scanner.nextLine();
         }
+
         bye();
     }
 }
