d0eed7cff08fe4aa3e9748c7720fe00f3915797e
Terry Tay <59829631+terrytay@users.noreply.github.com>
2020-01-17 12:49:51 +0800

    Change logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "######  #     # ######  #     #\n"
+                + "#     # #     # #     # #     #\n"
+                + "#     # #     # #     # #     #\n"
+                + "######  #     # ######  #     #\n"
+                + "#       #     # #       #     #\n"
+                + "#       #     # #       #     #\n"
+                + "#        #####  #        #####\n";
         System.out.println("Hello from\n" + logo);
     }
 }
