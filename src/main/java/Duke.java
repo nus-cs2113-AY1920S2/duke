a98cf9996793e9198867aa0e1e87c6ca864dea9b
wang qin <347656409nick@gmail.com>
2020-01-17 22:18:33 +0800

    v1.0 add echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,51 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+    private static void greeting () {
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
     }
+
+    private static void farewell() {
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void echo(String command_word) {
+
+        System.out.println("____________________________________________________________");
+        System.out.println("  "+command_word);
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void runCommandLoopUntilExitCommand() {
+
+        final String exit_command = "bye";
+        Scanner sc = new Scanner(System.in);
+        String command_word = sc.nextLine();
+
+        while (!command_word.equals(exit_command)) {
+            echo(command_word);
+            command_word = sc.nextLine();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        greeting();
+        runCommandLoopUntilExitCommand();
+        farewell();
+    }
+
+
+
 }
+
+
