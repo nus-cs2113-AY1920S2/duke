57d74b088465a0124e7866b1694f7d8d008941cd
kcubey <keziakew98@gmail.com>
2020-01-17 17:30:07 +0800

    add w2 level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean continueRun = true;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        while (continueRun == true){
+            System.out.println("==========================");
+            System.out.println("What can I do for you?");
+            String userCmd = sc.nextLine();
+
+            System.out.println("You have typed: " +userCmd);
+
+            if (userCmd.toLowerCase().equals("bye")){
+                System.out.println("Bye. Hope to see you again!");
+                continueRun = false;
+            }
+        }
+
+
+
     }
 }
