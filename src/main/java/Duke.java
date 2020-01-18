f798950a14a97d955d462ddf87812003710928ee
Keith-JK <keithcjk123@gmail.com>
2020-01-18 23:54:20 +0800

    Week 2 Level 1 increment - Greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        // Create Scanner object
+        Scanner myScanner = new Scanner(System.in);
+
+        // Create Hello message
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String userInput = "";
+        String NEW_LINE = "____________________________________________________________";
+
+        // Get user input in a loop
+        while(!userInput.equals("bye")){
+            System.out.print("USER:");
+            userInput = myScanner.nextLine();
+            System.out.println(NEW_LINE);
+            System.out.println("DUKE:" + userInput);
+            System.out.println(NEW_LINE);
+        }
+
+        System.out.println("Exiting DUKE\n" + logo);
     }
 }
