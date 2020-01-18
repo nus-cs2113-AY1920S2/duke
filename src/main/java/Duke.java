7fd52c5cc9cb1128e4e75332ec5bcdd3e1811df0
A11riseforme <ycl0217@hotmail.com>
2020-01-18 18:42:14 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,28 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String indentation = "     ";
+        String line = "    ____________________________________________________________";
+        String greetings = "     Hello! I'm Duke\n" +
+                "     What can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+
+        //System.out.println("Hello from\n" + logo);
+
+        System.out.println(line);
+        System.out.println(greetings);
+        System.out.println(line);
+
+        Scanner s  = new Scanner(System.in);
+        String command = s.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(line);
+            System.out.println(indentation + command);
+            System.out.println(line);
+            command = s.nextLine();
+        }
+        System.out.println(line);
+        System.out.println(indentation + bye);
+        System.out.println(line);
     }
 }
