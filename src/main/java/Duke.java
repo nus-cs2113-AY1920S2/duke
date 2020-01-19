0a6624d9e55b41faa4a3453ed078f4e0666afb96
lwxymere <chaoscold@hotmail.com>
2020-01-19 10:04:43 +0800

    Update duke welcome message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("_________________________________________________");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("This is\n" + logo);
+        System.out.println("How can I help you today?");
+        System.out.println("_________________________________________________");
     }
 }
