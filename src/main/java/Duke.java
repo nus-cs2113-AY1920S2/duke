14fe0a3555e4f01b24998f206fb2c355b636156f
ramdm <ramdmtech@gmail.com>
2020-01-19 10:37:16 +0800

    Change logo, add initial methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;  // Import the Scanner class
+
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+
+    public static void intro()
+    {
+        // Logo generated using http://patorjk.com/software/taag/#p=display&f=Fire%20Font-s&t=NUSBOT
+        String logo = "    )       (           )          \n"
+                + " ( /(       )\\ )  (  ( /(   *   )  \n"
+                + " )\\())   ( (()/(( )\\ )\\())` )  /(  \n"
+                + "((_)\\    )\\ /(_))((_|(_)\\  ( )(_)) \n"
+                + " _((_)_ ((_|_))((_)_  ((_)(_(_())  \n"
+                + "| \\| | | | / __|| _ )/ _ \\|_   _|  \n"
+                + "| .` | |_| \\__ \\| _ \\ (_) | | |    \n"
+                + "|_|\\_|\\___/|___/|___/\\___/  |_|    \n"
+                + "                                   \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Type 'bye' to leave at any time.");
+    }
+
+    public static void echoInput(String input) {
+        System.out.println(input);
+    }
+
+
+    public static void main(String[] args) {
+        intro();
+
+        // Prepre for first input
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("What can I do for you?");
+        String userInput = inputScanner.nextLine();
+
+        while(args[0] != "bye" || args[0] != "Bye") {
+            echoInput(userInput);
+
+        }
     }
 }
