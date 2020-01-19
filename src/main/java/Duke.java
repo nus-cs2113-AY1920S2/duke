08298dc04e3834b12bdb59c796a728484c2eb444
sliu107 <dldy0825@gmail.com>
2020-01-19 23:20:29 +0800

    initial skeleton can simply echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner read=new Scanner(System.in);
+        System.out.println("Hello! I'm Momo");
+        System.out.println("What can I do for you?");
+        System.out.println("\n");
+        String command=read.nextLine();
+        while(!command.equals("bye")){
+            switch(command) {
+                case "list":
+                    System.out.println("list");
+                    break;
+                case "blah":
+                    System.out.println("blah");
+                    break;
+                default:
+                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+            }
+            command=read.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
