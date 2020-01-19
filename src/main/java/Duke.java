df78db4796de3f784cf724960969d897f9336536
g-lilian <guolilian98@gmail.com>
2020-01-20 07:58:02 +0800

    Add L0: Greet user
    Duke greets the user and exits.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static void br () {
+        System.out.println("...................................................");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +12,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Does the human have a request today?");
+        br();
+
+        System.out.println("Goodbye, see you in the seventh dimension!");
+        br();
     }
 }
