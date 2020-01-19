45c1abd94220879c8dfd5d77490ea44f10b0bbc9
lwxymere <chaoscold@hotmail.com>
2020-01-19 11:17:19 +0800

    Add logic to echo user input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        printWelcomeBanner();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            if (input.toLowerCase().equals("bye")) {
+                printByeMessage();
+                break;
+            } else {
+                // echo user's input
+                System.out.println(input);
+                System.out.println("_________________________________________________");
+            }
+        }
+    }
+
+    // am I supposed to declare helper functions this way?
+    // seems abit dodgy
+    private static void printWelcomeBanner() {
         System.out.println("_________________________________________________");
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
