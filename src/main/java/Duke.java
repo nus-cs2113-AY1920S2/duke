73b9d0bb9947e5707ac6a9df6f83b8dc493dc9c1
Nyan Wun Paing <nwp.nyan@gmail.com>
2020-01-19 14:11:26 +0800

    Add Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "Nyan\n";
+        System.out.println("    ____________________________________________________________");
+        System.out.print("    Hello! I'm " + logo);
+        System.out.println("    ____________________________________________________________");
+
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            String s = input.nextLine();
+            System.out.println("    ____________________________________________________________");
+            if (s.contains("bye")){
+                System.out.println("    Bye. Hope to see you soon!");
+                System.out.println("    ____________________________________________________________");
+                break;
+            } else {
+                System.out.println("    " + s);
+            }
+            System.out.println("    ____________________________________________________________");
+        }
     }
 }
+
