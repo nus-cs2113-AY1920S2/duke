a38d1c4d8c7de653073ea14d006c787e1d22c66e
jichngan <jcngan97@gmail.com>
2020-01-19 11:16:07 +0800

    Add week 1 functionalities. Change name to Bob.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,36 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = ".______     ______   .______   \n"
+                + "|   _  \\   /  __  \\  |   _  \\  \n"
+                + "|  |_)  | |  |  |  | |  |_)  | \n"
+                + "|   _  <  |  |  |  | |   _  <  \n"
+                + "|  |_)  | |  `--'  | |  |_)  | \n"
+                + "|______/   \\______/  |______/  \n";
+        String divider = "-----------------------------------------------";
+        String welcomeMessage = "Hello human! I am Bob.\n" + "What can I do for you?";
+
+        System.out.println("Bob the chatbot\n" + logo);
+        System.out.println(welcomeMessage);
+        System.out.println(divider);
+
+        Scanner command = new Scanner(System.in);
+        String endCommand = "bye";
+        String endMessage = "Bob thanks you for coming! See you again soon!";
+
+        while (true) {
+            String userInput = command.nextLine();
+            if (userInput.equals(endCommand)) {
+                System.out.println(divider);
+                System.out.println(endMessage);
+                System.out.println(divider);
+                break;
+            } else {
+                System.out.println(divider);
+                System.out.println(userInput);
+                System.out.println(divider);
+            }
+        }
     }
 }
