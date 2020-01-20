4806bf8bd13c49dd4efa46c1a5a06d2b9c8bc253
GanapathySanathBalaji <e0311091@u.nus.edu>
2020-01-20 20:09:29 +0800

    Add indentation

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("____________________________________________________________"+"\n"+
-               " Hello! I'm Duke\n" +
-               " What can I do for you?\n" +
-               "____________________________________________________________");
-       String command;
-       do{
-           command = sc.next();
-           System.out.println("____________________________________________________________");
-           if(command.equals("bye")){
-               System.out.println(" Bye. Hope to see you again soon!");
-           }
-           else
-           {
-               System.out.println(" "+command);
-           }
-           System.out.println("____________________________________________________________");
-       }while(!command.equals("bye"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("    ____________________________________________________________"+"\n"+
+                "     Hello! I'm Duke\n" +
+                "     What can I do for you?\n" +
+                "    ____________________________________________________________\n");
+        String command;
+        do{
+            command = sc.next();
+            System.out.println("    ____________________________________________________________");
+            if(command.equals("bye")){
+                System.out.println("     Bye. Hope to see you again soon!");
+            }
+            else
+            {
+                System.out.println("     "+command);
+            }
+            System.out.println("    ____________________________________________________________\n");
+        }while(!command.equals("bye"));
     }
 }
