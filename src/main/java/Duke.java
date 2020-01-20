cdb11fbe6973e190e5cde57e3145e44c2724da82
g-lilian <guolilian98@gmail.com>
2020-01-20 09:04:26 +0800

    Add L2: Add, List
    Dook stores text entered by user and displays them when "list" command is entered,

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,37 +2,55 @@ import java.util.Scanner;
 
 public class Duke {
     public static void br () {
-        System.out.println("...................................................");
+        System.out.println("    ...................................................");
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        br();
         String logo =
-                "┌┬┐┌─┐┌─┐┬┌─\n" +
-                " │││ ││ │├┴┐\n" +
-                "─┴┘└─┘└─┘┴ ┴";
-        System.out.println("Hello! I am \n" + logo);
-        System.out.println("Does the human have a request today?");
+                "        ┌┬┐┌─┐┌─┐┬┌─\n" +
+                "         │││ ││ │├┴┐\n" +
+                "        ─┴┘└─┘└─┘┴ ┴";
+        System.out.println("     Hello! I am \n" + logo);
+        System.out.println("     Does the human have a request today?");
         br();
 
         String command = " ";
+        String[] list = new String[100];
+        int num_items = 0;
         while (true) {
-            command = input.next();
+            command = input.nextLine();
             if (command.equals("bye")) break;
-            System.out.println(command); br();
+
+            switch(command) {
+                case "list":
+                    br();
+                    for (int i=0; i<num_items; i++) {
+                        int num = i+1;
+                        System.out.println("     " + num + ". " + list[i]);
+                    }
+                    br();
+                    break;
+                default:
+                    list[num_items] = command; num_items++;
+                    br(); System.out.println("     added: " + command); br();
+                    break;
+            }
         }
 
-        System.out.println("Goodbye, see you in the seventh dimension!");
-        System.out.println("                 *       +\n" +
-                            "           '                  |\n" +
-                            "       ()    .-.,=\"``\"=.    - o -\n" +
-                            "             '=/_       \\     |\n" +
-                            "          *   |  '=._    |\n" +
-                            "               \\     `=./`,        '\n" +
-                            "            .   '=.__.=' `='      *\n" +
-                            "   +                         +\n" +
-                            "        O      *        '       .");
+        br();
+        System.out.println("     Goodbye, see you in the seventh dimension!");
+        System.out.println("                   *       +\n" +
+                            "             '                  |\n" +
+                            "         ()    .-.,=\"``\"=.    - o -\n" +
+                            "               '=/_       \\     |\n" +
+                            "            *   |  '=._    |\n" +
+                            "                 \\     `=./`,        '\n" +
+                            "              .   '=.__.=' `='      *\n" +
+                            "     +                         +\n" +
+                            "          O      *        '       .");
         br();
     }
 }
