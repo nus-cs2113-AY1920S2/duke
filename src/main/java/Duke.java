71524bbbb4d4e694dc5e7c7e4fc5e9395556aeac
JeremiasLiew <jeremias.liew@hotmail.com>
2020-01-20 15:19:07 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String name = "***Rick***";
+    	Scanner sc = new Scanner(System.in);
+        String name = "**Rick**";
         System.out.println("____________________________________________________________");
         System.out.println("  Hello! I'm " + name);
         System.out.println("  What can I do for you?");
+        System.out.println("____________________________________________________________" + '\n');
+        while (true) {
+        	String str = sc.nextLine();
+        	if (str.equals("bye")) {
+        		break;
+        	}
+        	System.out.println("____________________________________________________________");
+        	System.out.println(str);
+        	System.out.println("____________________________________________________________" + '\n');
+        }
         System.out.println("____________________________________________________________");
         System.out.println("  Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+        System.exit(0);
     }
 }
