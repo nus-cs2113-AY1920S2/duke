083aea03fb26025b82b0c27101fc97c00a34b37a
Nyan Wun Paing <nwp.nyan@gmail.com>
2020-01-21 00:20:18 +0800

    Add Increments ( level-3 )

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+
+        public void taskDone() {
+            this.isDone = true;
+        }
+    }
+
     public static void main(String[] args) {
         String logo = "Nyan\n";
         System.out.println("    ____________________________________________________________");
@@ -8,23 +26,30 @@ public class Duke {
         System.out.println("    ____________________________________________________________");
 
         Scanner input = new Scanner(System.in);
-        String[] buffer = new String[100];
+        Task[] buffer = new Task[100];
         int counter = 0;
 
         while(true) {
-            String s = input.nextLine();
+            String s = input.next();
             System.out.println("    ____________________________________________________________");
-            if (s.equalsIgnoreCase("bye")){
+            if (s.equalsIgnoreCase("bye")) {
                 System.out.println("    Bye. Hope to see you soon!");
                 System.out.println("    ____________________________________________________________");
                 break;
-            } else if(s.equalsIgnoreCase("list")) {
-                for(int i = 0; i < counter; i++) {
-                    System.out.println("    " + (i+1) + ". " + buffer[i]);
+            } else if (s.equalsIgnoreCase("list")) {
+                System.out.println("    Here are the tasks in your list:");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println("    " + (i + 1) + ".[" + buffer[i].getStatusIcon() + "] " + buffer[i].description);
                 }
+            } else if (s.equalsIgnoreCase("done")){
+                int num = input.nextInt() - 1;
+                buffer[num].taskDone();
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("      [" + buffer[num].getStatusIcon() + "] " + buffer[num].description);
             } else {
-                buffer[counter] = s;
-                System.out.println("    added: " + buffer[counter]);
+                s = s + input.nextLine();
+                buffer[counter] = new Task(s);
+                System.out.println("    added: " + buffer[counter].description);
                 counter++;
             }
             System.out.println("    ____________________________________________________________");
