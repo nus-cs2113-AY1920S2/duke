ee5b0237f83994d840868a574741f16255f5ac4a
g-lilian <guolilian98@gmail.com>
2020-01-20 08:22:12 +0800

    Customise personality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,24 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo =
+                "┌┬┐┌─┐┌─┐┬┌─\n" +
+                " │││ ││ │├┴┐\n" +
+                "─┴┘└─┘└─┘┴ ┴";
+        System.out.println("Hello! I am \n" + logo);
         System.out.println("Does the human have a request today?");
         br();
 
         System.out.println("Goodbye, see you in the seventh dimension!");
+        System.out.println("                 *       +\n" +
+                            "           '                  |\n" +
+                            "       ()    .-.,=\"``\"=.    - o -\n" +
+                            "             '=/_       \\     |\n" +
+                            "          *   |  '=._    |\n" +
+                            "               \\     `=./`,        '\n" +
+                            "            .   '=.__.=' `='      *\n" +
+                            "   +                         +\n" +
+                            "        O      *        '       .");
         br();
     }
 }
