5b54e9e45fb618bc02396f69ada1eeb321b991fb
Joel <joelchang97@gmail.com>
2020-01-21 21:22:12 +0800

    Implemented Level 2 Features

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,23 +1,76 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
 
-    public static void greet() {
-        System.out.println("    ===================================================================");
-        System.out.println("    Hi! I am Duke, your next doooooorrrr friendly elf.....I mean bot");
-        System.out.println("    ===================================================================");
+    private static ArrayList<String> tasks;
+
+    public Duke() {
+        this.tasks = new ArrayList<String>();
     }
 
-    public static void bye() {
-        System.out.println("    ===================================================================");
+    private void printGreetingMessage() {
+        System.out.println("Hi! I am Duke, your next doooooorrrr friendly elf.....I mean bot");
+        System.out.println("How may Dukeeeeee be of service to you today?");
+        System.out.println("_______________________________________________________________________");
+    }
+
+    private void printLeavingMessage() {
         System.out.println("    Bye! Duke is now a freeeeee elf again!!!!");
-        System.out.println("    ===================================================================");
     }
 
-    public static void echo(String convertToLowerCase) {
-        System.out.println("    ===================================================================");
+    private void printLine() {
+        System.out.println(" ======================================================================");
+    }
+
+    private void echo(String convertToLowerCase) {
         System.out.println("    " + convertToLowerCase);
-        System.out.println("    ===================================================================");
+    }
+
+    private void add(String description) {
+        tasks.add(description);
+        System.out.println("    Added: " + description);
+        System.out.println("    There are currently " + tasks.size() + " item(s) queued");
+    }
+
+    private void printList() {
+        int position = 1;
+        for (String text: tasks) {
+            System.out.println("    " + position + ". " + text);
+            position ++;
+        }
+    }
+
+    private void list() {
+        if (tasks.size() == 0) {
+            System.out.println("    Hmph!! There are currently no items queued");
+        } else {
+            System.out.println(" Here are the list of tasks:");
+            printList();
+        }
+    }
+
+    private void run() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            String line = sc.nextLine();
+            String removeTrailingSpaces = line.trim();
+            String convertToLowerCase = removeTrailingSpaces.toLowerCase();
+            boolean isBye = convertToLowerCase.equals("bye");
+            boolean isList = convertToLowerCase.equals("list");
+            printLine();
+            if (isBye) {
+                printLeavingMessage();
+                printLine();
+                break;
+            } else if(isList) {
+                list();
+            } else {
+                add(removeTrailingSpaces);
+            }
+            printLine();
+        }
+        sc.close();
     }
 
     public static void main(String[] args) {
@@ -27,19 +80,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        greet();
-        Scanner sc = new Scanner(System.in);
-        while (true) {
-            String line = sc.nextLine();
-            String convertToLowerCase = line.toLowerCase();
-            boolean isBye = convertToLowerCase.equals("bye");
-            if (isBye) {
-                bye();
-                break;
-            } else {
-                echo(line);
-            }
-        }
-        sc.close();
+        Duke dukeFeatures = new Duke();
+        dukeFeatures.printGreetingMessage();
+        dukeFeatures.run();
     }
 }
