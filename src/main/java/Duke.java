f42ae0a7fc944bbfb7670bf073543d3c4c4f9940
JustinnT <e0311076@u.nus.edu>
2020-01-21 20:31:11 +0800

    Added greeting and exit methods

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,14 @@
 public class Duke {
+    public static void greets() {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+    }
+    public static void printLines() {
+        System.out.println("---------------------------------------");
+    }
+    public static void exits() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +16,10 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        printLines();
+        greets();
+        printLines();
+        exits();
+        printLines();
     }
 }
