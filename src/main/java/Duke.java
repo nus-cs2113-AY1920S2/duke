a584e1bb088bbcfdda65db883f9f8e889c634336
andy-aw-why <andd9912529@gmail.com>
2020-01-21 22:50:00 +0800

    Added storing the actions into a list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,11 +12,27 @@ public class Duke {
 
         Scanner myInput = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+        String[] actionList;
+        actionList = new String[100];
+        int numberOfActions = 0;
         String userInput = myInput.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+            System.out.println(System.lineSeparator());
+            if (userInput.equals("list") && numberOfActions > 0) {
+                for (int i = 0; i < numberOfActions; i++) {
+                    System.out.println(Integer.toString(i + 1) + ". " + actionList[i]);
+                }
+            } else if (userInput.equals("list") && numberOfActions == 0) {
+                System.out.println("Nothing yet");
+            } else {
+                actionList[numberOfActions] = userInput;
+                numberOfActions = numberOfActions + 1;
+                System.out.println("added: " + userInput);
+            }
+            System.out.println("What else do you want to do?");
             userInput = myInput.nextLine();
         }
+        System.out.println(System.lineSeparator());
         System.out.println("Bye. Hope to see you again soon! Maybe next time");
         myInput.close();
     }
