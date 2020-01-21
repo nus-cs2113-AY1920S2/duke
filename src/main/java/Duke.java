050359aabeba2876ae23f4e396c0531057f2d590
andy-aw-why <andd9912529@gmail.com>
2020-01-21 23:49:27 +0800

    Added marking of action to be done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class Duke {
 
+public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,23 +12,38 @@ public class Duke {
 
         Scanner myInput = new Scanner(System.in);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
-        String[] actionList;
-        actionList = new String[100];
+        Task[] actionList;
+        actionList = new Task[100];
         int numberOfActions = 0;
         String userInput = myInput.nextLine();
         while (!userInput.equals("bye")) {
             System.out.println(System.lineSeparator());
+            String[] splitUserInput = userInput.split(" ");
             if (userInput.equals("list") && numberOfActions > 0) {
                 for (int i = 0; i < numberOfActions; i++) {
-                    System.out.println(Integer.toString(i + 1) + ". " + actionList[i]);
+                    System.out.println(
+                            Integer.toString(i + 1) + ". [" + actionList[i].getStatusIcon() + "] " +
+                                    actionList[i].description);
                 }
             } else if (userInput.equals("list") && numberOfActions == 0) {
                 System.out.println("Nothing yet");
+            } else if (splitUserInput[0].equals("done")) {
+                int actionListNumber = Integer.parseInt(splitUserInput[1]);
+                if ((actionListNumber - 1) > numberOfActions || actionListNumber == 0) {
+                    System.out.println("Out of range");
+                } else {
+                    actionList[actionListNumber - 1].markAsDone();
+                    System.out.println(
+                            "Nice! I marked this as done: [" + actionList[actionListNumber - 1].getStatusIcon() + "] " +
+                                    actionList[actionListNumber - 1].description);
+                }
             } else {
-                actionList[numberOfActions] = userInput;
+                Task newTask = new Task(userInput);
+                actionList[numberOfActions] = newTask;
                 numberOfActions = numberOfActions + 1;
                 System.out.println("added: " + userInput);
             }
+
             System.out.println("What else do you want to do?");
             userInput = myInput.nextLine();
         }
@@ -36,4 +51,6 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon! Maybe next time");
         myInput.close();
     }
+
+
 }
