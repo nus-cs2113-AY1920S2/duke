34cc161e68271e290b606e2b789b3b5b8a925c6d
JeremiasLiew <jeremias.liew@hotmail.com>
2020-01-22 15:57:26 +0800

    Clean up code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,9 +2,75 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+	static ArrayList<Task> list = new ArrayList<>();
+	
+	public static String makeDone(String line) {
+		String msg = "";
+		// sets a specified task as done
+        int index = Integer.valueOf(line.split(" ")[1]);
+        if (index > list.size()) {
+            msg += "No such task!";
+        } else if (list.get(index - 1).isDone) {
+            msg += "Task is already completed!";
+        } else {
+            list.get(index - 1).markAsDone();
+            msg += "Nice! I've marked this task as done: " + '\n';
+            msg += "    " + list.get(index - 1).toString();
+        }
+        return msg;
+	}
+	
+	public static String addTodo(String line) {
+		String msg = "";
+		Task todo = new ToDo(line.substring(5));
+    	list.add(todo);
+    	msg += "Got it. I've added this task: " + '\n';
+    	msg += "    " + todo.toString() + '\n';
+    	msg += "  Now you have " + list.size() + " task(s) in the list.";
+    	return msg;
+	}
+	
+	public static String addDeadline(String line) {
+		String msg = "";
+		int ind1 = line.indexOf('/');
+    	Task deadline = new Deadline(line.substring(9, ind1 - 1), line.substring(ind1 + 4));
+    	list.add(deadline);
+    	msg += "Got it. I've added this task: " + '\n';
+    	msg += "    " + deadline.toString() + '\n';
+    	msg += "  Now you have " + list.size() + " task(s) in the list.";
+    	return msg;
+	}
+	
+	public static String addEvent(String line) {
+		String msg = "";
+		int ind1 = line.indexOf('/');
+    	Task event = new Event(line.substring(6, ind1 - 1), line.substring(ind1 + 4));
+    	list.add(event);
+    	msg += "Got it. I've added this task: " + '\n';
+    	msg += "    " + event.toString() + '\n';
+    	msg += "  Now you have " + list.size() + " task(s) in the list.";
+    	return msg;
+	}
+	
+	public static String list() {
+		String msg = "";
+		// accesses the list
+        if (list.size() == 0) {
+            msg += "list is empty";
+        } else {
+            msg += "Here are the tasks in your list:" + '\n';
+            int counter = 1;
+            for (Task s : list) {
+                msg += "    " + counter + ". " + s.toString();
+                counter++;
+                msg += '\n';
+            }
+        }
+        return msg;
+	}	
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<Task> list = new ArrayList<>();
         String name = "**Rick**";
         String line = "____________________________________________________________";
         System.out.println(line);
@@ -14,62 +80,21 @@ public class Duke {
         while (true) {
             String str = sc.nextLine();
             String msg = "  ";
-            // to do: split each command into diff methods so as not to 
-            // clutter up the main method
             if (str.equals("bye")) {
                 // exits the program
                 break;
             } else if (str.equals("list")) {
-                // accesses the list
-                if (list.size() == 0) {
-                    msg += "list is empty";
-                } else {
-                    msg += "Here are the tasks in your list:" + '\n';
-                    int counter = 1;
-                    for (Task s : list) {
-                        msg += "    " + counter + ". " + s.toString();
-                        counter++;
-                        msg += '\n';
-                    }
-                }
+                msg += list();
             } else if (str.split(" ")[0].equals("done")) {
-                // sets a specified task as done
-                int index = Integer.valueOf(str.split(" ")[1]);
-                if (index > list.size()) {
-                    msg += "No such task!";
-                } else if (list.get(index - 1).isDone) {
-                    msg += "Task is already completed!";
-                } else {
-                    list.get(index - 1).markAsDone();
-                    msg += "Nice! I've marked this task as done: " + '\n';
-                    msg += "    " + list.get(index - 1).toString();
-                }
+                msg += makeDone(str);
             } else if (str.split(" ")[0].equals("todo")) {
-            	Task todo = new ToDo(str.substring(5));
-            	list.add(todo);
-            	msg += "Got it. I've added this task: " + '\n';
-            	msg += "    " + todo.toString() + '\n';
-            	msg += "  Now you have " + list.size() + " task(s) in the list.";
+            	msg += addTodo(str);
             } else if (str.split(" ")[0].equals("deadline")) {
-            	int ind1 = str.indexOf('/');
-            	Task deadline = new Deadline(str.substring(9, ind1 - 1), str.substring(ind1 + 4));
-            	list.add(deadline);
-            	msg += "Got it. I've added this task: " + '\n';
-            	msg += "    " + deadline.toString() + '\n';
-            	msg += "  Now you have " + list.size() + " task(s) in the list.";
+            	msg += addDeadline(str);
             } else if (str.split(" ")[0].equals("event")) {
-            	int ind1 = str.indexOf('/');
-            	Task event = new Event(str.substring(6, ind1 - 1), str.substring(ind1 + 4));
-            	list.add(event);
-            	msg += "Got it. I've added this task: " + '\n';
-            	msg += "    " + event.toString() + '\n';
-            	msg += "  Now you have " + list.size() + " task(s) in the list.";
-            }
-            else {
-                // adds a task to the list
-                Task task = new Task(str);
-                list.add(task);
-                msg += "added: " + str;
+            	msg += addEvent(str);
+            } else {
+                msg += "Unknown command";
             }
             System.out.println(line);
             System.out.println(msg);
