22655bc6fb7781912651d3b915c7d72ecc599410
ngua2014 <ngua2014@uni.sydney.edu.au>
2020-01-22 16:19:50 +0800

    Level 3 Completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,7 +7,7 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int counter = 0;
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -19,10 +19,16 @@ public class Duke {
                 break;
             } else if (next.equals("list")) {
                 for (int i = 0; i < counter; i++) {
-                System.out.printf("%d. %s\n",i +1,tasks[i]);
+                System.out.printf("%d.[%s] %s\n",i +1,tasks[i].getStatusIcon(),tasks[i].getDescription());
                 }
+            } else if (next.startsWith("done")){
+                String temp = next.replaceAll("\\D+","");
+                int FinishedNumber = Integer.parseInt(temp);
+                tasks[FinishedNumber-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.printf("  [%s] %s\n",tasks[FinishedNumber-1].getStatusIcon(),tasks[FinishedNumber-1].getDescription());
             } else {
-                    tasks[counter] = next;
+                    tasks[counter] = new Task(next);
                     System.out.println("added: "+ next);
                     counter +=1;
             }
