cc5cee13c01047b830199bd53159dd0a0295555f
NizarMohd <mohdnizar97@gmail.com>
2020-01-22 17:54:17 +0800

    Level 2 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String line = "__________________________________________________________";
-        String banner="~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                +"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                +"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                +"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
-                +"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
-        String logo = "~~~~~~~~~~~~~~~~~~ ____        _        ~~~~~~~~~~~~~~~~~~\n"
-                + "~~~~~~~~~~~~~~~~~~|  _ \\ _   _| | _____ ~~~~~~~~~~~~~~~~~~\n"
-                + "~~~~~~~~~~~~~~~~~~| | | | | | | |/ / _ \\~~~~~~~~~~~~~~~~~~\n"
-                + "~~~~~~~~~~~~~~~~~~| |_| | |_| |   <  __/~~~~~~~~~~~~~~~~~~\n"
-                + "~~~~~~~~~~~~~~~~~~|____/ \\__,_|_|\\_\\___|~~~~~~~~~~~~~~~~~~\n";
+        List<String> l1= new ArrayList<String>();
+        String line = "\t__________________________________________________________";
+        String banner="\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                +"\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                +"\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                +"\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                +"\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+        String logo = "\t~~~~~~~~~~~~~~~~~~ ____        _        ~~~~~~~~~~~~~~~~~~\n"
+                + "\t~~~~~~~~~~~~~~~~~~|  _ \\ _   _| | _____ ~~~~~~~~~~~~~~~~~~\n"
+                + "\t~~~~~~~~~~~~~~~~~~| | | | | | | |/ / _ \\~~~~~~~~~~~~~~~~~~\n"
+                + "\t~~~~~~~~~~~~~~~~~~| |_| | |_| |   <  __/~~~~~~~~~~~~~~~~~~\n"
+                + "\t~~~~~~~~~~~~~~~~~~|____/ \\__,_|_|\\_\\___|~~~~~~~~~~~~~~~~~~\n";
         System.out.println(line);
-        System.out.println("Hello from\n" + banner + logo + banner);
-        System.out.println("What can I do for you?");
+        System.out.println("\t" +"Hello from\n" + banner + logo + banner);
+        System.out.println("\t" +"What can I do for you?");
 
         System.out.println(line);
         try {
@@ -23,12 +26,22 @@ public class Duke {
                 String userIn = in.nextLine();
                 if (userIn.equals("bye")) {
                     System.out.println(line);
-                    System.out.println("Bye.Hope to see you again soon!");
+                    System.out.println("\tBye.Hope to see you again soon!");
                     System.out.println(line);
                     break;
-                } else {
+                } else if(userIn.equals("list")) {
+                    int i=0;
                     System.out.println(line);
-                    System.out.println(userIn);
+                    for(i=0; i< l1.size(); i++){
+                        int count =i+1;
+                        System.out.println("\t" +count+". "+l1.get(i));
+                    }
+                    System.out.println(line);
+                }else{
+
+                    System.out.println(line);
+                    l1.add(userIn);
+                    System.out.println("\t" +"added: " + userIn);
                     System.out.println(line);
                 }
             }
