1c436630de4c4c912d74d70868b5b0dc07e9ecb2
JensonWee <55829267+JensonWee@users.noreply.github.com>
2020-01-22 16:29:50 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,36 @@
-public class Duke {
-    public static void doGreeting(){
-        String line = "_".repeat(60);
-        System.out.println(line);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?");
+import java.util.Scanner;
 
+public class Duke {
+    public static void doLine(){
+        String line = "_".repeat(60);
+        System.out.println("\t"+line);
+    }
+    public static void doGreeting(){
+        doLine();
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?");
+        doLine();
     }
     public static void doFarewell(){
-        String line = "_".repeat(60);
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        doLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        doLine();
     }
     public static void main(String[] args) {
         doGreeting();
-        doFarewell();
+        while(true){
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if(line.equals("bye")){
+                doFarewell();
+                break;
+            }else{
+                doLine();
+                System.out.println("\t"+line);
+                doLine();
+            }
+
+        }
+
     }
 }
