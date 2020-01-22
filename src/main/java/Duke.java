0c86d868c44a5de5dd85211b26d53a0b6f44d0bd
JensonWee <55829267+JensonWee@users.noreply.github.com>
2020-01-22 16:49:36 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
+    public static List<String> dukeList = new ArrayList<String>();
     public static void doLine(){
         String line = "_".repeat(60);
         System.out.println("\t"+line);
@@ -15,6 +18,25 @@ public class Duke {
         System.out.println("\tBye. Hope to see you again soon!");
         doLine();
     }
+    public static void addList(String text){
+        dukeList.add(text);
+        removeEmptyItem();
+        doLine();
+        System.out.println("added: " +text);
+        doLine();
+    }
+    public static void removeEmptyItem(){
+        List<String> string = new ArrayList<String>();
+        string.add("");
+        dukeList.removeAll(string);
+    }
+    public static void getList(){
+        doLine();
+        for(int i =0; i<dukeList.size();i++){
+            System.out.println("\t"+(i+1)+". "+dukeList.get(i));
+        }
+        doLine();
+    }
     public static void main(String[] args) {
         doGreeting();
         while(true){
@@ -24,10 +46,10 @@ public class Duke {
             if(line.equals("bye")){
                 doFarewell();
                 break;
+            }else if(line.equals("list")){
+                getList();
             }else{
-                doLine();
-                System.out.println("\t"+line);
-                doLine();
+                addList(line);
             }
 
         }
