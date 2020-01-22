7ea11f7da93deafd4cfbd640011f281bf2b35a21
ngua2014 <ngua2014@uni.sydney.edu.au>
2020-01-22 16:01:42 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,17 +7,26 @@ public class Duke {
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
+        String[] tasks = new String[100];
+        int counter = 0;
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
         Scanner input = new Scanner(System.in);
         while (true){
-            String next = input.next();
+            String next = input.nextLine();
             if (next.equals("bye")){
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (next.equals("list")) {
+                for (int i = 0; i < counter; i++) {
+                System.out.printf("%d. %s\n",i +1,tasks[i]);
+                }
             } else {
-                System.out.println(""+ next);
+                    tasks[counter] = next;
+                    System.out.println("added: "+ next);
+                    counter +=1;
+            }
             }
         }
     }
-}
+
