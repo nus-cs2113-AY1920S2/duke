a40555409897f191708def12dd01c0a5ff337839
weiyang <piadruids@gmail.com>
2020-01-23 00:24:50 +0800

    [Update] Week 0 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        String lines = "____________________________________________________________\n";
+        String toprint = lines + "Hello! I'm Duke\n" + "What can I do for you?\n" + lines + "Bye. Hope to see you again soon!\n" + lines;
+        System.out.print(toprint);
+//        System.out.println("Hello from\n" + logo);
     }
+
+
+
+//    public static int multiply(int i, int j) {
+//        return i * j;
+//    }
+
 }
