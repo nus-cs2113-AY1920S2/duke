4047647c1bdd02ba0dc15c9048b94b669a6c26d5
JensonWee <55829267+JensonWee@users.noreply.github.com>
2020-01-22 17:14:12 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    public static List<String> dukeList = new ArrayList<String>();
+    public static List<Task> dukeList = new ArrayList<Task>();
     public static void doLine(){
         String line = "_".repeat(60);
         System.out.println("\t"+line);
@@ -19,21 +19,17 @@ public class Duke {
         doLine();
     }
     public static void addList(String text){
-        dukeList.add(text);
-        removeEmptyItem();
+        dukeList.add(new Task(text));
         doLine();
         System.out.println("added: " +text);
         doLine();
     }
-    public static void removeEmptyItem(){
-        List<String> string = new ArrayList<String>();
-        string.add("");
-        dukeList.removeAll(string);
-    }
+
     public static void getList(){
         doLine();
+        System.out.println("Here are the tasks in your list:");
         for(int i =0; i<dukeList.size();i++){
-            System.out.println("\t"+(i+1)+". "+dukeList.get(i));
+            System.out.println("\t"+(i+1)+".["+dukeList.get(i).getStatusIcon()+"] "+dukeList.get(i).getDescription() );
         }
         doLine();
     }
@@ -46,8 +42,25 @@ public class Duke {
             if(line.equals("bye")){
                 doFarewell();
                 break;
-            }else if(line.equals("list")){
+            }else if(line.equals("list")) {
                 getList();
+            }else if(line.split(" ")[0].equals("done")){
+                if(line.split(" ")[1].matches("-?\\d+")){
+                    try {
+                        dukeList.get(Integer.parseInt(line.split(" ")[1]) - 1).markAsDone();
+                        doLine();
+                        System.out.println("\tNice! I've marked this task as done:");
+                        System.out.println("\t\t[" + dukeList.get(Integer.parseInt(line.split(" ")[1]) - 1).getStatusIcon() + "] " + dukeList.get(Integer.parseInt(line.split(" ")[1]) - 1).getDescription());
+                    } catch (Exception e){
+                        doLine();
+                        System.out.println("Index Out of Bounds. Please input within List range.");
+                        doLine();
+                    }
+                }else{
+                    doLine();
+                    System.out.println("You have entered incorrect input. Please try again!");
+                    doLine();
+                }
             }else{
                 addList(line);
             }
