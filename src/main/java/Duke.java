8c7f78edc6e9060a0bd3d74cf52a3b613652f2bd
brandoncjh <brandonchongjh@gmail.com>
2020-01-22 15:56:15 +0800

    added greet user and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        String introMessage = "____________________________________________________________\n" 
+        + "Hello! I'm Duke\n" 
+        + "What can I do for you?\n"
+        + "____________________________________________________________";
+        String goodbyeMessage = "Bye! Hope to see you again soon\n"
+        + "____________________________________________________________";
+        System.out.println(introMessage);
+        System.out.println(goodbyeMessage);
     }
 }
