57a6633c102bb040b86b1f29c16c1a16ae489593
siuhian <42374522+siuhian@users.noreply.github.com>
2020-01-22 23:56:25 +0800

    Add Level-0 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  ____        _        \n"
+                + " |  _ \\ _   _| | _____ \n"
+                + " | | | | | | | |/ / _ \\\n"
+                + " | |_| | |_| |   <  __/\n"
+                + " |____/ \\__,_|_|\\_\\___|\n";
+        printLine();
+        System.out.println(logo);
+        System.out.println(" Hello! I'm Duke");
+        System.out.println(" What can I do for you?");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+    public static void printLine() {
+        for (int i = 0; i < 60; i += 1) {
+            System.out.print("_");
+        }
+        System.out.println();
     }
 }
