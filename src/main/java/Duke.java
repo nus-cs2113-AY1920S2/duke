eedb22198de9d730b8926fff093d376995e72697
GanapathySanathBalaji <e0311091@u.nus.edu>
2020-01-22 18:22:42 +0800

    Add increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,75 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    private static String[] list = new String[100];
+    private static int sizeOfList = 0;
+
+    public static void printSpaces(int numberOfSpaces){
+        while(numberOfSpaces > 0){
+            System.out.print(" ");
+            numberOfSpaces--;
+        }
+    }
+
+    public static void printLine(){
+        printSpaces(4);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void welcomeMessage(){
+        printLine();
+        printSpaces(5);
+        System.out.println("Hello! I'm Duke");
+        printSpaces(5);
+        System.out.println("What can I do for you?");
+        printLine();
+        System.out.println();
+    }
+
+    public static void byeMessage(){
+        printLine();
+        printSpaces(5);
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void displayList(){
+        printLine();
+        for(int i = 1; i <= sizeOfList; i++){
+            printSpaces(5);
+            System.out.println( i + ". " + list[i-1]);
+        }
+        printLine();
+    }
+
+    public static void addToList(String command){
+        list[sizeOfList] = command;
+        printLine();
+        printSpaces(5);
+        System.out.println("added: " + command );
+        printLine();
+        sizeOfList++;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("    ____________________________________________________________"+"\n"+
-                "     Hello! I'm Duke\n" +
-                "     What can I do for you?\n" +
-                "    ____________________________________________________________\n");
+        welcomeMessage();
         String command;
-        do{
-            command = sc.next();
-            System.out.println("    ____________________________________________________________");
-            if(command.equals("bye")){
-                System.out.println("     Bye. Hope to see you again soon!");
+        command = sc.nextLine();
+        while(!command.equals("bye")){
+            switch(command) {
+            case "list":
+                displayList();
+                break;
+            default:
+                addToList(command);
+                break;
             }
-            else
-            {
-                System.out.println("     "+command);
-            }
-            System.out.println("    ____________________________________________________________\n");
-        }while(!command.equals("bye"));
+            System.out.println();
+            command = sc.nextLine();
+        }
+        byeMessage();
     }
 }
