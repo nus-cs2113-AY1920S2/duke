00238bb33a4b996cdba74b37739407a0b4ae6211
kokjoon97 <e0309136@u.nus.edu>
2020-01-22 21:03:16 +0800

    Tweak code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,32 +3,32 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm KJ\n" + "How can I help you today?");
-        String cmd;
-        boolean isAsking = true;
+        String command;
+        boolean shouldContinue = true;
         Scanner input = new Scanner(System.in);
-        Task[] list = new Task[100];
+        Task[] instructions = new Task[100];
         int count = 0;
         do {
-            cmd = input.nextLine();
-            if (cmd.equals("bye")) {
+            command = input.nextLine();
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                isAsking = false;
-            } else if (cmd.equals("list")) {
+                shouldContinue = false;
+            } else if (command.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int x = 0; x < count; x++) {
-                    System.out.println((x+1) + ".[" + list[x].getStatus() + "] " + list[x].getDescription());
+                    System.out.println((x+1) + ".[" + instructions[x].getStatus() + "] " + instructions[x].getDescription());
                 }
-            } else if (cmd.contains("done")) {
-                String[] phrases = cmd.split(" ");
+            } else if (command.contains("done")) {
+                String[] phrases = command.split(" ");
                 int index = Integer.parseInt(phrases[1]);
-                list[index-1].markAsDone();
+                instructions[index-1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [" + list[index-1].getStatus() + "] " + list[index-1].getDescription());
+                System.out.println("  [" + instructions[index-1].getStatus() + "] " + instructions[index-1].getDescription());
             } else {
-                list[count] = new Task(cmd);
+                instructions[count] = new Task(command);
                 count += 1;
-                System.out.println("added: " + cmd);
+                System.out.println("added: " + command);
             }
-        } while (isAsking);
+        } while (shouldContinue);
     }
 }
