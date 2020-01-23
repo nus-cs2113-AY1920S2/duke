3bbc2ad5403d81b99101494711a1bec7e4d5a950
jinfayap <jinfayap@gmail.com>
2020-01-23 19:04:32 +0800

    Add Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,12 +13,16 @@ public class Duke {
         System.out.println("What can i do for you?");
         System.out.println("____________________________________________________________");
 
+        String[] tasksList = new String[100];
+        int taskCount = 0;
         Scanner in = new Scanner(System.in);
         while (true) {
             String userInput = in.nextLine();
             if (userInput.toLowerCase().equals("list")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("\tlist");
+                for (int i = 0; i < taskCount; ++i) {
+                    System.out.println((i+1) + ". " + tasksList[i]);
+                }
                 System.out.println("____________________________________________________________");
             } else if (userInput.toLowerCase().equals("blah")) {
                 System.out.println("____________________________________________________________");
@@ -29,6 +33,11 @@ public class Duke {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else {
+                tasksList[taskCount++] = userInput;
+                System.out.println("____________________________________________________________");
+                System.out.println("\tadded: " + userInput);
+                System.out.println("____________________________________________________________");
             }
         }
     }
