0b5216b2c265e060f1a91cebe92d872718265573
Janicetyy <janice.tohy2@gmail.com>
2020-01-23 23:21:12 +0800

    add skeletal code to echo user input and exit on bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void greet(){
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke\n" +
-                " What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+    public static void instruction(String dukeCommand){
+        if (dukeCommand.equals("greet")) {
+            System.out.println("____________________________________________________________");
+            System.out.println("Hello! I'm Duke\n" +
+                    " What can I do for you?");
+            System.out.println("____________________________________________________________");
+        }else if (dukeCommand.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("____________________________________________________________");
+        }else{
+            System.out.println("____________________________________________________________");
+            System.out.println(dukeCommand + "\n");
+            System.out.println("____________________________________________________________");
+        }
+        /*else{
+            System.out.println("____________________________________________________________");
+            System.out.println("Duke cannot understand your command.\n");
+            System.out.println("____________________________________________________________");
+        }*/
     }
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -14,6 +29,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        greet();
+        instruction("greet");
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            instruction(line);
+            line = in.nextLine();
+        }
+        instruction("bye");
+
     }
 }
