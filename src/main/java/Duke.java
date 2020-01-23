bce24efa31ef7a1ef02d99c0e6a97d301a4cf714
jinfayap <jinfayap@gmail.com>
2020-01-23 18:59:40 +0800

    Add Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,7 +12,24 @@ public class Duke {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can i do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String userInput = in.nextLine();
+            if (userInput.toLowerCase().equals("list")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("\tlist");
+                System.out.println("____________________________________________________________");
+            } else if (userInput.toLowerCase().equals("blah")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("\tblah");
+                System.out.println("____________________________________________________________");
+            } else if (userInput.toLowerCase().equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
     }
 }
