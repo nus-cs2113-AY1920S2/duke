f931edc9af7901ac4c193fb2d7cbac575b5e6eb3
Joel <joelchang97@gmail.com>
2020-01-23 15:43:43 +0800

    Implemented Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,10 +3,15 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private ArrayList<String> tasks;
+    Task newTask;
+    private ArrayList<Task> tasks;
 
     public Duke() {
-        this.tasks = new ArrayList<String>();
+        this.tasks = new ArrayList<Task>();
+    }
+    
+    private void printLine() {
+        System.out.println(" ======================================================================");
     }
 
     private void printGreetingMessage() {
@@ -16,26 +21,24 @@ public class Duke {
     }
 
     private void printLeavingMessage() {
-        System.out.println("    Bye! Duke is now a freeeeee elf again!!!!");
+        System.out.println(" Bye! Duke is now a freeeeee elf again!!!!");
     }
 
-    private void printLine() {
-        System.out.println(" ======================================================================");
-    }
-
-    private void echo(String convertToLowerCase) {
-        System.out.println("    " + convertToLowerCase);
+    private void printExceptionError(Exception exception) {
+        System.out.println(" Duke cannot understand what you are trying to do!");
+        System.out.println(" Error message: " + exception);
     }
 
     private void add(String description) {
-        tasks.add(description);
-        System.out.println("    Added: " + description);
-        System.out.println("    There are currently " + tasks.size() + " item(s) queued");
+        newTask = new Task(description);
+        tasks.add(newTask);
+        System.out.println(" Added: " + description);
+        System.out.println(" There are currently " + tasks.size() + " item(s) queued");
     }
 
     private void printList() {
         int position = 1;
-        for (String text: tasks) {
+        for (Task text: tasks) {
             System.out.println("    " + position + ". " + text);
             position ++;
         }
@@ -43,10 +46,46 @@ public class Duke {
 
     private void list() {
         if (tasks.size() == 0) {
-            System.out.println("    Hmph!! There are currently no items queued");
+            System.out.println(" Hmph!! There are currently no items queued");
         } else {
             System.out.println(" Here are the list of tasks:");
             printList();
+        }
+    }
+
+    private String getFirstWord(String line) {
+        String removeTrailingSpaces = line.trim();
+        String [] words = removeTrailingSpaces.split(" ");
+        String firstWord = words[0].toLowerCase();
+        return firstWord;
+    }
+
+    private int getIndex(String line) {
+        String removeTrailingSpaces = line.trim();
+        String numericalIndex = removeTrailingSpaces.substring(5,line.length()).trim();
+        int index = Integer.valueOf(numericalIndex) - 1;
+        return index;
+    }
+
+    private void done(int index) {
+        Task completedTask = tasks.get(index);
+        boolean isCompleted = completedTask.getStatus();
+        if (isCompleted) {
+            System.out.println(" The task(s) has already been marked as done :x");
+        } else {
+            completedTask.markAsDone();
+            tasks.set(index, completedTask);
+            System.out.println(" Duke has marked the following tasks as done:");
+            System.out.println("  " + completedTask);
+        }
+    }
+
+    private void getDoneException(String line) {
+        try {
+            int index = getIndex(line);
+            done(index);
+        } catch (Exception exception) {
+            printExceptionError(exception);
         }
     }
 
@@ -54,25 +93,27 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         while (true) {
             String line = sc.nextLine();
-            String removeTrailingSpaces = line.trim();
-            String convertToLowerCase = removeTrailingSpaces.toLowerCase();
-            boolean isBye = convertToLowerCase.equals("bye");
-            boolean isList = convertToLowerCase.equals("list");
+            String firstWord = getFirstWord(line);
+            boolean isBye = firstWord.equals("bye");
+            boolean isList = firstWord.equals("list");
+            boolean isDone = firstWord.equals("done");
             printLine();
             if (isBye) {
                 printLeavingMessage();
                 printLine();
                 break;
-            } else if(isList) {
+            } else if (isList) {
                 list();
+            } else if (isDone) {
+                getDoneException(line);
             } else {
-                add(removeTrailingSpaces);
+                add(line);
             }
             printLine();
         }
         sc.close();
     }
-
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean getStatus() {
+        return isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String toString() {
+        return "[ " + getStatusIcon() + " ] " + getDescription();
+    }
+}
