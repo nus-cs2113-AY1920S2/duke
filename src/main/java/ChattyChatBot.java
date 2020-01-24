468a40fe3f5c78fb063888f03909293123be224d
Zhilin <huangzhilin1999@gmail.com>
2020-01-25 02:55:32 +0800

    Add function to add and list tasks

--- src/main/java/ChattyChatBot.java
+++ src/main/java/ChattyChatBot.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ChattyChatBot {
@@ -7,13 +9,21 @@ public class ChattyChatBot {
         String name = "Chatty Chat Bot";
         System.out.println("Hello from " + name);
 
+        List<String> tasks = new ArrayList<>();
+
         while (true) {
             String userInput = SCANNER.nextLine();
             if (userInput.equals("bye")) {
                 System.out.println("Thanks for chatting with me! Bye~");
                 break;
+            } else if (userInput.equals("list")) {
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println((i + 1) + ". " + tasks.get(i));
+                }
+            } else {
+                tasks.add(userInput);
+                System.out.println("added: " + userInput);
             }
-            System.out.println(userInput);
         }
     }
 }
