391ff15fe251a5f1e3e9c8f7b9d814a662dc31aa
lowjiayee <lowjiayee123@gmail.com>
2020-01-24 17:06:18 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final int MAX_TASKS_COUNT = 100;
+    private static String[] tasks = new String[MAX_TASKS_COUNT];
+    private static int indexOfTasks = 0;
+
     public static void main(String[] args) {
         String name = "Duke";
         String input;
@@ -11,7 +15,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("\tHello from\n" + logo);
 
         printLine();
         welcome(name);
@@ -19,21 +23,42 @@ public class Duke {
 
         do {
             input = reader.nextLine();
+            printLine();
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                // close the interpreter
+                System.out.println("\tBye. Hope to see you again soon!");
+            } else if (input.equals("list")) {
+                // list all tasks
+                listTasks();
             } else {
-                System.out.println(input);
+                // add stuff to String[] tasks
+                addTask(input);
             }
             printLine();
         } while (!input.equals("bye"));
     }
 
     public static void printLine() {
-        System.out.println("____________________________________________________________");
+        System.out.println("\t____________________________________________________________");
     }
 
     public static void welcome(String name) {
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("\tHello! I'm " + name);
+        System.out.println("\tWhat can I do for you?");
+    }
+
+    public static void addTask(String task) {
+        if (indexOfTasks < MAX_TASKS_COUNT) {
+            tasks[indexOfTasks] = task;
+            indexOfTasks++;
+            System.out.println("\tadded: " + task);
+        }
+    }
+
+    public static void listTasks() {
+        for (int i = 0; i < indexOfTasks; i++) {
+            System.out.printf("\t%d. %s", i+1, tasks[i]);
+            System.out.println();
+        }
     }
 }
