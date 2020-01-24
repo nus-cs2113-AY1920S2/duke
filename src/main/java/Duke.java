394d897ae9b2b15b8e81ab99416067770d0de1a5
anqi-nus <e0322837@u.nus.edu>
2020-01-24 12:43:56 +0800

    1. Add function "Greet, Echo, Exit" 2. Add function "Add task" 3. Add function "Delete task"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
 public class Duke {
+    public static void printSplit(){
+        System.out.println("    ✻❊✽✼❉✱✲✾❃❋❈❆✿❀❁");
+    }
+
+
+    public static void printBye() {
+        printSplit();
+        System.out.println("    Bye. Hope to see you again soon!");
+        printSplit();
+    }
+
     public static void main(String[] args) {
+        Task task = new Task();
+        Duke split = new Duke();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("    Hello from\n" + logo);
+        printSplit();
+        System.out.println("    Sorry I don't understand your command :(\n" +
+                "    Please select one of the following (key in number): \n" +
+                "    1. Add a new task, \n" +
+                "    2. Delete an old task, \n" +
+                "    3. Show my tasks, or\n"+
+                "    4.bye \n" +
+                "    to end this conversation");
+        printSplit();
+
+        Scanner scan = new Scanner(System.in);
+        int inst = scan.nextInt();
+        while (inst != 4) {
+            Task.exe(inst);
+            inst = scan.nextInt();
+        }
+        printBye();
+
+
     }
 }
+
