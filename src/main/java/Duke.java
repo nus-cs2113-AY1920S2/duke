368ddec3860d32a69095d8af1d7da3dcf975336c
brandoncjh <brandonchongjh@gmail.com>
2020-01-25 21:38:33 +0800

    added required echo function, removed Duke logo, customised display

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,37 @@
+import java.util.Scanner; //object takes in user input
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        String introMessage = "____________________________________________________________\n" 
-        + "Hello! I'm Duke\n" 
+        
+        String introMessage = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" 
+        + "Hello! I'm Duke\n"
         + "What can I do for you?\n"
-        + "____________________________________________________________";
-        String goodbyeMessage = "Bye! Hope to see you again soon\n"
-        + "____________________________________________________________";
+        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        + System.lineSeparator();
+        String goodbyeMessage = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+        + "Bye! Hope to see you again soon\n"
+        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        + System.lineSeparator();
+        String goodbyeMessage2 = "********************CONNECTION TERMINATED********************";
+        
         System.out.println(introMessage);
+        //Level-1: run echo command loop, break on input "bye"
+        while (true){
+            System.out.print(">>>");  //easier to identify lines input by user (a la Python)  
+            String line;
+            Scanner in = new Scanner(System.in);
+
+            line = in.nextLine();
+            if (line.equals("bye")){
+                break;
+            } else{
+                System.out.println( "\t____________________________________________________________\n\t"
+                + line + System.lineSeparator()
+                + "\t____________________________________________________________");
+            }
+        }
+
         System.out.println(goodbyeMessage);
+        System.out.println(goodbyeMessage2);
     }
 }
