aea45259a8f57c89c1bdaaabdeb33010ffed452c
lowjiayee <lowjiayee123@gmail.com>
2020-01-25 11:55:00 +0800

    Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int MAX_TASKS_COUNT = 100;
-    private static String[] tasks = new String[MAX_TASKS_COUNT];
+    private static Task[] tasks = new Task[MAX_TASKS_COUNT];
     private static int indexOfTasks = 0;
 
     public static void main(String[] args) {
@@ -30,6 +30,12 @@ public class Duke {
             } else if (input.equals("list")) {
                 // list all tasks
                 listTasks();
+            } else if (input.split(" ")[0].equals("done")) {
+                // mark a task as done
+                System.out.println("\tNice! I've marked this task as done:");
+                int indexOfTasks = Integer.parseInt(input.split(" ")[1]) - 1;
+                tasks[indexOfTasks].setDone(true);
+                System.out.println("\t" + tasks[indexOfTasks]);
             } else {
                 // add stuff to String[] tasks
                 addTask(input);
@@ -47,11 +53,11 @@ public class Duke {
         System.out.println("\tWhat can I do for you?");
     }
 
-    public static void addTask(String task) {
+    public static void addTask(String taskDescription) {
         if (indexOfTasks < MAX_TASKS_COUNT) {
-            tasks[indexOfTasks] = task;
+            tasks[indexOfTasks] = new Task(taskDescription);
             indexOfTasks++;
-            System.out.println("\tadded: " + task);
+            System.out.println("\tadded: " + taskDescription);
         }
     }
 
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,37 @@
 public class Task {
+    private static int taskCount;
+
+    private String description;
+    private boolean isDone;
+
+    public Task(String s) {
+        setDescription(s);
+        setDone(false);
+    }
+
+    public char getDone() {
+        if (isDone) {
+            return '✓';
+        } else {
+            return '✗';
+        }
+    }
+
+    public void setDone(boolean status) {
+        isDone = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    private void setDescription(String s) {
+        description = s;
+    }
+
+    @Override
+    public String toString() {
+        String output = "[" + getDone() + "] " + getDescription();
+        return output;
+    }
 }
