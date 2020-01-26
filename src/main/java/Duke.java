b579879bbd27c3e4c0244fe25d4248f262e46b60
alwayshuizhen <alwayshuizhen@gmail.com>
2020-01-27 02:43:21 +0800

    Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 public class Duke {
+    public static String printItemDescription(Task item){
+        return "[" + item.getStatusIcon() + "] " + item.description;
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -10,17 +14,22 @@ public class Duke {
         System.out.println("\nWhat can i do for you?");
         Scanner myObj = new Scanner(System.in);
         String cmd;
-        String[] toAdd = new String[100];
+        Task[] toAdd = new Task[100];
         int counter = 0;
         do {
             cmd = myObj.nextLine();
-            if (!cmd.equals("bye")){
+            if (!cmd.equals("bye")) {
                 if (cmd.equals("list")) {
                     for (int i = 0; i < counter; i++) {
-                        System.out.println(i+1 + ". " + toAdd[i]);
+                        System.out.println(i + 1 + ". " + printItemDescription(toAdd[i]));
                     }
-                } else {
-                    toAdd[counter] = cmd;
+                } else if (cmd.substring(0,4).equals("done")) {
+                    int index = Integer.parseInt(cmd.substring(cmd.length() - 1)) - 1;
+                    toAdd[index].setDone();
+                    System.out.println("Nice! I've marked this task as done: " + printItemDescription(toAdd[index]));
+                }else{
+                    Task t = new Task(cmd);
+                    toAdd[counter] = t;
                     counter++;
                     System.out.println("added: " + cmd);
                 }
