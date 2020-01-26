371a2de8b1fc7992f0bb473698c97a8959be42e6
Bencotti <HanaTwoC@gmail.com>
2020-01-27 02:24:17 +0800

    Add save feature and listing of all recorded tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Duke {
+
+    static String[] tasks = new String[100];
+    static int taskCounter = 0;
 
     public static void getDateTime() {
         LocalDateTime myDateObj = LocalDateTime.now();
@@ -11,35 +15,56 @@ public class Duke {
         System.out.println(formattedDate);
     }
 
-    public static String parseCommand() {
-        Scanner input = new Scanner(System.in);
-        String command = input.next();
-        return command;
+    public static void printList() {
+        int count = 1;
+        System.out.println("Listing tasks below:");
+        for (String task : tasks) {
+            if (task == null){
+                break;
+            }
+            System.out.println(count + ". " + task);
+            count++;
+        }
+        System.out.println("");
     }
 
     public static void main(String[] args) {
         String logo =   "  ,--,       ,---,   .--.--.       ,---,        \n"
-                        + ",--.'|    ,`--.' |  /  /    '.    '  .' \\       \n"
-                        + "|  | :    |   :  : |  :  /`. /   /  ;    '.     \n"
-                        + ":  : '    :   |  ' ;  |  |--`   :  :       \\    \n"
-                        + "|  ' |    |   :  | |  :  ;_     :  |   /\\   \\   \n"
-                        + "'  | |    '   '  ;  \\  \\    `.  |  :  ' ;.   : \n"
-                        + "|  | :    |   |  |   `----.   \\ |  |  ;/  \\   \\ \n"
-                        + "'  : |__  '   :  ;   __ \\  \\  | '  :  | \\  \\ ,' \n"
-                        + "|  | '.'| |   |  '  /  /`--'  / |  |  '  '--'   \n"
-                        + ";  :    ; '   :  | '--'.     /  |  :  : \n"
-                        + "|  ,   /  ;   |.'    `--'---'   |  | ,'\n"
-                        + "---`-'   '---'                 `--''   \n";
+                + ",--.'|    ,`--.' |  /  /    '.    '  .' \\       \n"
+                + "|  | :    |   :  : |  :  /`. /   /  ;    '.     \n"
+                + ":  : '    :   |  ' ;  |  |--`   :  :       \\    \n"
+                + "|  ' |    |   :  | |  :  ;_     :  |   /\\   \\   \n"
+                + "'  | |    '   '  ;  \\  \\    `.  |  :  ' ;.   : \n"
+                + "|  | :    |   |  |   `----.   \\ |  |  ;/  \\   \\ \n"
+                + "'  : |__  '   :  ;   __ \\  \\  | '  :  | \\  \\ ,' \n"
+                + "|  | '.'| |   |  '  /  /`--'  / |  |  '  '--'   \n"
+                + ";  :    ; '   :  | '--'.     /  |  :  : \n"
+                + "|  ,   /  ;   |.'    `--'---'   |  | ,'\n"
+                + "---`-'   '---'                 `--''   \n";
 
         System.out.println("\n" + logo + "\nYour Lifestyle Scheduling Assistant\n");
         System.out.println("____________________________________________________________\n\nCurrent time: ");
         getDateTime();
         System.out.println("____________________________________________________________");
 
-        String command = parseCommand();
-        while (!command.equals("bye")){
-            System.out.println("LISA: "+ command);
-            command = parseCommand();
+
+
+        Scanner input = new Scanner(System.in);
+        String userCommand = input.nextLine();
+
+        while (!userCommand.equals("bye")){
+            //System.out.println("LISA: "+ userCommand);
+
+            switch (userCommand) {
+                case "list":
+                    printList();
+                    break;
+                default:
+                    tasks[taskCounter++] = userCommand;
+                    System.out.println("added: " + userCommand);
+            }
+
+            userCommand = input.nextLine();
         }
 
         System.out.println("LISA: Bye, hope to see you again!");
