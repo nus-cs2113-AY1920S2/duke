39e6e32465aae1f451164451e84365af1c053b7e
jichngan <jcngan97@gmail.com>
2020-01-26 09:52:01 +0800

    Add functionality to add task to a list. Add functionality to list out the tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,9 +8,10 @@ public class Duke {
                 + "|   _  <  |  |  |  | |   _  <  \n"
                 + "|  |_)  | |  `--'  | |  |_)  | \n"
                 + "|______/   \\______/  |______/  \n";
-        String divider = "-----------------------------------------------";
+        String divider = "_________________________________________________";
         String welcomeMessage = "Hello human! I am Bob.\n" + "What can I do for you?";
 
+        //Welcome and sign out messages
         System.out.println("Bob the chatbot\n" + logo);
         System.out.println(welcomeMessage);
         System.out.println(divider);
@@ -19,6 +20,14 @@ public class Duke {
         String endCommand = "bye";
         String endMessage = "Bob thanks you for coming! See you again soon!";
 
+        //Storing text
+        String[] tasks = new String[100]; //Array of tasks
+        int numTasks = 0;
+
+        //List items
+        String listCommand = "list";
+
+
         while (true) {
             String userInput = command.nextLine();
             if (userInput.equals(endCommand)) {
@@ -26,9 +35,18 @@ public class Duke {
                 System.out.println(endMessage);
                 System.out.println(divider);
                 break;
+            } else if (userInput.equals(listCommand)) {
+                System.out.println(divider);
+                for (int i = 1; i < numTasks+1; i++) {
+                    String taskNum = Integer.toString(i);
+                    System.out.println(taskNum + ". " + tasks[i-1]);
+                }
+                System.out.println(divider);
             } else {
                 System.out.println(divider);
-                System.out.println(userInput);
+                tasks[numTasks] = userInput;
+                numTasks++;
+                System.out.println("added: " + userInput);
                 System.out.println(divider);
             }
         }
