04a4afb136ba60880544dd704016cd85a3b40e8e
alwayshuizhen <alwayshuizhen@gmail.com>
2020-01-27 01:50:59 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,10 +10,20 @@ public class Duke {
         System.out.println("\nWhat can i do for you?");
         Scanner myObj = new Scanner(System.in);
         String cmd;
+        String[] toAdd = new String[100];
+        int counter = 0;
         do {
             cmd = myObj.nextLine();
             if (!cmd.equals("bye")){
-                System.out.println(cmd);
+                if (cmd.equals("list")) {
+                    for (int i = 0; i < counter; i++) {
+                        System.out.println(i+1 + ". " + toAdd[i]);
+                    }
+                } else {
+                    toAdd[counter] = cmd;
+                    counter++;
+                    System.out.println("added: " + cmd);
+                }
             } else {
                 System.out.println("Bye bye! Talk to me again soon!");
             }
