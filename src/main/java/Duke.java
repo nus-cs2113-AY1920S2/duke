945c1750a672da31e9f2e748a382f321542df68c
lowxizhi <lowxizhi@gmail.com>
2020-01-27 16:00:46 +0800

    add ability to store and list items entered by the user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,16 +12,31 @@ public class Duke {
                 + "\tHello! I'm Duke\n" +
                 "\tWhat can I do for you?\n"
                 + "\t____________________________________________________________\n");
+
+
+        String[] taskList = new String[100];
+        int taskCount = 0;
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
         while (!line.equals("bye")){
-            System.out.println("\t____________________________________________________________\n"
-                + "\t" + line + "\n"
-                + "\t____________________________________________________________\n");
+            if(line.equals("list")){
+                System.out.println("\t____________________________________________________________");
+                for(int i=0; i<taskCount; ++i){
+                    System.out.println("\t" + i + ". " + taskList[i]);
+                }
+                System.out.println("\t____________________________________________________________\n");
+            }else{
+                taskList[taskCount] = line;
+                taskCount++;
+                System.out.println("\t____________________________________________________________\n"
+                        + "\t added: " + line + "\n"
+                        + "\t____________________________________________________________\n");
+            }
             line = in.nextLine();
         }
         System.out.println("\t____________________________________________________________\n"
                 + "\tBye. Hope to see you again soon!\n"
                 + "\t____________________________________________________________\n");
+
     }
 }
