8f57e0394978f99480bbe5e430f2ca895963bfaf
Bencotti <HanaTwoC@gmail.com>
2020-01-27 15:54:33 +0800

    Add logic for task completion and updated code to support Task class
    This has version creates new tasks as a task object, and stores each new task as an arrayList for dynamic storage.
    Code has improved user input validation to prevent breaking by undefined behaviour.
    A predefined command has to be used before activating a method.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,10 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Duke {
-
-    static String[] tasks = new String[100];
-    static int taskCounter = 0;
 
     public static void getDateTime() {
         LocalDateTime myDateObj = LocalDateTime.now();
@@ -15,17 +13,49 @@ public class Duke {
         System.out.println(formattedDate);
     }
 
-    public static void printList() {
+    public static void completeTask(ArrayList<Task> tasks, int index){
+        index--; // index starts from 0, unlike list
+        if ( (index < tasks.size()) || (index > 0)) { // check if out of bounce
+            Task t = tasks.get(index);
+            if (t.getStatus()) { // check if already completed
+                System.out.println("Task already completed!\n");
+            } else {
+                t.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + t.getStatusIcon() + "] " + t.getDescription() + "\n");
+            }
+        } else {
+            System.out.println("Error: No such index in use\n");
+        }
+    }
+
+    public static void printList(ArrayList<Task> tasks) {
         int count = 1;
         System.out.println("Listing tasks below:");
-        for (String task : tasks) {
-            if (task == null){
-                break;
+
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks at the moment!");
+        } else {
+            for (Task currentTask : tasks) {
+                System.out.println("[" + currentTask.getStatusIcon() + "] " + count + ". " + currentTask.getDescription());
+                count++;
             }
-            System.out.println(count + ". " + task);
-            count++;
         }
         System.out.println("");
+    }
+
+    public static void printHelp() {
+        System.out.println("Commands: ");
+        System.out.println("List: lists all recorded tasks - usage: list");
+        System.out.println("Done: mark task as completed - usage: done <task number>");
+        System.out.println("New: add a new task - usage: new");
+        System.out.println("");
+    }
+
+    public static void addTask(ArrayList<Task> tasks, String newTask) {
+        Task t = new Task(newTask);
+        tasks.add(t);
+        System.out.println("added: " + newTask + "\n");
     }
 
     public static void main(String[] args) {
@@ -43,11 +73,13 @@ public class Duke {
                 + "---`-'   '---'                 `--''   \n";
 
         System.out.println("\n" + logo + "\nYour Lifestyle Scheduling Assistant\n");
+        System.out.println("type \"help\" for list of commands");
         System.out.println("____________________________________________________________\n\nCurrent time: ");
         getDateTime();
         System.out.println("____________________________________________________________");
 
-
+        ArrayList<Task> tasks = new ArrayList<Task>();
+        int taskCounter = 0;
 
         Scanner input = new Scanner(System.in);
         String userCommand = input.nextLine();
@@ -55,13 +87,33 @@ public class Duke {
         while (!userCommand.equals("bye")){
             //System.out.println("LISA: "+ userCommand);
 
-            switch (userCommand) {
+            String[] words = userCommand.split(" ");
+            int length = words.length;
+
+            if (length == 1) { //for user commands
+                switch (words[0]) {
                 case "list":
-                    printList();
+                    printList(tasks);
+                    break;
+                case "done":
+                    System.out.println("What is the index of the completed task?");
+                    int index = input.nextInt();
+                    input.nextLine();
+                    completeTask(tasks, index);
+                    break;
+                case "new":
+                    System.out.println("Please input your new task:");
+                    String newTask = input.nextLine();
+                    addTask(tasks, newTask);
+                    break;
+                case "help":
+                    printHelp();
                     break;
                 default:
-                    tasks[taskCounter++] = userCommand;
-                    System.out.println("added: " + userCommand);
+                    System.out.println("Command not recognised \n");
+                }
+            } else {
+                System.out.println("Command not recognised, please refer to the help guide.");
             }
 
             userCommand = input.nextLine();
