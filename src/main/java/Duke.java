53a4e43616932f973bcd7466af65e464b843dd94
A11riseforme <ycl0217@hotmail.com>
2020-01-27 17:43:15 +0800

    more structured level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,42 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String logo = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    private static final String lineDivider = "____________________________________________________________";
+    private static final String indentation = "     ";
+    private static final String greetingWords = indentation + "Hello! I'm Duke\n" + indentation + "What can I do for you?";
+    private static final String byeWords = "Bye. Hope to see you again soon!";
+
+    private static void greet() {
+        System.out.println(logo);
+        System.out.println(greetingWords);
+    }
+
+    private static void echo(String command) {
+        System.out.println(lineDivider);
+        System.out.println(indentation + command);
+        System.out.println(lineDivider);
+    }
+    
+    private static void bye() {
+        System.out.println(lineDivider);
+        System.out.println(indentation + byeWords);
+        System.out.println(lineDivider);
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        String indentation = "     ";
-        String line = "    ____________________________________________________________";
-        String greetings = "     Hello! I'm Duke\n" +
-                "     What can I do for you?";
-        String bye = "Bye. Hope to see you again soon!";
-
-        //System.out.println("Hello from\n" + logo);
-
-        System.out.println(line);
-        System.out.println(greetings);
-        System.out.println(line);
+        greet();
 
         Scanner s  = new Scanner(System.in);
         String command = s.nextLine();
         while (!command.equals("bye")) {
-            System.out.println(line);
-            System.out.println(indentation + command);
-            System.out.println(line);
+            echo(command);
             command = s.nextLine();
         }
-        System.out.println(line);
-        System.out.println(indentation + bye);
-        System.out.println(line);
+        bye();
     }
 }
