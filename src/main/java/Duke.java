6bdcfa401f9d75ac4864362bc7e70b3f39d4fc87
lowxizhi <lowxizhi@gmail.com>
2020-01-27 15:17:07 +0800

    add echo function and exit message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,11 +8,20 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n"
-                + " Hello! I'm Duke\n" +
-                " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________");
+        System.out.println("\t____________________________________________________________\n"
+                + "\tHello! I'm Duke\n" +
+                "\tWhat can I do for you?\n"
+                + "\t____________________________________________________________\n");
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while (!line.equals("bye")){
+            System.out.println("\t____________________________________________________________\n"
+                + "\t" + line + "\n"
+                + "\t____________________________________________________________\n");
+            line = in.nextLine();
+        }
+        System.out.println("\t____________________________________________________________\n"
+                + "\tBye. Hope to see you again soon!\n"
+                + "\t____________________________________________________________\n");
     }
 }
