b6b966aa270d661686ee069ff8321b5cb0d0a7a9
siuhian <42374522+siuhian@users.noreply.github.com>
2020-01-27 17:07:33 +0800

    Add Echo and Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "  ____        _        \n"
@@ -10,13 +12,31 @@ public class Duke {
         System.out.println(" Hello! I'm Duke");
         System.out.println(" What can I do for you?");
         printLine();
-        System.out.println(" Bye. Hope to see you again soon!");
-        printLine();
+        String line;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                exitFromApp();
+                break;
+            } else {
+                printLine();
+                System.out.println(" " + line);
+                printLine();
+            }
+        }
     }
+
     public static void printLine() {
         for (int i = 0; i < 60; i += 1) {
             System.out.print("_");
         }
         System.out.println();
+    }
+
+    public static void exitFromApp() {
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
     }
 }
