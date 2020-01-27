470c7464ac0912d034e929e823cba988320321b1
YanChenghao <yanchenghao17@gmail.com>
2020-01-27 16:32:51 +0800

    week3: two modes:1.echo,2.command; class Task used for tasks support addding tasks and marking task status

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,20 +8,79 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("________________________________");
-        System.out.println("Hello,I'm Duke.");
-        System.out.println("What can I do for you?");
+        System.out.println("Hello,I'm little pepper. Your personal sweetheart.");
+        System.out.println("What can I do for you? You can choose two model:");
+        System.out.println("1.echo mode\n2.command mode");
         System.out.println("________________________________");
         Scanner in = new Scanner(System.in);
-        String input = in.next();
-        while(!(input.equals("Bye"))){
-            System.out.println("________________________________");
-            System.out.println(input);
-            System.out.println("________________________________");
-            input = in.next();
+        String input = in.nextLine();
+        Boolean inputNotValid = Boolean.TRUE;
+        while(inputNotValid) {
+            switch(input.trim()){
+            case "1":
+                echoMode(input,in);
+                inputNotValid = Boolean.FALSE;
+                break;
+            case "2":
+                commandMode(input,in);
+                inputNotValid = Boolean.FALSE;
+                break;
+            default:
+                System.out.println("Unknown mode, please try again.");
+                input = in.nextLine();
+            }
         }
-        System.out.println("________________________________");
-        System.out.println("Bye.Hope to see you again soon!");
-        System.out.println("________________________________");
     }
 
+    public static void echoMode(String input,Scanner in){
+        input = in.nextLine().trim().toLowerCase();
+        while(!(input.equals("bye"))){
+            System.out.println("    ________________________________");
+            System.out.println("    "+input);
+            System.out.println("    ________________________________");
+            input = in.nextLine();
+        }
+        sayBye();
+    }
+
+    public static void commandMode(String input,Scanner in){
+        Task[] taskList = new Task[50];
+        int taskNum = 0;
+        input = in.nextLine().trim().toLowerCase();
+        while(!input.equals("bye")){
+            System.out.println("    ________________________________");
+            if(input.startsWith("done")){
+                int dividePosition = input.indexOf(" ");
+                int taskIndex = Integer.parseInt(input.substring(dividePosition+1));
+                Task cur_task = taskList[taskIndex-1];
+                cur_task.setTaskStatus("[\u2713]");
+                System.out.println("    Congratulations! You have just finished this task:");
+                System.out.println("    "+cur_task.getTaskStatus()+cur_task.getTaskName());
+            }
+            else if(!input.equals("list")) {
+                input = input.replaceAll("i want to ","");
+                System.out.println("    added: "+input);
+                taskList[taskNum++] = new Task(input);
+            }
+            else
+                printTaskList(Arrays.copyOf(taskList,taskNum));
+            System.out.println("    ________________________________");
+            input = in.nextLine().trim().toLowerCase();
+        }
+        sayBye();
+    }
+
+    public static void sayBye(){
+        System.out.println("    ________________________________");
+        System.out.println("    Don't leave me alone! Please come back soon!");
+        System.out.println("    ________________________________");
+    }
+
+    public static void printTaskList(Task[] taskList){
+        int index = 0;
+        for(Task task:taskList){
+            index++;
+            System.out.println("    "+index+"."+task.getTaskStatus()+task.getTaskName());
+        }
+    }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,41 @@
+public class Task {
+    private static int taskNum = 0;
+    private String taskStatus;
+    private String taskName;
+
+    public Task(){
+        this("");
+    }
+
+    public Task(String taskName){
+        setTaskName(taskName);
+        if(!taskName.equals(""))
+            setTaskNum(Task.taskNum+1);
+        setTaskStatus("[\u2717]");
+    }
+
+    public static void setTaskNum(int taskNum){
+        Task.taskNum = taskNum;
+    }
+
+    public void setTaskName(String taskName){
+        this.taskName = taskName;
+    }
+
+    public void setTaskStatus(String taskStatus){
+        this.taskStatus = taskStatus;
+    }
+
+    public String getTaskName(){
+        return this.taskName;
+    }
+
+    public String getTaskStatus(){
+        return this.taskStatus;
+    }
+
+    public static int getTaskNum(){
+        return taskNum;
+    }
+
+}
