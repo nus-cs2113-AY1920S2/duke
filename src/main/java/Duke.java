6c606fa10b1862e4c8550877ae80a2050d2de769
kcubey <keziakew98@gmail.com>
2020-01-27 15:32:56 +0800

    add Level1-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        boolean continueRun = true;
+    public static final int maxCmd = 100;
 
+    public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -12,10 +11,17 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
+        Scanner sc = new Scanner(System.in);
+
+        boolean continueRun = true;
+        String userCmd = "";
+        String[] cmdArr = new String[maxCmd];
+        int cmdCounter = 0;
+
         while (continueRun == true){
             System.out.println("==========================");
             System.out.println("What can I do for you?");
-            String userCmd = sc.nextLine();
+            userCmd = sc.nextLine();
 
             System.out.println("You have typed: " +userCmd);
 
@@ -23,9 +29,20 @@ public class Duke {
                 System.out.println("Bye. Hope to see you again!");
                 continueRun = false;
             }
+            else if (userCmd.toLowerCase().equals("list")){
+                System.out.println("Current command list: ");
+
+                for (int i = 0; i < cmdCounter; i++){
+                    int cmdNum = i + 1;
+                    System.out.println( cmdNum +". " +cmdArr[i]);
+                }
+            }
+            else{
+                cmdArr[cmdCounter] = userCmd;
+                cmdCounter++;
+
+                System.out.println("Text added: " +userCmd);
+            }
         }
-
-
-
     }
 }
