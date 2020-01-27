20de280d0b2ac30f8598cefe1e40a92d70c3ae4f
ananda_lye <lyeananda@gmail.com>
2020-01-27 19:55:01 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,37 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
-    /*
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
-    */
 
     public static void main(String[] args) {
         String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
         String goodbye = "Bye. Hope to see you again soon!";
         System.out.println(greeting);
 
+        String[] tasklist = new String[100];
+
         Scanner scanner = new Scanner(System.in);
         int flag = 0;
+        int index = 0;
 
         while (flag == 0) {
             String x = scanner.nextLine();
             if (x.equals("bye")) {
                 System.out.println(goodbye);
                 flag = 1;
+            } else if (x.equals("list")){
+                for (int i = 1; i <= index; i++) {
+                    System.out.print(i);
+                    System.out.print(". " + tasklist[i-1] + "\n");
+                }
             } else {
-                System.out.println(x);
+                System.out.println("added: " + x);
+                tasklist[index] = x;
+                index++;
             }
         }
+
+
 
     }
 
