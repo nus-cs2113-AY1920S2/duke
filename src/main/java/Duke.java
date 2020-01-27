7dbafe5e36ba8d47d8a861de112e4a80748e9339
sitinadiah25 <sitinadiah333@gmail.com>
2020-01-27 19:16:57 +0800

    Add List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static void printList(String[] array, int curr) {
+        for (int i = 0; i < curr; i++) {
+            System.out.println(i+1 + ". " + array[i]);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -20,15 +27,29 @@ public class Duke {
 
         String line = "____________________________________________________________\n";
 
+        String[] array;
+        array = new String[100];
+        int curr = 0;
+
         System.out.println(intro);
 
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
 
         while (!s.equals("bye")) {
-            System.out.println(line);
-            System.out.println(s);
-            System.out.println(line);
+            if (s.equals(("list"))) {
+                //function
+                System.out.println(line);
+                printList(array, curr);
+                System.out.println(line);
+            }
+            else {
+                System.out.println(line);
+                System.out.println("added: " + s);
+                System.out.println(line);
+                array[curr] = s;
+                curr++;
+            }
             in = new Scanner(System.in);
             s = in.nextLine();
         }
