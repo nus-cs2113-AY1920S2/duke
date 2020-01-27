004730d8e0c88dd20ed326340bc5f16cde25f82b
Keith-JK <keithcjk123@gmail.com>
2020-01-27 18:00:16 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,29 +1,46 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
+    public static void printNewLine(){
+        String NEW_LINE = "____________________________________________________________";
+        System.out.println(NEW_LINE);
+    }
+
     public static void main(String[] args) {
         // Create Scanner object
         Scanner myScanner = new Scanner(System.in);
-
+        ArrayList<String> myList = new ArrayList<>();
+        
         // Create Hello message
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("Hello from\n" + logo + "What can I do for you?\n");
 
-        String userInput = "";
-        String NEW_LINE = "____________________________________________________________";
-
-        // Get user input in a loop
-        while(!userInput.equals("bye")){
+        String userInput;
+        do {
             System.out.print("USER:");
             userInput = myScanner.nextLine();
-            System.out.println(NEW_LINE);
-            System.out.println("DUKE:" + userInput);
-            System.out.println(NEW_LINE);
-        }
+            printNewLine();
+
+            switch(userInput){
+            case "bye":
+                break;
+            case "list":
+                for (int i = 0; i < myList.size(); i++){
+                    System.out.println((i+1) + ". " + myList.get(i));
+                }
+                printNewLine();
+                break;
+            default:
+                myList.add(userInput);
+                System.out.println("Added: " + userInput);
+                printNewLine();
+            }
+        }while(!userInput.equals("bye"));
 
         System.out.println("Exiting DUKE\n" + logo);
     }
