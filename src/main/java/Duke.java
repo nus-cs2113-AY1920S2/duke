fa228964bf44d0f96b852cb2fda21101a91a4224
hongquan448 <hongquan448@hotmail.com>
2020-01-28 05:26:47 +0800

    complete level-2, Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,24 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String command;
+        String[] tasks = new String[100];
+        int taskNumber = 0;
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         do {
             Scanner input = new Scanner(System.in);
-            command = input.next();
-            boolean isBye = command.equals("bye");
-            if (isBye) {
+            command = input.nextLine();
+
+            if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < taskNumber; ++i) {
+                    System.out.println(i+1 + ". " + tasks[i]);
+                }
             } else {
-                System.out.println(command);
+                System.out.println("added: " + command);
+                tasks[taskNumber] = command;
+                ++taskNumber;
             }
         } while (command != "bye");
     }
