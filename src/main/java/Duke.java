f4af9fa9ca4d62c0d56eba285826e9c70e00dc5b
ananda_lye <lyeananda@gmail.com>
2020-01-27 12:36:17 +0800

    Level 0. Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 public class Duke {
+    /*
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +7,13 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+    */
+
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Duke\n" + "What can I do for you?";
+        String goodbye = "Bye. Hope to see you again soon!";
+        System.out.println(greeting);
+        System.out.println(goodbye);
     }
 }
