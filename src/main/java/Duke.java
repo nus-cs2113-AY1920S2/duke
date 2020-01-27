6c3515c7d99996b64c7b15b631ced8aadf54e426
harithadiv <haritha.divakar@gmail.com>
2020-01-27 16:50:53 +0800

    Add list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,37 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private static final String LINE = "____________________________________________________________ ";
-    private static final String SPACE = "     ";
+    private static final String BORDER = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ";
+    private static final String SPACE = "  ";
+    private static final List<String> tasksLists = new ArrayList<>(100);
 
-    private static void printLine(){
-        System.out.println(LINE);
+    private static void printBorder(){
+        System.out.println(BORDER);
     }
     private static void printMessage(String message){
         System.out.println(SPACE + message);
     }
 
     private static void greetUser(){
-        printLine();
+        printBorder();
         printMessage("Hello! I'm Duke");
         printMessage("What can I do for you?");
-        printLine();
+        printBorder();
     }
 
     private static void replyUser(){
         Scanner scanner = new Scanner(System.in);
         while(true){
             String input = scanner.nextLine();
-            if(input.equals("bye")){
-                printLine();
+            printBorder();
+            if(input.equals("bye")) {
                 printMessage("Bye. Hope to see you again soon!");
-                printLine();
+                printBorder();
                 return;
             }
+            if(input.equals("list")){
+                for (int i = 0; i < tasksLists.size(); i++ ){
+                    printMessage((i+1) + ". " + tasksLists.get(i));
+                }
+                printBorder();
+            }
             else{
-                printLine();
-                printMessage(input);
-                printLine();
+                tasksLists.add(input);
+                printMessage("added: " + input);
+                printBorder();
             }
         }
     }
