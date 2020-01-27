6819cd8a9dd191e4b5454910efadb8c005fbd5e9
sliu107 <dldy0825@gmail.com>
2020-01-27 16:49:16 +0800

    add the ability to mark tasks as done with new data type Task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String[] tasks=new String[100];
+        Task[] tasks=new Task[100];
         int taskCountor=0;
         Scanner read=new Scanner(System.in);
         System.out.println("------------***------------");
@@ -14,17 +14,30 @@ public class Duke {
             System.out.println("------------***------------");
             switch(command) {
             case "list":
+                System.out.println("Here are the tasks in your list:");
                 for(int i=0;i<100;i++){
                     if(tasks[i]==null){
                         break;
                     }
-                    System.out.println((i+1)+": "+tasks[i]);
+                    System.out.println((i+1)+": ["+tasks[i].getStatusIcon()+"] "+tasks[i].description);
                 }
                 break;
             default:
-                System.out.println("added: "+command);
-                tasks[taskCountor]=command;
-                taskCountor++;
+                if(command.matches("done \\d")){
+                    String[] splitCommand=command.split(" ");
+                    int taskIndex=Integer.parseInt(splitCommand[1])-1;
+                    if(taskIndex<taskCountor) {
+                        tasks[taskIndex].markAsDone();
+                        System.out.println("Nice! I've marked this task as done:");
+                        System.out.println("  ["+tasks[taskIndex].getStatusIcon() + "] " + tasks[taskIndex].description);
+                    }else{
+                        System.out.println("There is no task No."+(taskIndex+1));
+                    }
+                }else{
+                    System.out.println("added: "+command);
+                    tasks[taskCountor]=new Task(command);
+                    taskCountor++;
+                }
             }
             System.out.println("------------***------------");
             System.out.println("Do you have any other commands? ");
