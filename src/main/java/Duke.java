f5a5d1ae4181b2cb0b060e162dbb005d3507ec63
weiyang <piadruids@gmail.com>
2020-01-27 18:30:08 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,32 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
     private static String lines = "____________________________________________________________\n";
+    private static Scanner in = new Scanner(System.in);
 
+    private static void botResponse(String userInput) {
+        ArrayList<String> userList = new ArrayList<String>();
+
+        while (!userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                System.out.print(lines);
+                if (userList.size() == 0) {
+                    System.out.println("No items added!");
+                }
+                for (int i=0; i<userList.size(); i++) {
+                    Integer index = i+1;
+                    System.out.println(index + ". " + userList.get(i));
+                }
+                System.out.print(lines);
+            } else {
+                userList.add(userInput);
+                System.out.print(lines + "added: " + userInput + "\n" + lines);
+            }
+            userInput = in.nextLine();
+        }
+        System.out.print(lines + "Bye. Hope to see you again soon!\n" + lines);
+    }
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
@@ -12,13 +36,8 @@ public class Duke {
 
         String toprint = lines + "Hello! I'm Duke\n" + "What can I do for you?\n" + lines;
         System.out.print(toprint);
-        Scanner in = new Scanner(System.in);
         String userInput = in.nextLine();
-        while (!userInput.equals("bye")) {
-            System.out.print(lines + userInput + "\n" + lines);
-            userInput = in.nextLine();
-        }
-        System.out.print(lines + "Bye. Hope to see you again soon!\n" + lines);
+        botResponse(userInput);
     }
 
 
