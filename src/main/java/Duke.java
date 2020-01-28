8b142f61b63d5bbda0509e1cd3344bc17f7986ce
DeetoMok <darylmok1@gmail.com>
2020-01-29 00:55:51 +0800

    added new Task class of updating toDo list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,18 +14,34 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println("----------------------------------------------");
         String userInput;
-        ArrayList<String> toDo = new ArrayList<>();
+        ArrayList<Task> toDo = new ArrayList<>();
         do {
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
-            if (userInput.equalsIgnoreCase("list")){
+            if (userInput.equalsIgnoreCase("list")){        //user checks list
                 System.out.println("----------------------------------------------");
+                System.out.println("Here are the tasks in your list");
                 for (int i=0; i<toDo.size(); i++){
-                    System.out.println(i+1 + ". " + toDo.get(i));
+                    System.out.println(i+1 + ".[" + toDo.get(i).getStatusIcon() + "] " + toDo.get(i).toString());
                 }
                 System.out.println("----------------------------------------------");
-            }else {
-                toDo.add(userInput);
+
+            }else if (userInput.startsWith("done")){
+                int indexCompleted=Integer.parseInt(userInput.substring(5));
+                if (indexCompleted == 0){
+                    System.out.println("Error, please enter a valid integer");
+                    System.out.println("----------------------------------------------");
+                    continue;
+                }
+                toDo.get(indexCompleted-1).markAsDone();
+                System.out.println("----------------------------------------------");
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + toDo.get(indexCompleted-1).getStatusIcon() + "] " + toDo.get(indexCompleted-1).toString());
+                System.out.println("----------------------------------------------");
+            }else {                                                     //user adds into list
+//                toDo.add(userInput);
+                Task t = new Task(userInput);
+                toDo.add(t);
                 System.out.println("----------------------------------------------");
                 System.out.println("added: " + userInput);
                 System.out.println("----------------------------------------------");
