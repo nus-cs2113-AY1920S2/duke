2b74c2a44e7207c5c84f64a6ed1f19b3ff2dc1d7
BenjaminChan <42601850+benchan911@users.noreply.github.com>
2020-01-28 16:43:46 +0800

    Level 2. Add, List Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
@@ -11,11 +12,27 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
         String UserCommand = sc.nextLine();
         String ExitCommand = "Bye";
+        String StrArray[] = new String[100];
+        int StrArrayIndex = 0;
         while (!UserCommand.equals(ExitCommand)){
-            System.out.println(lineDivider + BotName + "\n  " + UserCommand + "\n" + lineDivider);
-            System.out.print("USER\n  ");
+            if (UserCommand.equals("List")|| UserCommand.equals("list")){
+                System.out.println(lineDivider + "\nHere's your list!");
+                for (int i = 0; i < StrArrayIndex; i++){
+                    System.out.println( (i+1) + ". " + StrArray[i]);
+                }
+                System.out.println(lineDivider);
+            } else {
+                StrArray[StrArrayIndex] = UserCommand;
+                StrArrayIndex++;
+                System.out.println(lineDivider + BotName + "\n  Added: " + UserCommand + "\n" + lineDivider);
+                System.out.print("USER\n  ");
+
+            }
             UserCommand = sc.nextLine();
+
         }
         System.out.println(lineDivider + " Bye. Hope to see you again soon!\n" + lineDivider);
     }
+
+
 }
