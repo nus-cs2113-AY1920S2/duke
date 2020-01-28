c2ab1a4974174d578d63f37b71424324d7dccebd
Yuxiang <yuxianglim1227@gmail.com>
2020-01-28 23:54:12 +0800

    Completed Level-1 and some personalisations of robot.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,13 +7,26 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
         System.out.println("Hello from\n" + logo);
-
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can i do for you?");
+        System.out.println("Hey there! My name is Duke and i will be your personal assistant.");
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("First of all, how may i address you?");
+        System.out.println("Please enter your username: ");
+        String userName = myScanner.nextLine();
+        System.out.println("Hi "+ userName + "!" + " What can i do for you?");
         System.out.println("____________________________________________________________");
-
+        while(true){
+            String functions = myScanner.nextLine();
+            if(functions.equals("bye")){
+                break;
+            }
+            else{
+                System.out.println("Running " + functions);
+                System.out.println("Please wait for a moment...");
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!\n");
         System.out.println("____________________________________________________________");
     }
