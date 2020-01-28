5e0fb3ee9187bc8da5de43e281919700a3a0bc9e
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2020-01-28 19:10:37 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,14 +3,33 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = "***John***";
-        String userInput;
+        String[] tasks = new String[100];
+        String userInput = "";
+        int count = 0;
+        boolean isFinished = false;
+
         System.out.println("Hey! I am " + logo);
         System.out.println("What would you like to do?");
 
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
-        while(!userInput.equals("bye")){
-            System.out.println(userInput);
+
+        while(!isFinished){
+            switch (userInput) {
+            case "bye":
+                isFinished = true;
+                break;
+            case "list":
+                for (int j = 0; j<count; j++) {
+                    System.out.println(j+1 + ". " + tasks[j]);
+                }
+                break;
+            default:
+                System.out.println("added: " + userInput);
+                tasks[count] = userInput;
+                count++;
+                break;
+            }
             userInput = in.nextLine();
         }
         System.out.println("Bye. Hope to see you soon!");
