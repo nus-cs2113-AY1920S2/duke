95705903d0c835d4d00d500b470a800266c6f46b
jiajuinphoon <phoonjiajuin@hotmail.com>
2020-01-28 13:57:10 +0800

    Small change for lvl 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
+        System.out.println("Hello ! I'm Duke\n");
+        System.out.println("What can I do for you ? \n");
+        System.out.println("--------------------------\n");
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("--------------------------\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+
     }
 }
