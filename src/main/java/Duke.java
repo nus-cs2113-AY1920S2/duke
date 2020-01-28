20eba9b8d7a594289eb04895b0766cfc33f0f3d0
zi-hui <e0324175@u.nus.edu>
2020-01-28 17:25:40 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,12 +13,22 @@ public class Duke {
         System.out.println("What can I do for you?\n");
 
         Scanner scan = new Scanner( System.in );
-
+        String[] lists = new String[101];
+        int count =0;
         while(true) {
             String userData = scan.nextLine();
-            System.out.println(userData);
             if(userData.toLowerCase().equals("bye")){
                 break;
+            }else if(userData.toLowerCase().equals("list")){
+                 for (int i = 1; i <= lists.length; i++) {
+                     if (lists[i-1] != null) {
+                         System.out.println(i + ". " + lists[i-1]);
+                     }
+                }
+            }else{
+                lists[count] = userData;
+                count +=1;
+                System.out.println("added:  " +userData);
             }
         }
         System.out.println("Bye. Hope to see you again soon!\n");
