0f45a03948bbbe05793005c4c9f708dbcc6bbe92
NizarMohd <mohdnizar97@gmail.com>
2020-01-28 13:28:51 +0800

    add Level 3 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        List<String> l1= new ArrayList<String>();
+        List<Task> l1= new ArrayList<Task>();
         String line = "\t__________________________________________________________";
         String banner="\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                 +"\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
@@ -32,15 +33,26 @@ public class Duke {
                 } else if(userIn.equals("list")) {
                     int i=0;
                     System.out.println(line);
+                    System.out.print("\tHere are the tasks in your list:\n");
                     for(i=0; i< l1.size(); i++){
                         int count =i+1;
-                        System.out.println("\t" +count+". "+l1.get(i));
+                        Task temp=l1.get(i);
+                        System.out.println("\t" +count+". " +"[" +temp.getStatus()+ "]"+ " " + temp.action);
                     }
                     System.out.println(line);
-                }else{
-
+                }else if(userIn.contains("done ")) {
+                    String[] temp=userIn.split(" ");
+                    int number= Integer.parseInt(temp[1])-1;
+                    Task tempTask=l1.get(number);
+                    tempTask.done();
                     System.out.println(line);
-                    l1.add(userIn);
+                    System.out.println("\tNice! I've marked this task as done:");
+                    System.out.println("\t ["+tempTask.getStatus()+"] "+ tempTask.action);
+                    System.out.println(line);
+                }else{
+                    Task task = new Task(userIn);
+                    System.out.println(line);
+                    l1.add(task);
                     System.out.println("\t" +"added: " + userIn);
                     System.out.println(line);
                 }
