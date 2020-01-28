9d7d45ef43199e683dea13cf21dc8d0c8cd70bf3
DeetoMok <darylmok1@gmail.com>
2020-01-29 00:03:12 +0800

    added list "toDo" (level 2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 package src.main.java;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -13,12 +14,22 @@ public class Duke {
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
         System.out.println("----------------------------------------------");
         String userInput;
+        ArrayList<String> toDo = new ArrayList<>();
         do {
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
-            System.out.println("----------------------------------------------");
-            System.out.println(userInput);
-            System.out.println("----------------------------------------------");
+            if (userInput.equalsIgnoreCase("list")){
+                System.out.println("----------------------------------------------");
+                for (int i=0; i<toDo.size(); i++){
+                    System.out.println(i+1 + ". " + toDo.get(i));
+                }
+                System.out.println("----------------------------------------------");
+            }else {
+                toDo.add(userInput);
+                System.out.println("----------------------------------------------");
+                System.out.println("added: " + userInput);
+                System.out.println("----------------------------------------------");
+            }
         } while (!userInput.equalsIgnoreCase("bye"));
 
         System.out.println("----------------------------------------------");
