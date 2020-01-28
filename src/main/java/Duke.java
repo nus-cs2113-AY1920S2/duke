833cd89912734d1444583602ffc4cd1e876d6bbe
Jeremy Cheung <z5142254@ad.unsw.edu.au>
2020-01-28 21:05:27 +1100

    add Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+
+    private static ArrayList<String> taskList = new ArrayList<String>();
+
     public static void main(String[] args) throws IOException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -13,6 +17,16 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         greeting();
         chat();
+
+
+    }
+
+    private static String readCommand() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = null;
+        System.out.println("Type something");
+        str = br.readLine().toLowerCase();
+        return str;
     }
 
     public static void greeting() {
@@ -23,30 +37,71 @@ public class Duke {
 
     }
 
-    public static void  chat() throws IOException {
+    public static void bye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+
+    }
+
+
+    public static void chat() throws IOException {
         System.out.println("____________________________________________________________");
         String str = readCommand();
         System.out.println("____________________________________________________________");
 
-        if(str.equals("bye")){
+        if (str.equals("bye")) {
+            bye();
+        } else if (str.equals("add task")) {
             System.out.println("____________________________________________________________");
-            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println("Please add no more than 100 tasks");
             System.out.println("____________________________________________________________");
-        }else {
+            addTaskScreen();
+        } else if (str.equals("list")) {
+            listTask();
+            chat();
+        } else {
             System.out.println(str);
             System.out.println("____________________________________________________________");
             chat();
-
         }
     }
 
-    private static String readCommand() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = null;
-        System.out.println("Type something");
-        str = br.readLine();
-        return str;
+
+    public static void addTaskScreen() throws IOException {
+        System.out.println("____________________________________________________________");
+        System.out.println("Please add tasks");
+        System.out.println("____________________________________________________________");
+        String newTask = readCommand();
+        if (newTask.equals("bye")) {
+            bye();
+        } else {
+            addTask(newTask);
+            addTaskScreen();
+        }
     }
+
+    public static void addTask(String newTask) {
+        if (newTask.equals("list")) {
+            listTask();
+        } else {
+            taskList.add(newTask);
+            System.out.println("added : " + newTask);
+        }
+    }
+
+    public static void listTask() {
+        System.out.println("____________________________________________________________");
+        int i = 0;
+        while (i < taskList.size()) {
+            int j = i + 1;
+            System.out.println(j + " ." + taskList.get(i));
+            i++;
+        }
+
+        System.out.println("____________________________________________________________");
+    }
+
 }
 
 
