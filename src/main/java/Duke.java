75dd2a2cef83220a527f02015ddafa1a35bd7e92
bennychanya <bennychanyongan@live.com.sg>
2020-01-28 23:17:10 +0800

    Added Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,5 +10,12 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
+        String userInput;
+
+        do {
+            Scanner scan = new Scanner(System.in);
+            userInput = scan.nextLine();
+            System.out.println(userInput);
+        } while (!userInput.equalsIgnoreCase("bye"));
     }
 }
