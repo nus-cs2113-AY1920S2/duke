4d98a05cbb5ecf619fb3c08d5b1a4c7d196c42b7
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2020-01-28 18:29:13 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,8 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = "***Morty***";
-        System.out.println("Hello from\n" + logo);
+        String logo = "***John***";
+        System.out.println("Hello! I'm " + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
