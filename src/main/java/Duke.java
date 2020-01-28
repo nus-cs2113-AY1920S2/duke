4d04b82f55dedadae79bc6ea36b29fd738cf3bb1
ISABELLA CHEONG <cheongisabella@gmail.com>
2020-01-28 22:58:50 +0800

    Commit Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,6 +7,18 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm Isabella" + System.lineSeparator() + "What can I do for you?" + System.lineSeparator() + "Bye. Hope to see you again soon!");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello! I'm Isabella" + System.lineSeparator() + "What can I do for you?" + System.lineSeparator());
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line + System.lineSeparator());
+            line = in.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
