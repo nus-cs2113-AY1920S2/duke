bfc9993b9036efa29b7539c46ab0a0cd9182134d
JustinnT <e0311076@u.nus.edu>
2020-01-28 10:03:59 +0800

    Add echoUntilBye class for Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,49 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void printIndentation() {
+        System.out.print("    ");
+    }
+
     public static void greets() {
+        printIndentation();
         System.out.println("Hello! I'm Duke");
+        printIndentation();
         System.out.println("What can I do for you?");
     }
+
     public static void printLines() {
+        printIndentation();
         System.out.println("---------------------------------------");
     }
+
     public static void exits() {
+        printIndentation();
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static String readFromUser() {
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+        return userInput;
+    }
+
+    public static void echoUntilBye() {
+        while (true) {
+            String text = readFromUser();
+            if (text.equals("bye")) {
+                break;
+            }
+            printLines();
+            printIndentation();
+            System.out.println(text);
+            printLines();
+        }
+        exits();
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -19,7 +54,7 @@ public class Duke {
         printLines();
         greets();
         printLines();
-        exits();
+        echoUntilBye();
         printLines();
     }
 }
