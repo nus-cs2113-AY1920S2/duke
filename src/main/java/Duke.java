6c5551baf8dc39b9fbc879881caee80a61333d68
btricec <btricec@users.noreply.github.com>
2020-01-29 07:40:29 +0800

    Add increment -- Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -11,6 +13,16 @@ public class Duke {
                         " Hello! I'm Duke\n" +
                         " What can I do for you?\n" +
                 "____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        System.out.println("  " + inputString + "\n" +
+                "____________________________________________________________");
+        while(!inputString.equals("bye bye")) {
+            inputString = scanner.nextLine();
+            System.out.println("  " + inputString + "\n" +
+                    "____________________________________________________________");
+        }
         System.out.println(" Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________");
     }
