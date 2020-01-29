21c6f434463374b80dabe65def5459e12385df23
btricec <btricec@users.noreply.github.com>
2020-01-29 10:49:55 +0800

    Add increment -- Mark as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,21 +15,26 @@ public class Duke {
                         " What can I do for you?\n" +
                 "____________________________________________________________");
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 0;
 
         Scanner scanner = new Scanner(System.in);
         String inputString = "";
         while(!inputString.equals("bye bye")) {
             inputString = scanner.nextLine();
-            if(inputString.equals("list")){
+            Task new_task = new Task(inputString);
+
+            if(inputString.equals("list")) {
                 for(int i = 0; i < index; i++) {
-                    System.out.print(i+1);
-                    System.out.println(". " + list[i]);
+                    System.out.println(String.valueOf(i+1) + ". " + list[i].getStatusIcon() + " " + list[i].description);
                 }
-            }
-            else {
-                list[index] = inputString;
+            } else if(inputString.indexOf("done") >= 0) {
+                int ind = Integer.parseInt((inputString.substring(5)));
+                System.out.println(ind);
+                list[ind-1].markAsDone();
+                System.out.println("done: " + list[ind-1].description);
+            } else {
+                list[index] = new_task;
                 index++;
                 System.out.println("  added: " + inputString + "\n" +
                         "____________________________________________________________");
