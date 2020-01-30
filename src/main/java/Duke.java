0ea94bafa9be0e7aac8d675ad0445d0124ae7e18
jiajuinphoon <phoonjiajuin@hotmail.com>
2020-01-30 14:07:16 +0800

    Level 2 completed

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,37 +1,53 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
 
-    //level 1 greet, echo, bye (function)
-    public static void echoCommand(String line)
-    {
-        String comparator = "bye";
-        System.out.println("--------------------------");
-
-        if(line.equals(comparator))
-        {
-            System.out.println("Bye. Hope to see you again soon!");
-            System.out.println("---------------------------");
-        }
-        else
-        {
-            System.out.println(line);
-            System.out.println("---------------------------");
-            String command;
-            Scanner in = new Scanner(System.in);
-            command = in.nextLine();
-            echoCommand(command);
-        }
-    }
     public static void main(String[] args) {
         System.out.println("Hello ! I'm Duke\n");
         System.out.println("What can I do for you ? \n");
 
-        //level 1 - greet, echo, exit
-        String line;
-        Scanner in = new Scanner(System.in);
-        line = in.nextLine();
-        echoCommand(line);
+        String command;
+        int i = 0;
+        String[] list = new String[100];
+        Scanner in = new Scanner (System.in);
+        command = in.nextLine();
+        String option_1 = "bye";
+        String option_2 = "list";
+
+        while (true)
+        {
+            if (command.equals(option_1))
+            {
+                System.out.println("------------------------");
+                System.out.println("Bye, see you again soon!");
+                System.out.println("------------------------");
+                System.exit(0);
+            }
+            else if (command.equals(option_2))
+            {
+                System.out.println("------------------------");
+
+                for (int j = 0; j < i; j++)
+                {
+                    System.out.print(j+1);
+                    System.out.print(". ");
+                    System.out.println(list[j]);
+                }
+
+                System.out.println("------------------------");
+                command = in.nextLine();
+            }
+            else
+            {
+                list[i] = command;
+                System.out.println("------------------------");
+                System.out.println("Added: " + command);
+                System.out.println("------------------------");
+                i++;
+                command = in.nextLine();
+            }
+        }
 
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -39,7 +55,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-
 
     }
 }
