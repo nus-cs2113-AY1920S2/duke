f3fd27baf0f2f852c85378625804205d74425b9d
matthewc97 <matthewchan@live.com.sg>
2020-01-30 16:54:05 +0800

    A-CodingStandard Add comments and refactor code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,63 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    public static final int MAX_TASK = 100;
+
     public static void main(String[] args) {
+        printWelcomeMessage();
+        boolean isExit = false;
+        Task[] list = new Task[MAX_TASK];
+        int itemCount = 0;
+        do {
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine(); // Get string input
+            if(input.equals("bye")) { // Exit
+                printExitMessage();
+                isExit = true;
+            } else if(input.equals("list")) { // List tasks
+                printBorder();
+                System.out.println("    Here are the tasks in your list:");
+                for(int i = 0; i < itemCount; ++i) { // Print list of tasks
+                    System.out.println("    " + (i + 1) + ". [" + list[i].getStatusIcon() + "] " + list[i].getTask() );
+                }
+                printBorder();
+            } else if(input.startsWith("done") ) { // Mark task as done
+                printBorder();
+                System.out.println("    Nice! I've marked this task as done: ");
+                String index = input.substring(5, input.length() );
+                int taskIndex = Integer.parseInt(index) - 1;
+                list[taskIndex].setDone(true);
+                System.out.println("    " + " [" + list[taskIndex].getStatusIcon() + "] " + list[taskIndex].getTask() ); // Print task marked as done
+                printBorder();
+            }
+            else { // Add task
+                printBorder();
+                list[itemCount] = new Task(input);
+                itemCount++;
+                System.out.println("    added: " + input); // Print task added
+                printBorder();
+            }
+        } while (isExit == false);
+    }
+
+    private static void printBorder() {
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void printExitMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(" _                \n"
+                + "| |               \n"
+                + "| |__  _   _  ___ \n"
+                + "| '_ \\| | | |/ _ \\\n"
+                + "| |_) | |_| |  __/\n"
+                + "|_.__/ \\__, |\\___|\n"
+                + "        __/ |     \n"
+                + "       |___/      \n");
+    }
+
+    private static void printWelcomeMessage() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,46 +65,5 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        boolean isExit = false;
-        Task[] list = new Task[100];
-        int itemCount = 0;
-        do {
-            Scanner in = new Scanner(System.in);
-            String input = in.nextLine();
-            if(input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(" _                \n"
-                        + "| |               \n"
-                        + "| |__  _   _  ___ \n"
-                        + "| '_ \\| | | |/ _ \\\n"
-                        + "| |_) | |_| |  __/\n"
-                        + "|_.__/ \\__, |\\___|\n"
-                        + "        __/ |     \n"
-                        + "       |___/      \n");
-                isExit = true;
-            } else if(input.equals("list")) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    Here are the tasks in your list:");
-                for(int i = 0; i < itemCount; ++i) {
-                    System.out.println("    " + (i + 1) + ". [" + list[i].getStatusIcon() + "] " + list[i].getTask() );
-                }
-                System.out.println("    ____________________________________________________________");
-            } else if(input.startsWith("done") ) {
-                System.out.println("    ____________________________________________________________");
-                System.out.println("    Nice! I've marked this task as done: ");
-                String index = input.substring(5, input.length() );
-                int taskIndex = Integer.parseInt(index) - 1;
-                list[taskIndex].setDone(true);
-                System.out.println("    " + " [" + list[taskIndex].getStatusIcon() + "] " + list[taskIndex].getTask() );
-                System.out.println("    ____________________________________________________________");
-            }
-            else {
-                System.out.println("    ____________________________________________________________");
-                list[itemCount] = new Task(input);
-                itemCount++;
-                System.out.println("    added: " + input);
-                System.out.println("    ____________________________________________________________");
-            }
-        } while (isExit == false);
     }
 }
