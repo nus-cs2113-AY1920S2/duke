88d53b05029a79b3a4b7cc621dd0203074940e41
JWang2000 <JTWang2000@users.noreply.github.com>
2020-01-30 17:03:17 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        display adisplay = new display();
+
+        String responde;
+        Scanner in = new Scanner(System.in);
+        do {
+            responde = in.nextLine();
+            adisplay.reply(responde);
+
+        }while(!responde.equals("bye"));
+
     }
 }
