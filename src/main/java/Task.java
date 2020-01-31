3c77b1c137570b261fd5168907b64a4adee711e6
jichngan <jcngan97@gmail.com>
2020-01-31 15:23:08 +0800

    Add Deadline and Event class extending from Todo class.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,12 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected static int totalTasks = 0;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        totalTasks++;
     }
 
     public String getStatusIcon() {
