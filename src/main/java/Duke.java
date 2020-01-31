58959609ad1d96539e81f59395098e57b5ea62f3
lamyuewei <lamyuewei1997@hotmail.com>
2020-01-31 18:15:13 +0800

    Completed Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,17 +2,27 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String[] lines = new String[100];
+        int count = 0;
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
         System.out.println("    ____________________________________________________________");
         Scanner sc = new Scanner(System.in);
-        String string = sc.next();
+        String string = sc.nextLine();
         while (!string.equals("bye")) {
             System.out.println("    ____________________________________________________________");
-            System.out.println("    " + string);
+            if (!string.equals("list")) {
+                lines[count] = string;
+                count++;
+                System.out.println("     added: " + string);
+            } else {
+                for (int i = 0; i < count; i++) {
+                    System.out.println("     " + (i + 1) + ". " + lines[i]);
+                }
+            }
             System.out.println("    ____________________________________________________________");
-            string = sc.next();
+            string = sc.nextLine();
         }
         System.out.println("    ____________________________________________________________");
         System.out.println("    Bye. Hope to see you again soon!");
