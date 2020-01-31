089cc001f49354c2b6f3ee669d95524da2ff2a85
quinnyyy <quinnyyy@bu.edu>
2020-01-31 09:57:06 +0800

    Parse user input and add simple error checking

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -7,6 +7,9 @@ public class TaskManager {
         tasks = new ArrayList<Task>();
     }
 
+    // Something to consider in the future is to put a from string constructor in tasks classes
+    // Then move validation into each class
+
     public void executeUserInput(String userInput) {
         // Get the first word in the userInput
         String command;
@@ -16,25 +19,72 @@ public class TaskManager {
             command = userInput;
         }
 
+        if (!validateInput(command, userInput)) {
+            Ui.printPretty("Sorry, couldn't recognize this input format");
+            return;
+        }
+
+        String description;
         switch(command) {
         case "list":
             listTasks();
             break;
         case "todo":
-            ToDo newTodo = new ToDo("this is a todo");
+            description = userInput.substring(userInput.indexOf(" ") + 1);
+            ToDo newTodo = new ToDo(description);
             addTask(newTodo);
             break;
         case "deadline":
-            Deadline newDeadline = new Deadline("this is a deadline", "this is a due time");
+            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf(" /by "));
+            String dueDateTime = userInput.substring(userInput.indexOf(" /by ") + 5);
+            Deadline newDeadline = new Deadline(description, dueDateTime);
             addTask(newDeadline);
             break;
         case "event":
-            Event newEvent = new Event("this is an event", "this is an event time");
+            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf(" /at "));
+            String startEndDateTime = userInput.substring(userInput.indexOf(" /at ") + 5);
+            Event newEvent = new Event(description, startEndDateTime);
             addTask(newEvent);
             break;
         default:
-            Ui.printPretty("Sorry, couldn't recognize this input format");
+            Ui.printPretty("Sorry, couldn't recognize this input format"); // Shouldn't reach here
         }
+    }
+
+    protected boolean validateInput(String command, String userInput) {
+        if (!command.equals("list") && !command.equals("todo") && !command.equals("deadline") &&
+                !command.equals("event")) {
+            return false;
+        }
+
+        if (command.equals("todo") || command.equals("deadline") || command.equals("event")) {
+            if (!userInput.contains(" ")) {
+                return false;
+            }
+        }
+
+        if (command.equals("deadline")) {
+            if (!userInput.contains(" /by ")) {
+                return false;
+            }
+
+            // Ensure there is a by time
+            if (userInput.indexOf(" /by ") + 5 > userInput.length() - 1) {
+                return false;
+            }
+        }
+
+        if (command.equals("event")) {
+            if (!userInput.contains(" /at ")) {
+                return false;
+            }
+
+            if (userInput.indexOf(" /at ") + 5 > userInput.length() - 1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     protected void addTask(Task newTask) {
