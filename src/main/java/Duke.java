ce2126ede96a351c5d02b00b2f11da180f36085b
lamyuewei <lamyuewei1997@hotmail.com>
2020-01-31 20:49:13 +0800

    Completed Level 3. Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String[] lines = new String[100];
-        int count = 0;
+        boolean[] isDone = new boolean[100];
+        int count = 1;
         System.out.println("    ____________________________________________________________");
         System.out.println("    Hello! I'm Duke");
         System.out.println("    What can I do for you?");
@@ -12,14 +13,23 @@ public class Duke {
         String string = sc.nextLine();
         while (!string.equals("bye")) {
             System.out.println("    ____________________________________________________________");
-            if (!string.equals("list")) {
+            String[] stringSplit = string.split(" ");
+            if (string.equals("list")) {
+                System.out.println("     Here are the tasks in your list:");
+                for (int i = 1; i < count; i++) {
+                    System.out.print("     " + (i) + ".[");
+                    System.out.print(isDone[i] ? "✓" : "✗");
+                    System.out.println("] " + lines[i]);
+                }
+            } else if (stringSplit[0].equals("done")) {
+                int done = Integer.parseInt(stringSplit[1]);
+                isDone[done] = true;
+                System.out.println("     Nice! I've marked this task as done: ");
+                System.out.println("       [✓] " + lines[done]);
+            } else {
                 lines[count] = string;
                 count++;
                 System.out.println("     added: " + string);
-            } else {
-                for (int i = 0; i < count; i++) {
-                    System.out.println("     " + (i + 1) + ". " + lines[i]);
-                }
             }
             System.out.println("    ____________________________________________________________");
             string = sc.nextLine();
