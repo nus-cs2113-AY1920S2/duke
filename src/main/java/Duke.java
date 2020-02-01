08a67e19231eb9b3314b0808d164f5327520aefa
Shannonwje <e0325213@u.nus.edu>
2020-02-02 02:46:06 +0800

    Added Increment: Level 3: Mark as Done, added task.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,25 +6,36 @@ public class Duke {
         System.out.println("Hello from " + logo);
 
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke" + System.lineSeparator() + " What can I do for you?");
+        System.out.println(" Hello! I'm Shannon" + System.lineSeparator() + " What can I do for you?");
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
         String inputCommand = scanner.nextLine();
-        String[] listOfItems = new String[100];
+        Task[] listOfTasks = new Task[100];
         int sizeOfList = 0;
         while (inputCommand.equals("bye") == false) {
 
             if (inputCommand.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println(" Here are the tasks in your list:");
                 for (int i = 0; i < sizeOfList; i++) {
                     int k = i + 1;
-                    System.out.println(" " + k + ". " + listOfItems[i]);
+                    System.out.println(" " + k + ".[" + listOfTasks[i].getStatusIcon() + "] " + listOfTasks[i].description);
                 }
                 System.out.println("____________________________________________________________");
 
+            } else if (inputCommand.contains("done ") == true) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Nice! I've marked this task as done:");
+                String indexInString = inputCommand.substring(5);
+                int index = Integer.parseInt(indexInString) - 1;
+                listOfTasks[index].markAsDone();
+                System.out.println("   [" + listOfTasks[index].getStatusIcon() + "] " + listOfTasks[index].description);
+                System.out.println("____________________________________________________________");
+
             } else {
-                listOfItems[sizeOfList] = inputCommand;
+                Task newTask = new Task(inputCommand);
+                listOfTasks[sizeOfList] = newTask;
                 System.out.println("____________________________________________________________");
                 System.out.println(" added: " + inputCommand);
                 System.out.println("____________________________________________________________");
