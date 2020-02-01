a878bfcfef8242ac61124916ab14c179e2254089
jinfayap <jinfayap@gmail.com>
2020-02-01 22:13:15 +0800

    Add Todo Class

---
+++ src/main/java/Todo.java
@@ -0,0 +1,14 @@
+public class Todo extends Task {
+
+    private final String TAG = "[T]";
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return TAG + super.toString();
+    }
+    
+}
