278a7bbb14db05471f08879248dddadf6f35ca26
harithadiv <haritha.divakar@gmail.com>
2020-02-01 13:10:20 +0800

    Mark as done to tasksList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,10 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Duke {
     private static final String BORDER = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ";
     private static final String SPACE = "  ";
-    private static final List<String> tasksLists = new ArrayList<>(100);
+    private static final List<Task> tasksList = new ArrayList<>(100);
 
     private static void printBorder(){
         System.out.println(BORDER);
@@ -29,20 +30,52 @@ public class Duke {
             if(input.equals("bye")) {
                 printMessage("Bye. Hope to see you again soon!");
                 printBorder();
-                return;
+                break;
             }
-            if(input.equals("list")){
-                for (int i = 0; i < tasksLists.size(); i++ ){
-                    printMessage((i+1) + ". " + tasksLists.get(i));
+            else if(input.equals("list")){
+                if (tasksList.size() == 0){
+                    printMessage("There are no tasks in your listlis!");
                 }
-                printBorder();
+                else {
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < tasksList.size(); i++) {
+                        printMessage((i + 1) + "." + "[" + tasksList.get(i).getStatusIcon() + "] " + tasksList.get(i).getDescription());
+                    }
+                }
             }
             else{
-                tasksLists.add(input);
-                printMessage("added: " + input);
-                printBorder();
+                String[] completedTask = input.split(" ");
+                if (completedTask.length == 2 && completedTask[0].equals("done") && isNumeric(completedTask[1])){
+                    int i = Integer.parseInt(completedTask[1]);
+                    if (i > 0 && i <= tasksList.size()){
+                        System.out.println("Nice! I've marked this task as done");
+                        tasksList.get(i-1).markAsDone();
+                        System.out.println(i + "." + "[" + "\u2713" + "] " + tasksList.get(i-1).getDescription());
+                    }
+                    else {
+                        printMessage("Task not found!");
+                    }
+                }
+                else{
+                    Task t = new Task(input);
+                    tasksList.add(t);
+                    printMessage("Added: " + input);
+                }
             }
+            printBorder();
         }
+    }
+
+    public static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
