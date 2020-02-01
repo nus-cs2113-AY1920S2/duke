25f96f2a182e02e32ef395ad3c68c3876c67a285
jinfayap <jinfayap@gmail.com>
2020-02-01 22:13:03 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,20 @@
+public class Deadline extends Task{
+    
+    private String by;
+    private final String TAG = "[D]";
+    private final String OPEN_BRACKET = "(";
+    private final String CLOSE_BRACKET = ")";
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = OPEN_BRACKET + by + CLOSE_BRACKET;
+        this.by = stringBuilder(this.by);
+    }
+    
+    @Override
+    public String toString() {
+        return TAG + super.toString() + by;
+    }
+    
+
+}
