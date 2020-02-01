8a1d643318d849820c61b0aca259d2733e08da4d
wang qin <347656409nick@gmail.com>
2020-02-01 16:46:32 +0800

    Add Date Parse Only for Deadline Tasks [Level 8 Minimal]

--- src/main/java/commands/add/AddDeadlineCommand.java
+++ src/main/java/commands/add/AddDeadlineCommand.java
@@ -1,6 +1,7 @@
 package commands.add;
 
 import commands.CommandResult;
+import common.Month;
 import data.task.DeadlineTask;
 import data.task.Task;
 
@@ -20,11 +21,25 @@ public class AddDeadlineCommand extends AddCommand {
     @Override
     public CommandResult execute() {
         duke.addTask(deadlineTask);
-        return new CommandResult(String.format(
-                MESSAGE_DEADLINE_SUCCESS,
-                COMMAND_TYPE,
-                deadlineTask.getChar(),
-                deadlineTask.getTaskDescription(),
-                deadlineTask.getTaskDeadline()));
+        //according to the data format
+        if (deadlineTask.getTaskDeadlineDate()!=null){
+            String timeString = String.format("%s %s %s",
+                    Month.valueOf(deadlineTask.getTaskDeadlineDate().getMonthValue()),
+                    deadlineTask.getTaskDeadlineDate().getDayOfMonth(),
+                    deadlineTask.getTaskDeadlineDate().getYear());
+            return new CommandResult(String.format(
+                    MESSAGE_DEADLINE_SUCCESS,
+                    COMMAND_TYPE,
+                    deadlineTask.getChar(),
+                    deadlineTask.getTaskDescription(),
+                    timeString));
+        } else {
+            return new CommandResult(String.format(
+                    MESSAGE_DEADLINE_SUCCESS,
+                    COMMAND_TYPE,
+                    deadlineTask.getChar(),
+                    deadlineTask.getTaskDescription(),
+                    deadlineTask.getTaskDeadline()));
+        }
     }
 }
--- src/main/java/data/task/DeadlineTask.java
+++ src/main/java/data/task/DeadlineTask.java
@@ -1,8 +1,12 @@
 package data.task;
 
+import parser.ParseTime;
+import java.time.LocalDate;
+
 public class DeadlineTask extends Task{
 
     private String taskDeadline;
+    private LocalDate taskDeadlineDate;
 
     public DeadlineTask() {
     }
@@ -10,9 +14,14 @@ public class DeadlineTask extends Task{
     public DeadlineTask(String taskDescription, String taskDeadline) {
         super(taskDescription, 'D');
         this.taskDeadline = taskDeadline;
+        taskDeadlineDate = ParseTime.parseStringToLocalTime(this.taskDeadline);
     }
 
     public String getTaskDeadline() {
         return taskDeadline;
+    }
+
+    public LocalDate getTaskDeadlineDate() {
+        return taskDeadlineDate;
     }
 }
