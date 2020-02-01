d3ad071e003c45e773decf5f17f13e5e2ae60d2f
Shannonwje <e0325213@u.nus.edu>
2020-02-01 21:31:18 +0800

    Add Increment: Level 1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "Shannon";
+        System.out.println("Hello from " + logo);
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm Duke" + System.lineSeparator() + " What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+        String inputCommand = scanner.nextLine();
+        while (inputCommand.equals("bye") == false) {
+            System.out.println("____________________________________________________________");
+            System.out.println(inputCommand);
+            System.out.println("____________________________________________________________");
+            inputCommand = scanner.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
