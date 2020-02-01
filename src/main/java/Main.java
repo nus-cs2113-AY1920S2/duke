916e5309d849b6a72ab26c552c4f0a68171bf39b
Joseph Lim <limweijie@live.com.sg>
2020-02-02 00:52:22 +0800

    Add DukeException to handle incorrect inputs.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
+    /*
     public static void readInput(Scanner sc, Duke duke) {
         while(sc.hasNextLine()) {
             String input = sc.nextLine();
@@ -35,13 +36,21 @@ public class Main {
             }
         }
     }
+    */
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Duke duke = new Duke();
-
         System.out.println(duke);
-        readInput(sc, duke);
+        
+        while (sc.hasNextLine()) {
+            try {
+                String input = sc.nextLine();
+                duke = duke.runCommand(input);
+            } catch (DukeException e) {
+                System.err.println(e);
+            }
+        }
 
         sc.close();
     }
