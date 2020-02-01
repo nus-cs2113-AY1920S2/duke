0bc5b07ddd3b67e6f4bc58c376d0933c2e3d7ff7
Zhilin <huangzhilin1999@gmail.com>
2020-02-01 15:44:54 +0800

    Change if-else to switch-case statement

--- src/main/java/ChattyChatBot.java
+++ src/main/java/ChattyChatBot.java
@@ -9,7 +9,7 @@ public class ChattyChatBot {
 
     private static final String BYE_STRING = "bye";
     private static final String LIST_STRING = "list";
-    private static final String DONE_STRING_REGEX = "done [0-9]+";
+    private static final String DONE_STRING = "done";
     private static final String ADDED_PREFIX = "added: ";
 
     private static final String SPACE_SEPARATOR = " ";
@@ -22,19 +22,27 @@ public class ChattyChatBot {
 
         List<Task> tasks = new ArrayList<>();
 
-        while (true) {
-            String userInput = SCANNER.nextLine();
-            if (userInput.equals(BYE_STRING)) {
-                sendByeMessage();
-                break;
-            } else if (userInput.equals(LIST_STRING)) {
+        String userInput = SCANNER.nextLine();
+        do {
+            // Reference: https://stackoverflow.com/questions/5067942/what-is-the-best-way-to-extract-the-first-word
+            // -from-a-string-in-java
+            String[] array = userInput.split(SPACE_SEPARATOR, 2);
+            String action = array[0];
+
+            switch (action) {
+            case LIST_STRING:
                 listAllTasks(tasks);
-            } else if (userInput.matches(DONE_STRING_REGEX)) {
-                markTaskAsDone(tasks, userInput);
-            } else {
+                break;
+            case DONE_STRING:
+                markTaskAsDone(tasks, array[1]);
+                break;
+            default:
                 addTask(tasks, userInput);
             }
-        }
+
+            userInput = SCANNER.nextLine();
+        } while (!userInput.equals(BYE_STRING));
+        sendByeMessage();
     }
 
     private static void listAllTasks(List<Task> tasks) {
@@ -45,8 +53,8 @@ public class ChattyChatBot {
         }
     }
 
-    private static void markTaskAsDone(List<Task> tasks, String userInput) {
-        int taskIdx = Integer.parseInt(userInput.split(SPACE_SEPARATOR)[1]);
+    private static void markTaskAsDone(List<Task> tasks, String indexStr) {
+        int taskIdx = Integer.parseInt(indexStr);
         Task task = tasks.get(taskIdx - 1);
         task.markAsDone();
         System.out.println(LEFT_SQUARE_BRACKET + task.getStatusIcon() + RIGHT_SQUARE_BRACKET + SPACE_SEPARATOR +
