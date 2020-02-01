4a839d57c748106dd7c8c147f8bb77c80891b63f
Zhilin <huangzhilin1999@gmail.com>
2020-02-01 15:24:36 +0800

    Extract methods and constants for readability

--- src/main/java/ChattyChatBot.java
+++ src/main/java/ChattyChatBot.java
@@ -5,31 +5,66 @@ import java.util.Scanner;
 public class ChattyChatBot {
     private static final Scanner SCANNER = new Scanner(System.in);
 
+    private static final String BOT_NAME = "Chatty Chat Bot";
+
+    private static final String BYE_STRING = "bye";
+    private static final String LIST_STRING = "list";
+    private static final String DONE_STRING_REGEX = "done [0-9]+";
+    private static final String ADDED_PREFIX = "added: ";
+
+    private static final String SPACE_SEPARATOR = " ";
+    private static final String LEFT_SQUARE_BRACKET = "[";
+    private static final String RIGHT_SQUARE_BRACKET = "]";
+    private static final String DOT_CHARACTER = ".";
+
     public static void main(String[] args) {
-        String name = "Chatty Chat Bot";
-        System.out.println("Hello from " + name);
+        sendWelcomeMessage();
 
         List<Task> tasks = new ArrayList<>();
 
         while (true) {
             String userInput = SCANNER.nextLine();
-            if (userInput.equals("bye")) {
-                System.out.println("Thanks for chatting with me! Bye~");
+            if (userInput.equals(BYE_STRING)) {
+                sendByeMessage();
                 break;
-            } else if (userInput.equals("list")) {
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task task = tasks.get(i);
-                    System.out.println((i + 1) + ". [" + task.getStatusIcon() + "] " + task.getDescription());
-                }
-            } else if (userInput.matches("done [0-9]+")) {
-                int taskIdx = Integer.parseInt(userInput.split(" ")[1]);
-                Task task = tasks.get(taskIdx - 1);
-                task.markAsDone();
-                System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
+            } else if (userInput.equals(LIST_STRING)) {
+                listAllTasks(tasks);
+            } else if (userInput.matches(DONE_STRING_REGEX)) {
+                markTaskAsDone(tasks, userInput);
             } else {
-                tasks.add(new Task(userInput));
-                System.out.println("added: " + userInput);
+                addTask(tasks, userInput);
             }
         }
+    }
+
+    private static void listAllTasks(List<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            System.out.println((i + 1) + DOT_CHARACTER + SPACE_SEPARATOR + LEFT_SQUARE_BRACKET + task.getStatusIcon() +
+                    RIGHT_SQUARE_BRACKET + SPACE_SEPARATOR + task.getDescription());
+        }
+    }
+
+    private static void markTaskAsDone(List<Task> tasks, String userInput) {
+        int taskIdx = Integer.parseInt(userInput.split(SPACE_SEPARATOR)[1]);
+        Task task = tasks.get(taskIdx - 1);
+        task.markAsDone();
+        System.out.println(LEFT_SQUARE_BRACKET + task.getStatusIcon() + RIGHT_SQUARE_BRACKET + SPACE_SEPARATOR +
+                task.getDescription());
+    }
+
+    private static void addTask(List<Task> tasks, String userInput) {
+        tasks.add(new Task(userInput));
+        System.out.println(ADDED_PREFIX + userInput);
+    }
+
+    private static void sendWelcomeMessage() {
+        System.out.println("Hello from " + BOT_NAME);
+        System.out.println("Glad to be at your service!");
+    }
+
+    private static void sendByeMessage() {
+        System.out.println("Thanks for chatting with " + BOT_NAME);
+        System.out.println("See you again soon!");
     }
 }
