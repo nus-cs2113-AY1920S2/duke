9ecb6949651781c677186e9b37f45140fb9b1346
Shannonwje <e0325213@u.nus.edu>
2020-02-01 11:36:38 +0800

    Revert "Level 1 of Duke: Greet, Echo, Exit"
    This reverts commit 53e7279d3576922e75ceaf4af4bd9ea4a1c5e623.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,17 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
-        String logo = "Bob";
-        System.out.println("Hello from " + logo);
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
 
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Bob");
-        System.out.println(" What can I do for you?");
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-
-
-        Scanner scanner = new Scanner(System. in);
-        String inputCommand = scanner.nextLine();
-        while (inputCommand.equals("bye") == false) {
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + inputCommand);
-            System.out.println("____________________________________________________________");
-            inputCommand = scanner.nextLine();
-        }
-
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
