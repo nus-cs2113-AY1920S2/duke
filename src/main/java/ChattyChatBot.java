dd7202943fdf5bc79c9ea9802992c76c3e0f1c22
Zhilin <huangzhilin1999@gmail.com>
2020-02-01 16:49:49 +0800

    Add ToDos, Events, Deadlines tasks

--- src/main/java/ChattyChatBot.java
+++ src/main/java/ChattyChatBot.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ChattyChatBot {
+
     private static final Scanner SCANNER = new Scanner(System.in);
 
     private static final String BOT_NAME = "Chatty Chat Bot";
@@ -10,12 +11,20 @@ public class ChattyChatBot {
     private static final String BYE_STRING = "bye";
     private static final String LIST_STRING = "list";
     private static final String DONE_STRING = "done";
-    private static final String ADDED_PREFIX = "added: ";
+    private static final String TODO_STRING = "todo";
+    private static final String DEADLINE_STRING = "deadline";
+    private static final String EVENT_STRING = "event";
 
     private static final String SPACE_SEPARATOR = " ";
-    private static final String LEFT_SQUARE_BRACKET = "[";
-    private static final String RIGHT_SQUARE_BRACKET = "]";
     private static final String DOT_CHARACTER = ".";
+    private static final String BY_STRING = "/by";
+    private static final String AT_STRING = "/at";
+
+    private static final String LINE_BREAK = "____________________________________________________________";
+
+    private static final String ADDED_TASK_CONFIRMATION = "Got it. I've added this task: ";
+    private static final String TASK_SUMMARY_FIRST_HALF = "Now you have ";
+    private static final String TASK_SUMMARY_SECOND_HALF = " tasks in the list.";
 
     public static void main(String[] args) {
         sendWelcomeMessage();
@@ -24,8 +33,10 @@ public class ChattyChatBot {
 
         String userInput = SCANNER.nextLine();
         do {
-            // Reference: https://stackoverflow.com/questions/5067942/what-is-the-best-way-to-extract-the-first-word
-            // -from-a-string-in-java
+            System.out.println(LINE_BREAK);
+
+            // Solution below adapted from: https://stackoverflow
+            // .com/questions/5067942/what-is-the-best-way-to-extract-the-first-word-from-a-string-in-java
             String[] array = userInput.split(SPACE_SEPARATOR, 2);
             String action = array[0];
 
@@ -36,20 +47,30 @@ public class ChattyChatBot {
             case DONE_STRING:
                 markTaskAsDone(tasks, array[1]);
                 break;
+            case TODO_STRING:
+                addToDoTask(tasks, array[1]);
+                break;
+            case DEADLINE_STRING:
+                addDeadlineTask(tasks, array[1]);
+                break;
+            case EVENT_STRING:
+                addEventTask(tasks, array[1]);
+                break;
             default:
-                addTask(tasks, userInput);
+                sendDefaultResponse();
             }
 
+            System.out.println(LINE_BREAK);
             userInput = SCANNER.nextLine();
         } while (!userInput.equals(BYE_STRING));
         sendByeMessage();
     }
 
     private static void listAllTasks(List<Task> tasks) {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             Task task = tasks.get(i);
-            System.out.println((i + 1) + DOT_CHARACTER + SPACE_SEPARATOR + LEFT_SQUARE_BRACKET + task.getStatusIcon() +
-                    RIGHT_SQUARE_BRACKET + SPACE_SEPARATOR + task.getDescription());
+            System.out.println((i + 1) + DOT_CHARACTER + SPACE_SEPARATOR + task.toString());
         }
     }
 
@@ -57,22 +78,54 @@ public class ChattyChatBot {
         int taskIdx = Integer.parseInt(indexStr);
         Task task = tasks.get(taskIdx - 1);
         task.markAsDone();
-        System.out.println(LEFT_SQUARE_BRACKET + task.getStatusIcon() + RIGHT_SQUARE_BRACKET + SPACE_SEPARATOR +
-                task.getDescription());
+        System.out.println(task.toString());
     }
 
-    private static void addTask(List<Task> tasks, String userInput) {
-        tasks.add(new Task(userInput));
-        System.out.println(ADDED_PREFIX + userInput);
+    private static void addToDoTask(List<Task> tasks, String description) {
+        ToDo newToDoTask = new ToDo(description);
+        tasks.add(newToDoTask);
+
+        System.out.println(ADDED_TASK_CONFIRMATION);
+        System.out.println(newToDoTask);
+        System.out.println(TASK_SUMMARY_FIRST_HALF + tasks.size() + TASK_SUMMARY_SECOND_HALF);
+    }
+
+    private static void addDeadlineTask(List<Task> tasks, String inputStr) {
+        String[] array = inputStr.split(BY_STRING);
+        Deadline newDeadlineTask = new  Deadline(array[0], array[1]);
+        tasks.add(newDeadlineTask);
+
+        System.out.println(ADDED_TASK_CONFIRMATION);
+        System.out.println(newDeadlineTask);
+        System.out.println(TASK_SUMMARY_FIRST_HALF + tasks.size() + TASK_SUMMARY_SECOND_HALF);
+    }
+
+    private static void addEventTask(List<Task> tasks, String inputStr) {
+        String[] array = inputStr.split(AT_STRING);
+        Event newEventTask = new Event(array[0], array[1]);
+        tasks.add(newEventTask);
+
+        System.out.println(ADDED_TASK_CONFIRMATION);
+        System.out.println(newEventTask);
+        System.out.println(TASK_SUMMARY_FIRST_HALF + tasks.size() + TASK_SUMMARY_SECOND_HALF);
+    }
+
+    private static void sendDefaultResponse() {
+        System.out.println("I'm " + BOT_NAME);
+        System.out.println("How may I help you?");
     }
 
     private static void sendWelcomeMessage() {
+        System.out.println(LINE_BREAK);
         System.out.println("Hello from " + BOT_NAME);
         System.out.println("Glad to be at your service!");
+        System.out.println(LINE_BREAK);
     }
 
     private static void sendByeMessage() {
+        System.out.println(LINE_BREAK);
         System.out.println("Thanks for chatting with " + BOT_NAME);
         System.out.println("See you again soon!");
+        System.out.println(LINE_BREAK);
     }
 }
---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+public class Event extends Task {
+
+    protected String eventPeriod;
+
+    public Event(String description, String eventPeriod) {
+        super(description);
+        this.eventPeriod = eventPeriod;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + this.eventPeriod + ")";
+    }
+}
