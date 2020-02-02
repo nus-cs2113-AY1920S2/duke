ea89c0a36cc488f2c2bf5b69ded4f7903989ea62
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2020-02-02 21:52:15 +0800

    Add Increment: Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,15 +18,25 @@ public class Duke {
                 break;
             }
             else if (userInput.equals("list")){
+                System.out.println("Here are your tasks:");
                 for (int j = 0; j<count; j++) {
                     System.out.println(j+1 + ". " + tasks[j]);
                 }
             }
-            else if (userInput.startsWith("done ")){
+            else if (userInput.startsWith("done")){
                 int itemNumber = Integer.parseInt((userInput.substring(5)));
                 tasks[itemNumber-1].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println("  " + tasks[itemNumber-1]);
+            }
+            else if (userInput.startsWith("todo")){
+                count = addTodoTask(userInput, tasks, count);
+            }
+            else if (userInput.startsWith("deadline")){
+                count = addDeadlineTask(userInput, tasks, count);
+            }
+            else if (userInput.startsWith("event")){
+                count = addEventTask(userInput, tasks, count);
             }
             else {
                 System.out.println("added: " + userInput);
@@ -35,5 +45,39 @@ public class Duke {
             }
         }
         System.out.println("Bye. Hope to see you soon!");
+    }
+
+    private static int addTodoTask(String userInput, Task[] tasks, int count) {
+        String todoTask = userInput.substring(5);
+        tasks[count] = new Todo(todoTask);
+        count++;
+        System.out.println("New task added: ");
+        System.out.println(" " + tasks[count-1]);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        return count;
+    }
+
+    private static int addDeadlineTask(String userInput, Task[] tasks, int count) {
+        String[] details = userInput.split("/by ");
+        String deadlineTask = details[0].substring(9);
+        String date = details[1];
+        tasks[count] = new Deadline(deadlineTask, date);
+        count++;
+        System.out.println("New task added: ");
+        System.out.println(" " + tasks[count-1]);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        return count;
+    }
+
+    private static int addEventTask(String userInput, Task[] tasks, int count) {
+        String[] details = userInput.split("/at ");
+        String deadlineTask = details[0].substring(6);
+        String dateAndTime = details[1];
+        tasks[count] = new Event(deadlineTask, dateAndTime);
+        count++;
+        System.out.println("New task added: ");
+        System.out.println(" " + tasks[count-1]);
+        System.out.println("Now you have " + count + " tasks in the list.");
+        return count;
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -3,6 +3,11 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    public Task() {
+        this.description = "";
+        this.isDone = false;
+    }
+
     public Task(String description) {
         this.description = description;
         this.isDone = false;
