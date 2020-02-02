58ea8f8aaeeb226674ce5805790144bfe0a28343
alexlim56 <42902719+alexlim56@users.noreply.github.com>
2020-02-02 19:38:51 +0800

    Add increment: Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,34 +3,36 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String logo = "***John***";
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         String userInput = "";
         int count = 0;
-        boolean isFinished = false;
 
         System.out.println("Hey! I am " + logo);
         System.out.println("What would you like to do?");
 
         Scanner in = new Scanner(System.in);
-        userInput = in.nextLine();
 
-        while(!isFinished){
-            switch (userInput) {
-            case "bye":
-                isFinished = true;
+        while(true){
+            userInput = in.nextLine();
+            if (userInput.equals("bye")){
                 break;
-            case "list":
+            }
+            else if (userInput.equals("list")){
                 for (int j = 0; j<count; j++) {
                     System.out.println(j+1 + ". " + tasks[j]);
                 }
-                break;
-            default:
-                System.out.println("added: " + userInput);
-                tasks[count] = userInput;
-                count++;
-                break;
             }
-            userInput = in.nextLine();
+            else if (userInput.startsWith("done ")){
+                int itemNumber = Integer.parseInt((userInput.substring(5)));
+                tasks[itemNumber-1].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[itemNumber-1]);
+            }
+            else {
+                System.out.println("added: " + userInput);
+                tasks[count] = new Task(userInput);
+                count++;
+            }
         }
         System.out.println("Bye. Hope to see you soon!");
     }
