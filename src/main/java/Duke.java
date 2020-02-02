486b84117324bba7ab2297234039b4fb2ab8956e
HAOYUN49 <e01261960@u.nus.edu>
2020-02-02 12:31:30 +0800

    Add Greet, Echo, Exit to Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void exit() {
+        System.out.println("------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------");
+        System.out.println();
+    }
+
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+        String command;
+        while(in.hasNextLine()) {
+            command = in.nextLine();
+            if(command.equals("bye")) {
+                exit();
+                return;
+            } else {
+                System.out.println("------------------------------------");
+                System.out.println(command);
+                System.out.println("------------------------------------");
+                System.out.println();
+            }
+        }
+    }
 
     public static void greet(){
         System.out.println("------------------------------------");
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println();
         System.out.println("------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
         System.out.println();
-        System.out.println("------------------------------------");
     }
 
     public static void main(String[] args) {
@@ -19,5 +42,6 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+        echo();
     }
 }
