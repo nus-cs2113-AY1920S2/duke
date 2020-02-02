7bb1c007b247d368ec1c61e9bf63d1a06a0d5ace
Shannonwje <e0325213@u.nus.edu>
2020-02-02 10:47:37 +0800

    Added Increment: Added Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,52 +2,115 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        printWelcomeMessage();
+
+        // initialise an array of tasks that is <=100
+        //sizeOfList is the number of items in the list
+        Task[] listOfTasks = new Task[100];
+        int sizeOfList = 0;
+
+        // Reads in the first command from the user
+        Scanner scanner = new Scanner(System.in);
+        String inputCommand = scanner.nextLine();
+
+        while (inputCommand.equals("bye") == false) {
+
+            if (inputCommand.equals("list")) {
+                // prints out the list
+                printsOutTheList(listOfTasks, sizeOfList);
+
+            } else if (inputCommand.contains("done ") == true) {
+                // marks task in the stated index as done
+                indicateTaskAsDone(listOfTasks, inputCommand);
+
+            } else {
+                // adds a new task into the list
+                sizeOfList = addInNewTask(listOfTasks, sizeOfList, inputCommand);
+
+            }
+            // take in the next command
+            inputCommand = scanner.nextLine();
+        }
+
+        printExitMessage();
+    }
+
+    /**
+     * Prints the exit message
+     */
+    private static void printExitMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Adds in new task to the array,
+     * returns the new size of the list
+     *
+     * @param listOfTasks array of tasks
+     * @param sizeOfList number of tasks in list
+     * @param inputCommand name of the task to add
+     * @return the updated number of tasks in list
+     */
+    private static int addInNewTask(Task[] listOfTasks, int sizeOfList, String inputCommand) {
+        // creates new task
+        Task newTask = new Task(inputCommand);
+        listOfTasks[sizeOfList] = newTask;
+        // informs user that task has been added to list
+        System.out.println("____________________________________________________________");
+        System.out.println(" added: " + inputCommand);
+        System.out.println("____________________________________________________________");
+        sizeOfList++;
+        return sizeOfList;
+    }
+
+    /**
+     * Prints acknowledgment that the specified task is done,
+     * sets the task in the array to be done
+     *
+     * @param listOfTasks array of tasks
+     * @param inputCommand name of task to set as done
+     */
+    private static void indicateTaskAsDone(Task[] listOfTasks, String inputCommand) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Nice! I've marked this task as done:");
+        // obtain index of task to set as done
+        String indexInString = inputCommand.substring(5);
+        int index = Integer.parseInt(indexInString) - 1;
+        // mark as done
+        listOfTasks[index].markAsDone();
+        System.out.println("   [" + listOfTasks[index].getStatusIcon()
+                           + "] " + listOfTasks[index].description);
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Prints out the entire list
+     *
+     * @param listOfTasks array of tasks
+     * @param sizeOfList number of tasks in the list
+     */
+    private static void printsOutTheList(Task[] listOfTasks, int sizeOfList) {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Here are the tasks in your list:");
+        for (int i = 0; i < sizeOfList; i++) {
+            int k = i + 1;
+            System.out.println(" " + k + ".[" + listOfTasks[i].getStatusIcon()
+                               + "] " + listOfTasks[i].description);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    /**
+     * Prints out the welcome message
+     */
+    private static void printWelcomeMessage() {
         String logo = "Shannon";
         System.out.println("Hello from " + logo);
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Shannon" + System.lineSeparator() + " What can I do for you?");
-        System.out.println("____________________________________________________________");
-
-        Scanner scanner = new Scanner(System.in);
-        String inputCommand = scanner.nextLine();
-        Task[] listOfTasks = new Task[100];
-        int sizeOfList = 0;
-        while (inputCommand.equals("bye") == false) {
-
-            if (inputCommand.equals("list")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" Here are the tasks in your list:");
-                for (int i = 0; i < sizeOfList; i++) {
-                    int k = i + 1;
-                    System.out.println(" " + k + ".[" + listOfTasks[i].getStatusIcon() + "] " + listOfTasks[i].description);
-                }
-                System.out.println("____________________________________________________________");
-
-            } else if (inputCommand.contains("done ") == true) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" Nice! I've marked this task as done:");
-                String indexInString = inputCommand.substring(5);
-                int index = Integer.parseInt(indexInString) - 1;
-                listOfTasks[index].markAsDone();
-                System.out.println("   [" + listOfTasks[index].getStatusIcon() + "] " + listOfTasks[index].description);
-                System.out.println("____________________________________________________________");
-
-            } else {
-                Task newTask = new Task(inputCommand);
-                listOfTasks[sizeOfList] = newTask;
-                System.out.println("____________________________________________________________");
-                System.out.println(" added: " + inputCommand);
-                System.out.println("____________________________________________________________");
-                sizeOfList++;
-
-            }
-
-            inputCommand = scanner.nextLine();
-        }
-
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,15 +2,30 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Returns the task created with the specified
+     * description.
+     *
+     * @param description the description of the task
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the tick or the cross icon of the specified tsk
+     * If the task's isDone is false, a cross is returned, else its a tick
+     *
+     * @return either tick or cross
+     */
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718");
     }
 
+    /**
+     * Sets the specified task's isDone to be true
+     */
     public void markAsDone() {
         isDone = true;
     }
