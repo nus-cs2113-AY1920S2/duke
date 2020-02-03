3d38622a7a5bcf17838c4b1692d98f38f482b195
quinnyyy <quinnyyy@bu.edu>
2020-02-03 17:55:06 +0800

    Replace TaskFactory class with method in TaskManager

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -23,7 +23,7 @@ public class TaskManager {
         if (command.equals("list")) {
             listTasks();
         } else if (command.equals("todo") || command.equals("deadline") || command.equals("event")) {
-            Task newTask = TaskFactory.getTaskFromUserInput(command, userInput);
+            Task newTask = getTaskFromUserInput(command, userInput);
             if (newTask == null) {
                 Ui.printPretty("Sorry, couldn't recognize this input format");
             } else {
@@ -46,6 +46,29 @@ public class TaskManager {
         }
 
         return false; // Unrecognized command
+    }
+
+    protected Task getTaskFromUserInput(String taskType, String userInput) {
+        if (userInput == null) {
+            return null;
+        }
+
+        String description;
+        switch(taskType) {
+        case "todo":
+            description = userInput.substring(userInput.indexOf(" ") + 1);
+            return new ToDo(description);
+        case "deadline":
+            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf(" /by "));
+            String dueDateTime = userInput.substring(userInput.indexOf(" /by ") + 5);
+            return new Deadline(description, dueDateTime);
+        case "event":
+            description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf(" /at "));
+            String startEndDateTime = userInput.substring(userInput.indexOf(" /at ") + 5);
+            return new Event(description, startEndDateTime);
+        default:
+            return null;
+        }
     }
 
     protected void addTask(Task newTask) {
