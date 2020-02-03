33a6b02031e15eab5569bc549c9e018b87dbf68c
alwayshuizhen <alwayshuizhen@gmail.com>
2020-02-03 18:59:45 +0800

    Revised code based on pull request review

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 public class Duke {
+    private static int MAXIMUM_TASKS = 100;
+
     public static void printTaskList(Task[] taskList, int counter){
         for (int i = 0; i < counter; i++) {
             System.out.println(i + 1 + ". " + taskList[i].printObject());
@@ -47,23 +49,23 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___| ";
         System.out.println(logo + " says hello :)");
         System.out.println("\nWhat can i do for you?");
-        Task[] taskList = new Task[100];
+        Task[] taskList = new Task[MAXIMUM_TASKS];
         int counter = 0;
         Scanner myObj = new Scanner(System.in);
         String cmd;
         cmd = myObj.nextLine();
-        while (!cmd.equals("bye")) {
-            if (cmd.equals("list")) {
+        while (!cmd.equalsIgnoreCase("bye")) {
+            if (cmd.equalsIgnoreCase("list")) {
                 printTaskList(taskList, counter);
-            } else if (cmd.substring(0,4).equals("done")) {
+            } else if (cmd.substring(0,4).equalsIgnoreCase("done")) {
                 markTaskAsDone(taskList, cmd);
-            }else if (cmd.substring(0,4).equals("todo")){
+            }else if (cmd.substring(0,4).equalsIgnoreCase("todo")){
                 taskList[counter] = createToDo(cmd, counter);
                 counter++;
-            } else if (cmd.substring(0,5).equals("event")) {
+            } else if (cmd.substring(0,5).equalsIgnoreCase("event")) {
                 taskList[counter] = createEvent(cmd, counter);
                 counter++;
-            } else if (cmd.substring(0,8).equals("deadline")) {
+            } else if (cmd.substring(0,8).equalsIgnoreCase("deadline")) {
                 taskList[counter] = createDeadline(cmd, counter);
                 counter++;
             } else {
