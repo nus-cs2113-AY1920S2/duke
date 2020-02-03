123c721bf093f4f5f5617c270e72f41a0c03df4f
Sibing <1915563868@qq.com>
2020-02-03 13:07:11 +0800

    add Deadline, Event, Todo class (Level-4)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,62 @@ import java.util.Scanner;
 //import java.io.InputStreamReader;
 
 public class Duke {
+    private static final String LIST_COMMAND = "list";
+    private static final String BYE_COMMAND = "bye";
+    private static final String DONE_COMMAND = "done";
+    private static final String TODO_COMMAND = "todo";
+    private static final String DEADLINE_COMMAND = "deadline";
+    private static final String EVENT_COMMAND = "event";
+    private static final String DEADLINE_MARKER = "/by";
+    private static final String EVENT_MARKER = "/at";
+
+    // a array to store user input task
+    private static List<Task> tasks = new ArrayList<>();
+
+    private static void addTask(String input) {
+        Task task = null;
+        String description = null;
+        if (input.startsWith(TODO_COMMAND)) {
+            description = input.substring(TODO_COMMAND.length()).trim();
+            task = new Todo(description);
+        } else if(input.startsWith(DEADLINE_COMMAND)) {
+            int byPosition = input.indexOf(DEADLINE_MARKER);
+            description = input.substring(DEADLINE_COMMAND.length(), byPosition).trim();
+            String by = input.substring(byPosition + DEADLINE_MARKER.length()).trim();
+            task = new Deadline(description, by);
+        } else if (input.startsWith(EVENT_COMMAND)) { // Event
+            int atPosition = input.indexOf(EVENT_MARKER);
+            description = input.substring(EVENT_COMMAND.length(), atPosition).trim();
+            String at = input.substring(atPosition + EVENT_MARKER.length()).trim();
+            task = new Event(description, at);
+        }
+
+        tasks.add(task);
+
+        System.out.println("Got it. I've added this task:");
+        System.out.print("  ");
+        System.out.println(task);
+        System.out.println(String.format("Now you have %d tasks in the list.", tasks.size()));
+    }
+
+    private static void markAsDone(String input) {
+        int taskNumber = Integer.parseInt(input.substring(4).trim());
+        Task task = tasks.get(taskNumber - 1);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.print("  ");
+        System.out.println(task);
+    }
+
+    private static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            System.out.print(String.format("%d.", i + 1));
+            System.out.println(task);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -21,36 +77,24 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         String input = null;
 
-        // a array to store user input task
-        List<Task> tasks = new ArrayList<>();
-
         do {
             input = in.nextLine();
             switch (input) {
-            case "list":
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < tasks.size(); i++) {
-                    Task task = tasks.get(i);
-                    System.out.println(String.format("%d.[%s] %s", i + 1, task.getStatusIcon(), task.getDescription()));
-                }
+            case LIST_COMMAND:
+                listTasks();
                 break;
-            case "bye":
+            case BYE_COMMAND:
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             default:
-                if (input.startsWith("done")) {
-                    int taskNumber = Integer.parseInt(input.substring(4).trim());
-                    Task task = tasks.get(taskNumber - 1);
-                    task.markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(String.format("[%s] %s", task.getStatusIcon(), task.getDescription()));
+                if (input.startsWith(DONE_COMMAND)) {
+                    markAsDone(input);
                     break;
                 } else {
-                    System.out.println("added: "+ input);
-                    tasks.add(new Task(input));
+                    addTask(input);
                     break;
                 }
             }
-        } while (!input.equals("bye"));
+        } while (!input.equals(BYE_COMMAND));
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,31 +2,19 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
-    /** Constructor */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
-    /**
-     * Get the status icon of the task, finish or not
-     *
-     * @return Status icon.
-     */
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); // return tick or X symbols
     }
 
-    /**
-     * Task description getter
-     *
-     * @return Task content
-     */
-    public String getDescription() {
-        return description;
+    public String toString() {
+        return String.format("[%s] %s", getStatusIcon(), description);
     }
 
-    /** Mark the task as done */
     public void markAsDone() {
         isDone = true;
     }
