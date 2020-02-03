6b1a939dc6adcf9868b33d63499e1fa3979cd73c
BenjaminChan <42601850+benchan911@users.noreply.github.com>
2020-02-04 00:04:54 +0800

    Level 4. ToDos, Events, Deadlines Add support for tracking three types of tasks:
    ToDos: tasks without any date/time attached to it e.g., visit new theme park
    Deadlines: tasks that need to be done before a specific date/time e.g., submit report by 11/10/2019 5pm
    Events: tasks that start at a specific time and ends at a specific time e.g., team project meeting on 2/10/2019 2-4pm

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,68 +3,128 @@ import java.util.Arrays;
 
 public class Duke {
 
+    private static Scanner sc = new Scanner(System.in);
+
+    private static final String BOT_NAME = "E.D.I.T.H.";
+    private static final String USERNAME = "USER";
+    private static final String MESSAGE_WELCOME = "\nHello! I'm " + BOT_NAME + "\nWhat can I do for you?";
+    private static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!\n";
+    private static final String LINE_DIVIDER = "\n____________________________________________________________";
+    private static final String MESSAGE_INVALID_COMMAND = "Invalid Command. Please try again\n";
+    private static final String COMMAND_ADD_WORD = "ADD";
+    private static final String COMMAND_ADD_DESC = "Adds an item to the todo list";
+    private static final String COMMAND_HELP_WORD = "HELP";
+    private static final String COMMAND_HELP_DESC = "Here's the help list\n1. Add\n2. List\n3. Mark";
+    private static final String COMMAND_LIST_WORD =  "LIST";
+    private static final String COMMAND_MARK_WORD = "MARK";
+    private static final String COMMAND_DEADLINE_WORD = "DEADLINE";
+    private static final String COMMAND_EXIT_WORD = "EXIT";
+    private static final String COMMAND_EVENT_WORD = "EVENT";
+    private static final String COMMAND_DONE_WORD = "DONE";
+    private static final String COMMAND_TODO_WORD = "TODO";
+
+
+
+    public static final void displayWelcomeMessage(){ System.out.println( "\n" + LINE_DIVIDER + MESSAGE_WELCOME + LINE_DIVIDER);}
+    public static final void displayInvalidCommand(){ System.out.println(MESSAGE_INVALID_COMMAND);}
+    public static final void displayExitMessage(){ System.out.println(MESSAGE_EXIT);}
+    public static final void displayHelpMenu(){ System.out.print(COMMAND_HELP_DESC);}
+    public static final void displayLineDivider(){ System.out.println(LINE_DIVIDER);}
+    public static final void displayNumberOfTasks(int Tasks){ System.out.print("Now you have " + Tasks + " tasks in the list.");}
+
+    private static void echoUserCommand(String userCommand) {
+        System.out.println("[Command entered: " + userCommand + "]");
+    }
+
+    private static void exitProgram(){
+        displayExitMessage();
+    }
+
     public static void main(String[] args) {
-        String BotName = "E.D.I.T.H.";
-        String UserName = "USER";
-        String lineDivider = "____________________________________________________________\n";
-        System.out.println(lineDivider + " Hello! I'm " + BotName +
-                "\n What can I do for you?\n" + lineDivider);
 
-        Scanner sc = new Scanner(System.in);
-        String UserCommand;
-        TaskManager Shop = new TaskManager();
+        Task task[] = new Task[100];
+        int index = 0;
+
+        displayWelcomeMessage();
+        String userCommand;
         boolean run = true;
-        while (run) {
-            System.out.print("USER\n  ");
-            UserCommand = sc.nextLine();
-            switch (UserCommand) {
+        while(run) {
+            userCommand = sc.nextLine();
+            displayLineDivider();
+            echoUserCommand(userCommand);
+            switch (userCommand) {
 
-                case "List": {
-                    System.out.println(lineDivider + "\nHere are the tasks in your list:");
-                    Shop.getTask();
-                    System.out.println(lineDivider);
+                case COMMAND_TODO_WORD: {
+                    String input = sc.nextLine();
+                    Task item = new Task(input, "T");
+                    task[index] = item;
+                    index++;
+                    displayNumberOfTasks(index);
                     break;
                 }
 
-                case "Done": {
-                    System.out.println("Item to be marked? ");
-                    String cmd = sc.nextLine();
-                    int index = Integer.parseInt(cmd);
-                    Shop.markTask(index - 1);
-                    System.out.println("\nNice! I've marked this task as done:  \n  [YES]  " + Shop.getTaskItem(index - 1) + "\n" + lineDivider);
+                case COMMAND_EVENT_WORD: {
+                    String input = sc.nextLine();
+                    Task item = new Task(input, "E");
+                    task[index] = item;
+                    input = sc.nextLine();
+                    task[index].setBy(input);
+                    index++;
+                    displayNumberOfTasks(index);
                     break;
                 }
 
-                case "Add": {
-                    System.out.println("Item to be Added? ");
-                    String itemName = sc.nextLine();
-                    Shop.addTask(itemName);
-                    System.out.println(lineDivider + BotName + "\n  Added: " + itemName + "\n" + lineDivider);
+                case COMMAND_DEADLINE_WORD: {
+                    String input = sc.nextLine();
+                    Task item = new Task(input, "D");
+                    task[index] = item;
+                    input = sc.nextLine();
+                    task[index].setBy(input);
+                    index++;
+                    displayNumberOfTasks(index);
                     break;
                 }
 
-                case "Check": {
-                    System.out.println("Item to be Checked? ");
-                    String itemName = sc.nextLine();
-                    System.out.println(lineDivider + BotName + "\n Checked:" + itemName + "\n" + lineDivider);
-                    System.out.print("USER\n  ");
+                case COMMAND_DONE_WORD: {
+                    String input = sc.nextLine();
+                    int markIndex = Integer.parseInt(input);
+                    System.out.println("Done by? WRITE IN DD/MM/YY");
+                    String input1 = sc.nextLine();
+                    task[markIndex-1].setBy(input1);
                     break;
                 }
 
-                case "Bye": {
+                case COMMAND_MARK_WORD: {
+                    String input = sc.nextLine();
+                    int markIndex = Integer.parseInt(input);
+                    task[markIndex-1].markDone();
+                    break;
+                }
+
+                case COMMAND_LIST_WORD: {
+                    if (index == 0){
+                        System.out.print("Your list is empty! Try adding to the list first :)");
+                        break;
+                    }
+
+                    System.out.print("Here are the tasks in your list:");
+                    for (int i = 0 ; i < index; i++){
+                        System.out.print("\n" + (i+1) + ". " + task[i].getStatusIcon()  + "[" + task[i].getType() + "] " + task[i].getDescription()  + task[i].getBy());
+                    }
+                    break;
+                }
+
+                case COMMAND_EXIT_WORD: {
                     run = false;
+                    exitProgram();
                     break;
                 }
-
                 default: {
-                    System.out.println(lineDivider + BotName + "\n  Echoed: " + UserCommand + "\n" + lineDivider);
+                    System.out.println("INVALID");
                     break;
                 }
             }
-
+            displayLineDivider();
         }
-        System.out.println(lineDivider + " Bye. Hope to see you again soon!\n" + lineDivider);
     }
-
-
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,31 @@
 public class Task {
 
-    protected String description;
     protected boolean isDone;
+    protected String type;
+    protected String description;
+    protected String by;
 
-    public Task(String description) {
+
+    public Task(String description, String type) {
         this.description = description;
+        this.type = type;
         this.isDone = false;
+        this.by = "";
     }
+
+    public String getDescription() { return description; }
+
+    public String getBy(){ return by; }
+
+    public String getType(){ return type; }
+
+    public void markDone(){ this.isDone = true; }
+
+    public void setBy(String by){ this.by = "  (" + by + ")"; }
 
     public String getStatusIcon() {
-        //
-        // return (isDone ? "\u2713" : "\u2718");
-        // return tick or X symbols
-        return (isDone ? "YES" : "NO"); //return tick or X symbols
-    }
-
-    public void markDone() {
-        this.isDone = true;
+//        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        return (isDone ? "[Y]" : "[N]"); //return tick or X symbols
     }
 
 }
