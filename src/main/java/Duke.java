23994f6a4afe824e8fa7d7ad57171ed23c9d078d
Sibing <1915563868@qq.com>
2020-02-04 16:35:55 +0800

    refactor and move the function definition after main function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,53 +14,7 @@ public class Duke {
 
     // a array to store user input task
     private static List<Task> tasks = new ArrayList<>();
-
-    private static void addTask(String command, String description) {
-        Task task = null;
-        String taskDescription = null;
-        switch (command) {
-        case TODO_COMMAND:
-            taskDescription = description.trim();
-            task = new Todo(taskDescription);
-            break;
-        case DEADLINE_COMMAND:
-            String[] taskBy = description.split(DEADLINE_MARKER);
-            taskDescription = taskBy[0].trim();
-            String by = taskBy[1].trim();
-            task = new Deadline(taskDescription, by);
-            break;
-        case EVENT_COMMAND:
-            String[] taskAt = description.split(EVENT_MARKER);
-            taskDescription = taskAt[0].trim();
-            String at = taskAt[1].trim();
-            task = new Event(taskDescription, at);
-        }
-
-        tasks.add(task);
-
-        System.out.println("Got it. I've added this task:");
-        System.out.print("  ");
-        System.out.println(task);
-        System.out.println(String.format("Now you have %d tasks in the list.", tasks.size()));
-    }
-
-    private static void markAsDone(int taskNumber) {
-        Task task = tasks.get(taskNumber - 1);
-        task.markAsDone();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.print("  ");
-        System.out.println(task);
-    }
-
-    private static void listTasks() {
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.size(); i++) {
-            Task task = tasks.get(i);
-            System.out.print(String.format("%d.", i + 1));
-            System.out.println(task);
-        }
-    }
-
+    
     public static void main(String[] args) {
         displayWelcomeMessage();
         
@@ -101,7 +55,53 @@ public class Duke {
         System.out.println("What can I do for you?\n");
     }
 
+    private static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            Task task = tasks.get(i);
+            System.out.print(String.format("%d.", i + 1));
+            System.out.println(task);
+        }
+    }
+
+    private static void markAsDone(int taskNumber) {
+        Task task = tasks.get(taskNumber - 1);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.print("  ");
+        System.out.println(task);
+    }
+
     private static void displayExitMessage() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void addTask(String command, String description) {
+        Task task = null;
+        String taskDescription = null;
+        switch (command) {
+        case TODO_COMMAND:
+            taskDescription = description.trim();
+            task = new Todo(taskDescription);
+            break;
+        case DEADLINE_COMMAND:
+            String[] taskBy = description.split(DEADLINE_MARKER);
+            taskDescription = taskBy[0].trim();
+            String by = taskBy[1].trim();
+            task = new Deadline(taskDescription, by);
+            break;
+        case EVENT_COMMAND:
+            String[] taskAt = description.split(EVENT_MARKER);
+            taskDescription = taskAt[0].trim();
+            String at = taskAt[1].trim();
+            task = new Event(taskDescription, at);
+        }
+
+        tasks.add(task);
+
+        System.out.println("Got it. I've added this task:");
+        System.out.print("  ");
+        System.out.println(task);
+        System.out.println(String.format("Now you have %d tasks in the list.", tasks.size()));
     }
 }
