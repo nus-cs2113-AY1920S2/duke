d2a12d8a10ee229bea8e7b645c447789d07bc76f
anqi-nus <e0322837@u.nus.edu>
2020-02-04 22:54:14 +0800

    Change indentation to satisfy coding standard.

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -3,6 +3,9 @@ import java.util.List;
 
 public class TaskManager {
     protected List taskList = new LinkedList();
+    public TaskManager(List taskList) {
+        this.taskList = taskList;
+    }
     public void printSplit() {
         System.out.println("    ✻❊✽✼❉✱✲✾❃❋❈❆✿❀❁");
     }
@@ -10,15 +13,12 @@ public class TaskManager {
         printSplit();
         System.out.println(
                 "    Please select one of the following (please key in the number): \n" +
-                        "    1. Add a new task, \n" +
-                        "    2. Delete an old task, \n" +
-                        "    3. Show my tasks, or\n"+
-                        "    4. See you next time! \n" +
-                        "    to end this conversation");
+                "    1. Add a new task, \n" +
+                "    2. Delete an old task, \n" +
+                "    3. Show my tasks, or\n"+
+                "    4. See you next time! \n" +
+                "    to end this conversation");
         printSplit();
     }
 
-    public TaskManager(List taskList) {
-        this.taskList = taskList;
-    }
 }
