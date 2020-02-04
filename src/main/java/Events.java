79cf5c215dc429c257fc349720ff84f719ea250c
Yuxiang <yuxianglim1227@gmail.com>
2020-02-05 02:13:10 +0800

    Major changes to program inclusives but not exhausive of: - Modified greeting statments - Addition of list of functions available in table format - Changed While loop conditions such that program will end when "bye" function is keyed in - Added default statement for program to continue prompting user for functions if system is unable to detect any available functions - Reorganised swtich cases - 3 new class type for tasks - Addition of ability to store 3 different types of task. ( Events, To-do, Deadline) - Streamlined printing of newly added task with a printing method

---
+++ src/main/java/Events.java
@@ -0,0 +1,18 @@
+public class Events extends Task {
+    private String at;
+
+    public Events(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    public String getEvent(){
+        return at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(at: " + getEvent() + ")" ;
+    }
+
+}
