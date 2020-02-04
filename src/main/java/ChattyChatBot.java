0c4035ec3e3b265f2135151bc5fbced887629a08
Zhilin <huangzhilin1999@gmail.com>
2020-02-04 16:03:43 +0800

    Refactor constants to a Constants.java

--- src/main/java/ChattyChatBot.java
+++ src/main/java/ChattyChatBot.java
@@ -6,26 +6,6 @@ public class ChattyChatBot {
 
     private static final Scanner SCANNER = new Scanner(System.in);
 
-    private static final String BOT_NAME = "Chatty Chat Bot";
-
-    private static final String BYE_STRING = "bye";
-    private static final String LIST_STRING = "list";
-    private static final String DONE_STRING = "done";
-    private static final String TODO_STRING = "todo";
-    private static final String DEADLINE_STRING = "deadline";
-    private static final String EVENT_STRING = "event";
-
-    private static final String SPACE_SEPARATOR = " ";
-    private static final String DOT_CHARACTER = ".";
-    private static final String BY_STRING = "/by";
-    private static final String AT_STRING = "/at";
-
-    private static final String LINE_BREAK = "____________________________________________________________";
-
-    private static final String ADDED_TASK_CONFIRMATION = "Got it. I've added this task:";
-    private static final String TASK_SUMMARY_FIRST_HALF = "Now you have ";
-    private static final String TASK_SUMMARY_SECOND_HALF = " tasks in the list.";
-
     public static void main(String[] args) {
         sendWelcomeMessage();
 
@@ -35,45 +15,45 @@ public class ChattyChatBot {
         do {
             userInput = SCANNER.nextLine();
 
-            System.out.println(LINE_BREAK);
+            System.out.println(Constants.LINE_BREAK);
 
             // Solution below adapted from: https://stackoverflow
             // .com/questions/5067942/what-is-the-best-way-to-extract-the-first-word-from-a-string-in-java
-            String[] array = userInput.split(SPACE_SEPARATOR, 2);
+            String[] array = userInput.split(Constants.SPACE_SEPARATOR, 2);
             String action = array[0];
 
             switch (action) {
-            case LIST_STRING:
+            case Constants.LIST_STRING:
                 listAllTasks(tasks);
                 break;
-            case DONE_STRING:
+            case Constants.DONE_STRING:
                 markTaskAsDone(tasks, array[1]);
                 break;
-            case TODO_STRING:
+            case Constants.TODO_STRING:
                 addToDoTask(tasks, array[1]);
                 break;
-            case DEADLINE_STRING:
+            case Constants.DEADLINE_STRING:
                 addDeadlineTask(tasks, array[1]);
                 break;
-            case EVENT_STRING:
+            case Constants.EVENT_STRING:
                 addEventTask(tasks, array[1]);
                 break;
-            case BYE_STRING:
+            case Constants.BYE_STRING:
                 sendByeMessage();
                 break;
             default:
                 sendDefaultResponse();
             }
 
-            System.out.println(LINE_BREAK);
-        } while (!userInput.equals(BYE_STRING));
+            System.out.println(Constants.LINE_BREAK);
+        } while (!userInput.equals(Constants.BYE_STRING));
     }
 
     private static void listAllTasks(List<Task> tasks) {
         System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             Task task = tasks.get(i);
-            System.out.println((i + 1) + DOT_CHARACTER + SPACE_SEPARATOR + task.toString());
+            System.out.println((i + 1) + Constants.DOT_CHARACTER + Constants.SPACE_SEPARATOR + task.toString());
         }
     }
 
@@ -88,45 +68,45 @@ public class ChattyChatBot {
         ToDo newToDoTask = new ToDo(description);
         tasks.add(newToDoTask);
 
-        System.out.println(ADDED_TASK_CONFIRMATION);
+        System.out.println(Constants.ADDED_TASK_CONFIRMATION);
         System.out.println(newToDoTask);
-        System.out.println(TASK_SUMMARY_FIRST_HALF + tasks.size() + TASK_SUMMARY_SECOND_HALF);
+        System.out.println(Constants.TASK_SUMMARY_FIRST_HALF + tasks.size() + Constants.TASK_SUMMARY_SECOND_HALF);
     }
 
     private static void addDeadlineTask(List<Task> tasks, String inputStr) {
-        String[] array = inputStr.split(BY_STRING);
+        String[] array = inputStr.split(Constants.BY_STRING);
         Deadline newDeadlineTask = new  Deadline(array[0], array[1]);
         tasks.add(newDeadlineTask);
 
-        System.out.println(ADDED_TASK_CONFIRMATION);
+        System.out.println(Constants.ADDED_TASK_CONFIRMATION);
         System.out.println(newDeadlineTask);
-        System.out.println(TASK_SUMMARY_FIRST_HALF + tasks.size() + TASK_SUMMARY_SECOND_HALF);
+        System.out.println(Constants.TASK_SUMMARY_FIRST_HALF + tasks.size() + Constants.TASK_SUMMARY_SECOND_HALF);
     }
 
     private static void addEventTask(List<Task> tasks, String inputStr) {
-        String[] array = inputStr.split(AT_STRING);
+        String[] array = inputStr.split(Constants.AT_STRING);
         Event newEventTask = new Event(array[0], array[1]);
         tasks.add(newEventTask);
 
-        System.out.println(ADDED_TASK_CONFIRMATION);
+        System.out.println(Constants.ADDED_TASK_CONFIRMATION);
         System.out.println(newEventTask);
-        System.out.println(TASK_SUMMARY_FIRST_HALF + tasks.size() + TASK_SUMMARY_SECOND_HALF);
+        System.out.println(Constants.TASK_SUMMARY_FIRST_HALF + tasks.size() + Constants.TASK_SUMMARY_SECOND_HALF);
     }
 
     private static void sendDefaultResponse() {
-        System.out.println("I'm " + BOT_NAME);
+        System.out.println("I'm " + Constants.BOT_NAME);
         System.out.println("How may I help you?");
     }
 
     private static void sendWelcomeMessage() {
-        System.out.println(LINE_BREAK);
-        System.out.println("Hello from " + BOT_NAME);
+        System.out.println(Constants.LINE_BREAK);
+        System.out.println("Hello from " + Constants.BOT_NAME);
         System.out.println("Glad to be at your service!");
-        System.out.println(LINE_BREAK);
+        System.out.println(Constants.LINE_BREAK);
     }
 
     private static void sendByeMessage() {
-        System.out.println("Thanks for chatting with " + BOT_NAME);
+        System.out.println("Thanks for chatting with " + Constants.BOT_NAME);
         System.out.println("See you again soon!");
     }
 }
---
+++ src/main/java/Constants.java
@@ -0,0 +1,21 @@
+public class Constants {
+    public static final String BOT_NAME = "Chatty Chat Bot";
+
+    public static final String BYE_STRING = "bye";
+    public static final String LIST_STRING = "list";
+    public static final String DONE_STRING = "done";
+    public static final String TODO_STRING = "todo";
+    public static final String DEADLINE_STRING = "deadline";
+    public static final String EVENT_STRING = "event";
+
+    public static final String SPACE_SEPARATOR = " ";
+    public static final String DOT_CHARACTER = ".";
+    public static final String BY_STRING = "/by";
+    public static final String AT_STRING = "/at";
+
+    public static final String LINE_BREAK = "____________________________________________________________";
+
+    public static final String ADDED_TASK_CONFIRMATION = "Got it. I've added this task:";
+    public static final String TASK_SUMMARY_FIRST_HALF = "Now you have ";
+    public static final String TASK_SUMMARY_SECOND_HALF = " tasks in the list.";
+}
