efc5bf41172b205a9d97d948343a18e0f3c5c8c6
--global <riccardodimaio11@gmail.com>
2020-02-06 22:49:19 +0800

    Refactor into packages

---
+++ src/main/java/duke/exception/InvalidDateFormatException.java
@@ -0,0 +1,7 @@
+package duke.exception;
+
+// Thrown when user does not delimit description and date of Tasks with date (e.g. duke.task.Deadline, duke.task.Event) by using
+// the specified delimiter character '/' (forward slash)
+public class InvalidDateFormatException extends Exception {
+
+}
---
+++ src/main/java/duke/exception/NoDescException.java
@@ -0,0 +1,5 @@
+package duke.exception;
+
+// Thrown when user doesn't input the description of a duke.task.Task
+public class NoDescException extends Exception {
+}
