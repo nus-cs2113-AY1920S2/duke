c5f74d3c055ae0d69cce94f7ee3de431100173c3
Joel <joelchang97@gmail.com>
2020-02-07 13:41:21 +0800

    Revert "Implemented Level-5 changes"
    This reverts commit 1e6dd5052d237f45bbc12f1326ec298a230706d2.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,18 +7,28 @@ public class Duke {
     private ToDo newToDo;
     private Event newEvent;
     private Deadline newDeadline;
-    private UI userInterface;
-    private Parser parser;
-    private DukeExceptions dukeExceptions;
-    private final int startIndex = 0;
-    private final int lengthOfDeadline = 9;
-    private final int lengthOfEvent = 5;
 
     public Duke() {
         this.tasks = new ArrayList<Task>();
-        userInterface = new UI();
-        parser = new Parser();
-        dukeExceptions = new DukeExceptions();
+    }
+
+    private void printLine() {
+        String boundary = " ======================================================================";
+        System.out.println(boundary);
+    }
+
+    private void printGreetingMessage() {
+        String firstGreetingMessage = " Hi! I am Duke, your next doooooorrrr friendly elf.....I mean bot";
+        String secondGreetingMessage = " How may Dukeeeeee help you today?";
+        String line = "_______________________________________________________________________";
+        System.out.println(firstGreetingMessage);
+        System.out.println(secondGreetingMessage);
+        System.out.println(line);
+    }
+
+    private void printLeavingMessage() {
+        String leavingMessage = " Bye! Duke is now a freeeeee elf again!!!!";
+        System.out.println(leavingMessage);
     }
 
     private void printList() {
@@ -31,7 +41,7 @@ public class Duke {
 
     private void list() {
         if (tasks.size() == 0) {
-            dukeExceptions.printListExceptions();
+            System.out.println(" [Warning: There are currently no tasks!]");
         } else {
             System.out.println(" Here are the list of tasks:");
             printList();
@@ -49,7 +59,7 @@ public class Duke {
         Task completedTask = tasks.get(index);
         boolean isCompleted = completedTask.getStatus();
         if (isCompleted) {
-            dukeExceptions.printDoneExceptions();
+            System.out.println(" [Warning: The task(s) has already been marked as done]");
         } else {
             completedTask.markAsDone();
             tasks.set(index, completedTask);
@@ -63,9 +73,8 @@ public class Duke {
             int index = getIndex(input);
             markAsDone(index);
         } catch(IndexOutOfBoundsException exception) {
-            dukeExceptions.printDoneIOBExceptions(input);
-        } catch (NumberFormatException exception) {
-            dukeExceptions.printDoneNFExceptions();
+            System.out.println(" [Error: " + exception + "]");
+            System.out.println(" Please check your input!");
         }
     }
 
@@ -82,21 +91,22 @@ public class Duke {
     private void getToDoExceptions(String input) {
         try {
             addToDo(input);
-        } catch (StringIndexOutOfBoundsException exception) {
-            dukeExceptions.toDoExceptions(input);
+        } catch (Exception exception) {
+            System.out.println(" [Error: " + exception + "]");
+            System.out.println(" Please check your input!");
         }
     }
 
     private void addEvent(String input) {
-        String description = input.substring(lengthOfEvent).trim();
+        String description = input.substring(5, input.length()).trim();
         int index = description.indexOf("/");
-        String details = description.substring(startIndex, index).trim();
+        String details = description.substring(0, index).trim();
         String specifier = description.substring(index + 1, index + 3);
         String time = description.substring(index + 3, description.length()).trim();
         boolean isAt = specifier.toLowerCase().equals("at");
         if (details.isEmpty() || time.isEmpty() || !isAt) {
-            String errorMessage = "Missing Event Description / Event Time / Missing parameters";
-            dukeExceptions.printEventExceptions(errorMessage, input);
+            System.out.println(" Invalid input!");
+            System.out.println(" Please check your input!");
         } else {
             newEvent = new Event(details, time);
             tasks.add(newEvent);
@@ -109,22 +119,21 @@ public class Duke {
     private void getEventExceptions(String input) {
         try {
             addEvent(input);
-        } catch (IndexOutOfBoundsException exception) {
-            String errorMessage = "Missing Event Description / Event Time & Missing parameters";
-            dukeExceptions.printEventExceptions(errorMessage, input);
+        } catch (Exception exception) {
+            System.out.println(" [Error: " + exception + "]");
+            System.out.println(" Please check your input!");
         }
     }
 
     private void addDeadline(String input) {
-        String description = input.substring(lengthOfDeadline, input.length()).trim();
+        String description = input.substring(9, input.length()).trim();
         int index = description.indexOf("/");
-        String details = description.substring(startIndex, index);
+        String details = description.substring(0, index);
         String day = description.substring(index + 3, description.length()).trim();
         String specifier = description.substring(index + 1, index + 3);
         boolean isBy = specifier.toLowerCase().equals("by");
         if (details.isEmpty() || day.isEmpty() || !isBy) {
-            String errorMessage = "Missing Event Description / Event Time / Missing parameters";
-            dukeExceptions.printDeadlineExceptions(errorMessage, input);
+            System.out.println(" Invalid input!");
         } else {
             newDeadline = new Deadline(details, day);
             tasks.add(newDeadline);
@@ -137,41 +146,42 @@ public class Duke {
     private void getDeadlineExceptions(String input) {
         try {
             addDeadline(input);
-        } catch (IndexOutOfBoundsException exception) {
-            String errorMessage = "Missing Event Description / Event Time & Missing parameters";
-            dukeExceptions.printDeadlineExceptions(errorMessage, input);
+        } catch (Exception exception) {
+            System.out.println(" [Error: " + exception + "]");
+            System.out.println(" Please check your input!");
         }
     }
 
     private void run() {
         Scanner sc = new Scanner(System.in);
-        userInterface.printGreetingMessage();
+        printGreetingMessage();
         while (true) {
             String input = sc.nextLine().trim();
-            parser.updateInput(input);
-            userInterface.printLine();
-            if (parser.isBye()) {
-                userInterface.printLeavingMessage();
-                userInterface.printLine();
+            String [] words = input.split(" ");
+            String firstWord = words[0].toLowerCase();
+            printLine();
+            if (firstWord.equals("bye")) {
+                printLeavingMessage();
+                printLine();
                 break;
-            } else if (parser.isList()) {
+            } else if (firstWord.equals("list")) {
                 list();
-            } else if (parser.isDone()) {
+            } else if (firstWord.equals("done")) {
                 getDoneExceptions (input);
-            } else if (parser.isToDo()) {
+            } else if (firstWord.equals("todo")) {
                 getToDoExceptions (input);
-            } else if (parser.isEvent()) {
+            } else if (firstWord.equals("event")) {
                 getEventExceptions(input);
-            } else if (parser.isDeadline()) {
+            } else if (firstWord.equals("deadline")) {
                 getDeadlineExceptions(input);
             } else {
                 System.out.println(" Duke does not understand your command! Can you repeat again?");
             }
-            userInterface.printLine();
+            printLine();
         }
         sc.close();
     }
-
+    
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
