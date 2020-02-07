9bd7ea432b51984ebca670ba43d2d7f49f53bc95
ngua2014 <ngua2014@uni.sydney.edu.au>
2020-02-07 16:10:11 +0800

    Level 7 completed

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -2,16 +2,33 @@ package duke;
 
 import duke.Deadline;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    private static void writeToFile(String filePath, ArrayList<Task> tasksToWrite) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        try {
+            for (int i = 0; i < tasksToWrite.size(); i++) {
+                String writtenString = String.format("%d. %s\n", i + 1, tasksToWrite.get(i).toString());
+                fw.write(writtenString);
+            }
+        } catch (IOException e){
+            System.out.println("Invalid path");
+        }
+        fw.close();
+    }
+    public static void main(String[] args) throws IOException {
 //        String logo = " ____        _        \n"
 //                + "|  _ \\ _   _| | _____ \n"
 //                + "| | | | | | | |/ / _ \\\n"
 //                + "| |_| | |_| |   <  __/\n"
 //                + "|____/ \\__,_|_|\\_\\___|\n";
 //        System.out.println("Hello from\n" + logo);
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<Task>();
         int counter = 0;
         System.out.println("Hello! I'm duke.Duke");
         System.out.println("What can I do for you?");
@@ -25,14 +42,14 @@ public class Duke {
             } else if (next.equals("list")) {
                 System.out.println("Here are the tasks on your list:");
                 for (int i = 0; i < counter; i++) {
-                System.out.printf("%d. %s\n",i +1,tasks[i].toString());
+                System.out.printf("%d. %s\n",i +1,tasks.get(i).toString());
                 }
             } else if (next.startsWith("done")){
                 String temp = next.replaceAll("\\D+","");
                 int FinishedNumber = Integer.parseInt(temp);
-                tasks[FinishedNumber-1].markAsDone();
+                tasks.get(FinishedNumber-1).markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.printf("  [%s] %s\n",tasks[FinishedNumber-1].getStatusIcon(),tasks[FinishedNumber-1].getDescription());
+                System.out.printf("  [%s] %s\n",tasks.get(FinishedNumber-1).getStatusIcon(),tasks.get(FinishedNumber-1).getDescription());
             } else {
 
                 String[] splitString = next.split(" ", 2);
@@ -47,8 +64,8 @@ public class Duke {
                             ifError = true;
                         } else{
                             gotItMessage();
-                            tasks[counter] = new Deadline(splitString2[0],splitString2[1]);
-                            System.out.printf("  %s\n",tasks[counter].toString());
+                            tasks.add(counter,new Deadline(splitString2[0],splitString2[1]));
+                            System.out.printf("  %s\n",tasks.get(counter).toString());
                         }
 
                     }
@@ -59,8 +76,8 @@ public class Duke {
                         ifError = true;
                     } else{
                         gotItMessage();
-                        tasks[counter] = new Todo(splitString[1]);
-                        System.out.printf("  %s\n",tasks[counter].toString());
+                        tasks.add(counter,new Todo(splitString[1]));
+                        System.out.printf("  %s\n",tasks.get(counter).toString());
                     }
 
                 } else if (splitString[0].equals("event")) {
@@ -74,13 +91,17 @@ public class Duke {
                             ifError = true;
                         } else {
                             gotItMessage();
-                            tasks[counter] = new Event(splitString2[0], splitString2[1]);
-                            System.out.printf("  %s\n",tasks[counter].toString());
+                            tasks.add(counter,new Event(splitString2[0], splitString2[1]));
+                            System.out.printf("  %s\n",tasks.get(counter).toString());
                         }
 
                     }
 
-                } else{
+                } else if (splitString[0].equals("save")){
+                    writeToFile("./duke.txt",tasks);
+                    counter = counter -1;
+                }
+                else{
                     System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
                     ifError = true;
 //                    tasks[counter] = new duke.Task(next);
