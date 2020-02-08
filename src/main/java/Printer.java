2bacb7a39ec0482e8150832114c89b8e85a40d24
JustinnT <e0311076@u.nus.edu>
2020-02-08 14:20:45 +0800

    added message for deadline and personalized printer

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -11,18 +11,19 @@ public class Printer {
 
     public static void printGreetings() {
         printIndentation();
-        System.out.println("Hello! I'm Duke");
+        System.out.println("Hello! I'm Duke (￣▽￣*)ゞ");
         printIndentation();
         System.out.println("What can I do for you?");
     }
 
     public static void printLines() {
         printIndentation();
+        System.out.print("---------------------------------------");
         System.out.println("---------------------------------------");
     }
 
     public static void printBye() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye ~ Hope to see you again soon! o(〒﹏〒)o");
     }
 
     public static void printConfirmationMessage(String text) {
@@ -35,7 +36,7 @@ public class Printer {
     public static void printConfirmationMessage(Task task) {
         printLines();
         printIndentation();
-        System.out.printf("Nice! I've marked this task as done:\n");
+        System.out.printf("Nice! I've marked this task as done ヽ(・∀・)ﾉ :\n");
         printIndentation();
         System.out.printf("  %s\n", task);
         printLines();
@@ -44,11 +45,23 @@ public class Printer {
     public static void printConfirmationMessage(ToDo task) {
         printLines();
         printIndentation();
-        System.out.println("Got it. I've added this task:");
+        System.out.println("Got it! I've added this task ＠＾◡＾) :");
         printIndentation();
         System.out.println("  " + task);
+        printIndentation();
+        System.out.printf("Now you have %d tasks in the list\n", Storage.getSize());
         printLines();
+    }
 
+    public static void printConfirmationMessage(Deadline task) {
+        printLines();
+        printIndentation();
+        System.out.println("Oh a deadline huh! Don't worry, I have added this task <(￣︶￣)> :");
+        printIndentation();
+        System.out.println("  " + task);
+        printIndentation();
+        System.out.printf("Now you have %d tasks in the list\n", Storage.getSize());
+        printLines();
     }
 
     public static void printTasks(List<Task> myList) {
@@ -69,7 +82,7 @@ public class Printer {
     public static void printError() {
         printLines();
         printIndentation();
-        System.out.println("Error.. ;-;");
+        System.out.println("Error.. ٩(× ×)۶");
         printLines();
     }
 }
