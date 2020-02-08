4529e210ce9aa105c7550ead12d31469305667a6
jinfayap <jinfayap@gmail.com>
2020-02-09 00:20:35 +0800

    Add Level-5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,7 @@ public class Duke {
     private static final String DIVIDER = "____________________________________________________________";
     private static final String LS = System.lineSeparator();
     private static final String TAB = "\t";
+    private static final String EMPTY_STRING = "";
     
     private static final String WELCOME_MESSAGE = "Hello! I'm Duke" + LS + "What can i do for you?";
     
@@ -13,7 +14,7 @@ public class Duke {
     private static final String COMMAND_LIST_MESSAGE = "Here are the tasks in your list:";
     
     private static final String COMMAND_BLAH_WORD = "blah";
-    private static final String COMMAND_BLAH_MESSAGE = TAB + "blah";
+    private static final String COMMAND_BLAH_MESSAGE = "I'm sorry, but I don't know what that means :-(";
     
     private static final String COMMAND_BYE_WORD = "bye";
     private static final String COMMAND_BYE_MESSAGE = TAB + "Bye. Hope to see you again soon!";
@@ -27,10 +28,19 @@ public class Duke {
     private static final String COMMAND_ADD_MESSAGE =
             "Got it. I've added this task:" + LS + TAB + "%s" + LS + "Now " + "you have %d task(s) in the list.";
     
-    private static final String COMMAND_INVALID_MESSAGE = "Command is invalid" + LS + "Please try another command";
+    private static final String COMMAND_INVALID_MESSAGE = "Command is invalid, " + "please try another command";
     private static final String COMMAND_INVALID_INDEX = "Index is invalid";
     
+    private static final String ENTER_A_COMMAND = "Enter a command: ";
+    private static final String COMMAND_ENTERED = "Command entered: ";
+    
+    private static final String MISSING_DATE_MESSAGE = "OOPS!!! The date of a %s cannot be empty.";
+    private static final String MISSING_DESCRIPTION_MESSAGE = "OOPS!!! The description of a %s cannot be empty.";
+    private static final String SPILT_BY_SPACE = "\\s+";
+    private static final String SPILT_BY_SLASH = "/";
+    
     private static Scanner in = new Scanner(System.in);
+    
     
     private static final int MAX_CAPACITY = 100;
     private static Task[] tasks = new Task[MAX_CAPACITY];
@@ -47,12 +57,22 @@ public class Duke {
        https://github.com/nus-cs2113-AY1920S2/contacts/blob/master/src/main/java/Contacts1.java
      */
     private static void executeCommand(String userInput) {
-        String[] commandTypeAndParams = splitInputLine(userInput, "\\s+");
-        String[] paramAndDate = splitInputLine(commandTypeAndParams[1], "/");
+        String[] commandTypeAndParams = splitInputLine(userInput, SPILT_BY_SPACE);
+        String[] paramAndDate = splitInputLine(commandTypeAndParams[1], SPILT_BY_SLASH);
         String commandWord = commandTypeAndParams[0];
         String commandArgs = paramAndDate[0];
         String commandDate = paramAndDate[1];
         
+        try {
+            hasEmptyDescription(commandWord, commandArgs);
+            hasEmptyDate(commandWord, commandDate);
+            operateCommand(commandWord, commandArgs, commandDate);
+        } catch (DukeException e) {
+            System.out.println(e);
+        }
+    }
+    
+    private static void operateCommand(String commandWord, String commandArgs, String commandDate) {
         switch (commandWord.toLowerCase()) {
         case COMMAND_TODO_WORD:
             tasks[Task.getTaskCount()] = new Todo(commandArgs);
@@ -79,19 +99,33 @@ public class Duke {
             displayBlahMessage();
             break;
         default:
-            System.out.println(DIVIDER + LS + COMMAND_INVALID_MESSAGE + LS + DIVIDER);
+            displayInvalidMessage();
+        }
+    }
+    
+    private static void hasEmptyDescription(String commandWord, String commandArgs) throws DukeException {
+        if ((commandWord.equals(COMMAND_TODO_WORD) || commandWord.equals(COMMAND_DEADLINE_WORD) ||
+                commandWord.equals(COMMAND_EVENT_WORD)) && commandArgs.equals("")) {
+            throw new DukeException(String.format(MISSING_DESCRIPTION_MESSAGE, commandWord));
+        }
+    }
+    
+    private static void hasEmptyDate(String commandWord, String commandDate) throws DukeException {
+        if ((commandWord.equals(COMMAND_DEADLINE_WORD) || commandWord.equals(COMMAND_EVENT_WORD)) &&
+                commandDate.equals("")) {
+            throw new DukeException(String.format(MISSING_DATE_MESSAGE, commandWord));
         }
     }
     
     private static String getUserInput() {
-        System.out.print("Enter a command: ");
+        System.out.print(ENTER_A_COMMAND);
         String inputLine = in.nextLine();
         echoUserInput(inputLine.trim());
         return inputLine.trim();
     }
     
     private static void echoUserInput(String userInput) {
-        System.out.println("Command entered: " + userInput);
+        System.out.println(COMMAND_ENTERED + userInput);
     }
     
     /* Solution below adapted from
@@ -107,25 +141,43 @@ public class Duke {
     }
     
     private static void displayAddMessage() {
+        
         System.out.println(DIVIDER);
         System.out.println(String.format(COMMAND_ADD_MESSAGE, tasks[Task.getTaskCount() - 1], Task.getTaskCount()));
         System.out.println(DIVIDER);
     }
     
     private static void displayDoneMessage(String index) {
-        int doneTaskIndex = Integer.parseInt(index) - 1;
-        if (Task.getTaskCount() <= doneTaskIndex) {
-            System.out.println(DIVIDER + LS + COMMAND_INVALID_INDEX + LS + DIVIDER);
-        } else {
+        try {
+            int doneTaskIndex = Integer.parseInt(index) - 1;
+            if (doneTaskIndex >= Task.getTaskCount()) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
             tasks[doneTaskIndex].markAsDone();
             System.out.println(DIVIDER + LS + COMMAND_DONE_MESSAGE);
             System.out.println(TAB + tasks[doneTaskIndex]);
+            System.out.println(DIVIDER);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(DIVIDER + LS + COMMAND_INVALID_INDEX + LS + DIVIDER);
+        }
+    }
+    
+    private static void displayInvalidMessage() {
+        try {
+            throw new DukeException(COMMAND_INVALID_MESSAGE);
+        } catch (DukeException e) {
+            System.out.println(DIVIDER);
+            System.out.println(e);
             System.out.println(DIVIDER);
         }
     }
     
     private static void displayBlahMessage() {
-        System.out.println(DIVIDER + LS + COMMAND_BLAH_MESSAGE + LS + DIVIDER);
+        try {
+            throw new DukeException(COMMAND_BLAH_MESSAGE);
+        } catch (DukeException e) {
+            System.out.println(e);
+        }
     }
     
     private static void displayByeMessageAndExit() {
@@ -135,13 +187,14 @@ public class Duke {
     
     private static void displayTaskList() {
         System.out.println(DIVIDER);
-        if (Task.getTaskCount() > 0) {
-            System.out.println(COMMAND_LIST_MESSAGE);
-            for (int i = 0; i < Task.getTaskCount(); ++i) {
-                System.out.println((i + 1) + "." + tasks[i]);
-            }
-        } else {
+        if (Task.getTaskCount() == 0) {
             System.out.println(COMMAND_LIST_EMPTY);
+            System.out.println(DIVIDER);
+            return;
+        }
+        System.out.println(COMMAND_LIST_MESSAGE);
+        for (int i = 0; i < Task.getTaskCount(); ++i) {
+            System.out.println((i + 1) + "." + tasks[i]);
         }
         System.out.println(DIVIDER);
     }
