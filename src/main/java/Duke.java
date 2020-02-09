9fabfc00df2c761023513fe3f11556ca3fea4148
nigellenl <nigelle.nl@gmail.com>
2020-02-09 23:16:43 +0800

    Add Code Quality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,75 +2,83 @@ import java.util.Scanner;
 
 public class Duke {
     public static final int CAPACITY = 100;
+    public static final String BORDER = "____________________________________________________________";
+    public static final int TODO = 5;
+    public static final int DEADLINE = 9;
+    public static final int EVENT = 6;
+    public static final int DONE = 5;
+    public static final int DATE = 3;
 
     public static void main(String[] args) {
         printWelcomeMessage();
-
         Task[] tasks = new Task[CAPACITY];
-        int counter = 0;
+        int taskCounter = 0;
+        Scanner input = new Scanner(System.in);
 
         while(true) {
             String line;
-            Scanner input = new Scanner(System.in);
             line = input.nextLine();
-
             if (line.equalsIgnoreCase("bye")) {
                 printByeMessage();
                 break;
             } else if (line.equalsIgnoreCase("list")) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < counter; i++){
-                    System.out.println(i+1 + ". " + tasks[i].toString());
-                }
-                System.out.println("____________________________________________________________");
+                printList(tasks, taskCounter);
             } else if (line.startsWith("todo")) {
-                line = line.replaceAll("todo ", "");
+                line = line.substring(TODO);
                 Task t = new Todo(line);
-                tasks[counter] = t;
-                printAcknowledgement(tasks[counter], counter);
-                counter++;
+                tasks[taskCounter] = t;
+                printAcknowledgement(tasks[taskCounter], taskCounter);
+                taskCounter++;
             } else if (line.startsWith("deadline")) {
                 String[] words = line.split("/");
-                String description = words[0].substring(9);
-                String by = words[1].substring(3);
+                String description = words[0].substring(DEADLINE);
+                String by = words[1].substring(DATE);
                 Task d = new Deadline(description, by);
-                tasks[counter] = d;
-                printAcknowledgement(tasks[counter], counter);
-                counter++;
+                tasks[taskCounter] = d;
+                printAcknowledgement(tasks[taskCounter], taskCounter);
+                taskCounter++;
             } else if (line.startsWith("event")) {
                 String[] words = line.split("/");
-                String description = words[0].substring(6);
-                String at = words[1].substring(3);
+                String description = words[0].substring(EVENT);
+                String at = words[1].substring(DATE);
                 Task e = new Event(description, at);
-                tasks[counter] = e;
-                printAcknowledgement(tasks[counter], counter);
-                counter++;
+                tasks[taskCounter] = e;
+                printAcknowledgement(tasks[taskCounter], taskCounter);
+                taskCounter++;
             } else if (line.startsWith("done")) {
-                String strNumber = line.substring(5);
-                int number = Integer.parseInt(strNumber);
-                tasks[number-1].markAsDone();
-                System.out.println("____________________________________________________________");
-                System.out.println("Nice! I've marked this task as done: " + tasks[number-1].description);
-                System.out.println("____________________________________________________________");
+                String number = line.substring(DONE);
+                int taskNumber = Integer.parseInt(number);
+                tasks[taskNumber-1].markAsDone();
+                System.out.println(BORDER);
+                System.out.println("Nice! I've marked this task as done: " + tasks[taskNumber-1].description);
+                System.out.println(BORDER);
             } else {
                 System.out.println("error");
             }
         }
     }
 
+    private static void printList(Task[] tasks, int taskCounter) {
+        System.out.println(BORDER);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < taskCounter; i++){
+            System.out.println(i+1 + ". " + tasks[i].toString());
+        }
+        System.out.println(BORDER);
+    }
+
     private static void printAcknowledgement(Task task, int counter) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Got it. I've added this task: ");
+        System.out.println(BORDER);
+        System.out.println("Got it! I've added this task: ");
         System.out.println(task.toString());
-        System.out.println("Now you have " + (counter + 1) + " tasks in your list.");
-        System.out.println("____________________________________________________________");
+        System.out.println("Now you have " + (counter + 1) + " tasks in your list!");
+        System.out.println(BORDER);
     }
 
     private static void printByeMessage() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(BORDER);
+        System.out.println("Bye! Hope to see you again soon!");
+        System.out.println(BORDER);
     }
 
     private static void printWelcomeMessage() {
@@ -80,9 +88,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
+        System.out.println(BORDER);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(BORDER);
     }
 }
