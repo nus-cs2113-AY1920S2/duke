951b3e908ddfaa63f3249794948a6d9052c8a3cf
jinfayap <jinfayap@gmail.com>
2020-02-09 17:29:35 +0800

    Update Function

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -2,12 +2,15 @@ package duke.exception;
 
 public class DukeException extends Exception {
     
+    private static final String DIVIDER = "____________________________________________________________";
+    private static final String LS = System.lineSeparator();
+    
     public DukeException(String message) {
         super(message);
     }
     
     @Override
     public String toString() {
-        return getMessage();
+        return  DIVIDER + LS +  getMessage() + LS + DIVIDER;
     }
 }
