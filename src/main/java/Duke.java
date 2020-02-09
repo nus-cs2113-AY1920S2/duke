22f80e7bf64897724e5297cb7f1b1203f2b9fe2e
JLoh579 <joshualoh579@yahoo.com>
2020-02-09 14:18:28 +0800

    Add support for tracking three types of tasks: todos, deadlines, events

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,51 +1,116 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final String FOUR_SPACE_INDENT = "    ";
+    private static final String SIX_SPACE_INDENT = "      ";
+    private static Task[] tasks = new Task[100];
+    private static int taskCounter = 0;
+    private static int taskIndex;
+    private static String command;
+    private static Scanner in = new Scanner(System.in);
+    private static boolean isExiting = false;
+
     public static void main(String[] args) {
         greet();
-        executeCommands();
+        takeCommands();
         exit();
     }
 
-    private static void executeCommands() {
-        Task[] taskList = new Task[100];
-        int taskCounter = 0;
-        int taskIndex;
-        String input;
-        String line;
-        Scanner in = new Scanner(System.in);
-        while(true) {
-            input = in.nextLine();
-            if (input.equals("bye")) {
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("    Here are the tasks in your list:");
-                for (int i = 0; i < taskCounter; i++) {
-                    line = "    " + Integer.toString(i+1) + ".[" + taskList[i].getStatusIcon() + "] " +
-                            taskList[i].getDescription();
-                    System.out.println(line);
-                }
-            } else if (input.substring(0,4).equals("done")) {
-                 taskIndex = Integer.parseInt(input.substring(5)) - 1;
-                 taskList[taskIndex].markAsDone();
-                 line = "      " + "[" + taskList[taskIndex].getStatusIcon() + "] " +
-                         taskList[taskIndex].getDescription();
-                 System.out.println("    Nice! I've marked this task as done:");
-                 System.out.println(line);
-            } else {
-                taskList[taskCounter] = new Task(input);
-                System.out.println("    added: " + input);
-                taskCounter++;
-            }
+    private static void takeCommands() {
+        while(!isExiting) {
+            command = getCommand();
+            executeCommand(command);
         }
     }
 
+    private static void executeCommand(String command) {
+        if (command.equals("bye")) {
+            isExiting = true;
+        } else if (command.equals("list")) {
+            listTasks();
+        } else if (command.substring(0,4).equals("done")) {
+            checkOffTask(command);
+        } else if (command.substring(0,4).equals("todo")) {
+            addTodo(command.substring(5));
+        } else if (command.substring(0,8).equals("deadline")) {
+            addDeadline(command.substring(9));
+        } else if (command.substring(0,5).equals("event")) {
+            addEvent(command.substring(6));
+        }
+    }
+
+    private static void addEvent(String info) {
+        int dividerIndex = info.indexOf("/");
+        String description = info.substring(0, (dividerIndex - 1));
+        String at = info.substring(dividerIndex + 4);
+        tasks[taskCounter] = new Event(description, at);
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Got it. I've added this task:");
+        System.out.print(SIX_SPACE_INDENT);
+        System.out.println(tasks[taskCounter].toString());
+        taskCounter++;
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Now you have " + taskCounter + " tasks in the list.");
+    }
+
+    private static void addDeadline(String info) {
+        int dividerIndex = info.indexOf("/");
+        String description = info.substring(0, (dividerIndex - 1));
+        String by = info.substring(dividerIndex + 4);
+        tasks[taskCounter] = new Deadline(description, by);
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Got it. I've added this task:");
+        System.out.print(SIX_SPACE_INDENT);
+        System.out.println(tasks[taskCounter].toString());
+        taskCounter++;
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Now you have " + taskCounter + " tasks in the list.");
+    }
+
+    private static void addTodo(String description) {
+        tasks[taskCounter] = new ToDo(description);
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Got it. I've added this task:");
+        System.out.print(SIX_SPACE_INDENT);
+        System.out.println(tasks[taskCounter].toString());
+        taskCounter++;
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Now you have " + taskCounter + " tasks in the list.");
+    }
+
+    private static void checkOffTask(String command) {
+        taskIndex = Integer.parseInt(command.substring(5)) - 1;
+        tasks[taskIndex].markAsDone();
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.print(SIX_SPACE_INDENT);
+        System.out.println(tasks[taskIndex].toString());
+    }
+
+    private static void listTasks() {
+        int bulletNum;
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Here are the tasks in your list:");
+        for (taskIndex = 0; taskIndex < taskCounter; taskIndex++) {
+            bulletNum = taskIndex + 1;
+            System.out.print(FOUR_SPACE_INDENT);
+            System.out.println(bulletNum + "." + tasks[taskIndex].toString());
+        }
+    }
+
+    private static String getCommand() {
+        return in.nextLine();
+    }
+
     private static void exit() {
-        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
     private static void greet() {
-        System.out.println("    Hello I'm Yapyap.");
-        System.out.println("    What can I do for you?");
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("Hello I'm Yapyap.");
+        System.out.print(FOUR_SPACE_INDENT);
+        System.out.println("What can I do for you?");
     }
 }
