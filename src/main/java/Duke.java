20b1a87be201f58c8015db609df7645427867fd1
synCKun <>
2020-02-10 23:34:58 +0800

    Implement level 2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -7,17 +8,33 @@ public class Duke {
                 "     What can I do for you?\n" +
                 "    ____________________________________________________________\n");
         Scanner scanner = new Scanner(System.in);
-        String command = scanner.next();
 
-        while (!command.equals("bye")) {
-            System.out.println("    ____________________________________________________________\n     " +
-                    command + "\n    ____________________________________________________________\n");
-            command = scanner.next();
+        ArrayList<String> list = new ArrayList<String>(100);
+        while (true) {
+            String command = scanner.nextLine();
+
+            switch (command) {
+            case "list":
+                System.out.println("    ____________________________________________________________");
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println("     " + (i+1) + ". " + list.get(i));
+                }
+                System.out.print("    ____________________________________________________________\n");
+                break;
+            case "bye":
+                System.out.println("    ____________________________________________________________\n" +
+                        "     Bye. Hope to see you again soon!\n" +
+                        "    ____________________________________________________________");
+                return;
+            default:
+                System.out.println("    ____________________________________________________________\n     added: " +
+                        command + "\n    ____________________________________________________________\n");
+                list.add(command);
+                break;
+            }
         }
 
-        System.out.println("    ____________________________________________________________\n" +
-                "     Bye. Hope to see you again soon!\n" +
-                "    ____________________________________________________________");
+
     }
 }
 
