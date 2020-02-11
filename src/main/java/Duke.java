da534c2e93eca9d5f61331ae30d921b51b916a98
chengTzeNing <09nhnccgtzening@gmail.com>
2020-02-11 16:55:07 +0800

    Throw exceptions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,6 +11,32 @@ public class Duke {
         }
     }
 
+    public static void inputChecker(String userInput) throws IndexOutOfBoundsException, IllegalArgumentException {
+        if (userInput.startsWith("list")) {
+            printTasks();
+        } else if (userInput.startsWith("done")) {
+            tasks.get(Integer.parseInt(Character.toString(
+                    userInput.charAt(userInput.length() - 1))) - 1).setDone();
+        } else if (userInput.startsWith("todo")) {
+            Task task = new ToDo(userInput.substring(5));
+            tasks.add(task);
+            System.out.println("Got it. I've added this task:\n  " + task.toString()
+                    + String.format("\nNow you have %d tasks in the list.", Task.getTotalNumOfTasks()));
+        } else if (userInput.startsWith("deadline")) {
+            Task task = new Deadline(userInput.substring(9));
+            tasks.add(task);
+            System.out.println("Got it. I've added this task:\n  " + task.toString()
+                    + String.format("\nNow you have %d tasks in the list.", Task.getTotalNumOfTasks()));
+        } else if (userInput.startsWith("event")) {
+            Task task = new Event(userInput.substring(6));
+            tasks.add(task);
+            System.out.println("Got it. I've added this task:\n  " + task.toString()
+                    + String.format("\nNow you have %d tasks in the list.", Task.getTotalNumOfTasks()));
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
     public static void printLogo() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | __ __ \n"
@@ -26,30 +52,24 @@ public class Duke {
         Scanner scan = new Scanner(System.in);
         String userInput = scan.nextLine();
         while (!userInput.equals("bye")) {
-            //String[] splitUpInput = userInput.split(" ");
-            if (userInput.startsWith("list")) {
-                printTasks();
-            } else if (userInput.startsWith("done")) {
-                tasks.get(Integer.parseInt(Character.toString(
-                        userInput.charAt(userInput.length() - 1))) - 1).setDone();
-            } else if (userInput.startsWith("todo")) {
-                Task task = new ToDo(userInput.substring(5));
-                tasks.add(task);
-                System.out.println("Got it. I've added this task:\n  " + task.toString()
-                        + String.format("\nNow you have %d tasks in the list.", Task.getTotalNumOfTasks()));
-            } else if (userInput.startsWith("deadline")) {
-                Task task = new Deadline(userInput.substring(9));
-                tasks.add(task);
-                System.out.println("Got it. I've added this task:\n  " + task.toString()
-                        + String.format("\nNow you have %d tasks in the list.", Task.getTotalNumOfTasks()));
-            } else if (userInput.startsWith("event")) {
-                Task task = new Event(userInput.substring(6));
-                tasks.add(task);
-                System.out.println("Got it. I've added this task:\n  " + task.toString()
-                        + String.format("\nNow you have %d tasks in the list.", Task.getTotalNumOfTasks()));
+            try {
+                inputChecker(userInput);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("You have given the wrong number of arguments\n" +
+                        "Please try again or input \"bye\" to exit");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Sorry I don't understand that command. Here is the list of commands available\n"
+                        + "\"bye\": to exit\n\"list\": to show the list of all your tasks\n\"todo\": add a todo\n" +
+                        "\"deadline\": add a deadline\n\"event\': add an event\n" +
+                        "\"done\": check off a task on your list");
             }
-            userInput = scan.nextLine(); 
+            userInput = scan.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
+
+// should have:
+// invalid command, like no such starting word
+// too few arguments
+// incorrect format like "/by" -> "by" or sth
