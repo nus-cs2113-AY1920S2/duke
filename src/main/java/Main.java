d488dd6bb18edc06ce8565ad89da81bd7962210e
Joseph Lim <limweijie@live.com.sg>
2020-02-12 00:12:35 +0800

    Refactor Main class.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,23 @@ import duke.task.InvalidTaskArgumentException;
 import command.CommandResult;
 import command.ExitCommand;
 import command.InvalidCommandException;
+
+import java.io.File;
+import java.io.IOException;
+
 import command.Command;
 import parser.Parser;
+import storage.InvalidStorageFilePathException;
+import storage.Storage;
 import ui.Ui;
 
 public class Main {
 
     private Ui ui;
     private Duke duke;
+    private Storage storage;
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         new Main().run(args);
     }
     
@@ -23,9 +30,18 @@ public class Main {
     }
     
     public void start(String[] args) {
-        this.ui = new Ui();
-        this.duke = new Duke();
-        ui.displayWelcomeMessage();
+        try {
+            this.ui = new Ui();
+            this.storage = new Storage("storage.txt");
+            
+            this.duke = new Duke(storage.load());
+
+            ui.displayWelcomeMessage();
+        } catch (InvalidStorageFilePathException e) {
+            ui.displayErrorMessage(e.getMessage());
+        } catch (IOException e) {
+            ui.displayErrorMessage(e.getMessage());
+        }
     }
     
     private void runCommandUntilExit() {                     
@@ -37,6 +53,14 @@ public class Main {
             try {  
                 command = new Parser().parseCommand(commandText);
                 CommandResult output = command.execute(this.duke);
+                
+                try {
+                    storage.save("C:\\Users\\limwe\\OneDrive\\Documents\\GitHub\\duke\\src\\main\\java\\data\\storage.txt", this.duke.getTaskList());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } 
+                
                 ui.displayOutputMessage(output);
                 
                 if (ExitCommand.isExit(command)) {
@@ -58,5 +82,12 @@ public class Main {
     private void exit() {
         ui.displayExitMessage();
         System.exit(0);
-    }       
+    } 
+    
+    /*
+    private Storage initializeStorage(String[] args) {
+        boolean hasExistingStorage = args.length > 0;
+        return hasExistingStorage ? new Storage(args[0]) : new Storage();
+    }
+    */
 }
--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,13 +1,17 @@
 package duke;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import duke.task.Task;
 import duke.task.TaskList;
 
 public class Duke {
     private final TaskList taskList;
     
-    public Duke(TaskList newTaskList) {
-        this.taskList = new TaskList(newTaskList);
+    public Duke(TaskList newTaskList) {  
+        
+        this.taskList = new TaskList(newTaskList);       
     }
     
     public Duke() {
