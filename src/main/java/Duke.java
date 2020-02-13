8e898b31e84b3dd345242c80adba563a3bdbb870
chengTzeNing <09nhnccgtzening@gmail.com>
2020-02-13 16:33:47 +0800

    Change "Duke" logo to "Hi"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,11 +38,16 @@ public class Duke {
     }
 
     public static void printLogo() {
-        String logo = " ____        _        \n"
+        /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | __ __ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+
+         */
+        String logo = "|  |  \n" +
+                "|--| .\n" +
+                "|  | |\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke\nWhat can I do for you?");
     }
