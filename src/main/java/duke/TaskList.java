feb0e3ebb378f70d87682253b8613abb4bbc7901
lwxymere <chaoscold@hotmail.com>
2020-02-13 14:18:48 +0800

    Change array to ArrayList

--- src/main/java/duke/TaskList.java
+++ src/main/java/duke/TaskList.java
@@ -2,38 +2,37 @@ package duke;
 
 import duke.task.Task;
 
+import java.util.ArrayList;
+
 public class TaskList {
-    // assume list holds a maximum of 100 tasks
-    private Task[] list;
-    private int numTasks;
+    private ArrayList<Task> tasks;
 
     public TaskList() {
-        this.list = new Task[100];
-        this.numTasks = 0;
+        this.tasks = new ArrayList<Task>();
     }
 
     public void addTask(Task task) {
-        this.list[numTasks] = task;
-        numTasks++;
+        tasks.add(task);
         System.out.println("Task added:");
         System.out.println("  " + task);
-        System.out.println("You have " + numTasks + " task" + (numTasks == 1 ? "" : "s") + " in the list");
+        System.out.println("You have " + tasks.size() + " task" +
+                (tasks.size() == 1 ? "" : "s") + " in the list");
     }
 
     public void showTasks() {
-        if (numTasks == 0) {
+        if (tasks.size() == 0) {
             System.out.println("Nice, nothing to see here");
         } else {
             System.out.println("Quite a few tasks you got there");
-            for (int i = 0; i < numTasks; ++i) {
-                System.out.printf("%3d. %s\n", i + 1, list[i]);
+            for (int i = 0; i < tasks.size(); ++i) {
+                System.out.printf("%3d. %s%n", i + 1, tasks.get(i));
             }
         }
     }
 
     public void markTaskAsDone(int index) {
-        list[index].markAsDone();
+        tasks.get(index).markAsDone();
         System.out.println("Well, that's one task down");
-        System.out.println("  " + list[index]);
+        System.out.println("  " + tasks.get(index));
     }
 }
