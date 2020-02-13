a53c341872f8834b5a1023f55d838089b6ede677
Yuxiang <yuxianglim1227@gmail.com>
2020-02-13 18:17:07 +0800

    Level 5 + Exceptions class + Major bug fixes

---
+++ src/main/java/Duke/Events.java
@@ -0,0 +1,20 @@
+package Duke;
+
+public class Events extends Task {
+    private String at;
+
+    public Events(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    public String getEvent(){
+        return at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + "(at: " + getEvent() + ")" ;
+    }
+
+}
---
+++ src/main/java/Duke/Todo.java
@@ -0,0 +1,16 @@
+package Duke;
+
+public class Todo extends Task {
+    private String description;
+
+    public Todo(String description){
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+
+}
+
