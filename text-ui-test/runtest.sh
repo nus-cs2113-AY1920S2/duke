5fe17301f7304f9154f7ab91f3edd38de85798c9
Zhilin <huangzhilin1999@gmail.com>
2020-02-13 14:04:19 +0800

    Update text UI testing for file save function

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -19,6 +19,9 @@ then
     exit 1
 fi
 
+cat ../data/chattychatbotstarttemplate.txt | pbcopy
+pbpaste > ../data/chattychatbot.txt
+
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
 java -classpath ../bin chatty.ChattyChatBot < input.txt > ACTUAL.TXT
 
