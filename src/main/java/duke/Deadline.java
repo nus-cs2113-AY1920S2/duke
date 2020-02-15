9ce36a2e9c889fb4d0f473a295fe2802d81f1dfe
JeremiasLiew <jeremias.liew@hotmail.com>
2020-02-15 20:21:24 +0800

    A-Packages

---
+++ src/main/java/duke/Deadline.java
@@ -0,0 +1,16 @@
+package java.duke;
+
+public class Deadline extends Task {
+    private final String deadline;
+    
+    public Deadline(String description, String deadline) {
+        super(description);
+        this.deadline = deadline;
+    }
+    
+    @Override
+    public String toString() {
+        return "[D][" + this.getStatusIcon() + "] " + this.description
+            + "(by: " + this.deadline + ")";
+    }
+}
---
+++ src/main/java/duke/DukeException.java
@@ -0,0 +1,17 @@
+package java.duke;
+
+public class DukeException extends Exception {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    public DukeException() {
+        super();
+    }
+    
+    public DukeException(String message) {
+        super(message);
+    }
+}
