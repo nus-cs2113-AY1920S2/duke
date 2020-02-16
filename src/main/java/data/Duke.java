ec96bc311c43af15f53ed9edd7e2951131775ee7
brandoncjh <brandonchongjh@gmail.com>
2020-02-16 22:18:28 +0800

    added load/save taskList to text file functionality

--- src/main/java/data/Duke.java
+++ src/main/java/data/Duke.java
@@ -1,5 +1,13 @@
-package data; //Scanner object takes in user input
-import java.util.Scanner;
+package data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner; //Scanner object takes in user input
+import java.util.ArrayList;
 
 public class Duke {
     public static final int MAX_NO_OF_TASKS = 100;
@@ -48,13 +56,18 @@ public class Duke {
             // get additional remark part of userInput
             returnValue[1] = separatedSections[1];
             boolean isRemarksEmpty = ((commandWord.equals("event") || commandWord.equals("deadline")) 
-            && returnValue[1].trim().length() == 0);
+                    && returnValue[1].trim().length() == 0);
             if (isRemarksEmpty){
                 throw new NoRemarkException();
             }
             return returnValue;
         } else {
             // get description part of userInput without the command word
+            String[] separatedSections = originalInput.split(" /");
+            String commandWord = separatedSections[0].split(" ", 2)[0];
+            if (commandWord.equals("event") || commandWord.equals("deadline")) {
+                throw new IllegalKeywordException();
+            }
             try {
                 returnValue[0] = originalInput.trim().split(" ", 2)[1];
             } catch (ArrayIndexOutOfBoundsException e) {
@@ -70,7 +83,7 @@ public class Duke {
 
     
 
-    public static void addTaskToList(Task[] listInput) {
+    public static void printTaskList(ArrayList<Task> listInput) {
         //if list empty, inform user and await next command
         if (taskCount == 0) {
             System.out.println(underscoredLine + System.lineSeparator() + "\tThe list is empty."
@@ -80,13 +93,13 @@ public class Duke {
         //if list non-empty, print out all existing tasks
         System.out.println(underscoredLine + System.lineSeparator());
         for (int i = 0; i < taskCount; i++) {
-            System.out.println("\t" + Integer.toString(i + 1) + "." + listInput[i].toString());
+            System.out.println("\t" + Integer.toString(i + 1) + "." + listInput.get(i).toString());
         }
         System.out.println(underscoredLine);
         return;
     }
 
-    public static void updateTaskDone(String taskNumberInput, Task[] listInput){
+    public static void updateTaskDone(String taskNumberInput, ArrayList<Task> listInput){
         int queryNumber = Integer.parseInt(taskNumberInput);
         boolean isOutOfRange = queryNumber < 1 || queryNumber > taskCount;
         
@@ -97,22 +110,22 @@ public class Duke {
             return;
         }
         //handle case where user tries to mark as done an already completed task
-        boolean isTaskAlreadyDone = listInput[queryNumber-1].getIsDone();
+        boolean isTaskAlreadyDone = listInput.get(queryNumber-1).getIsDone();
         
         if (isTaskAlreadyDone){
             System.out.println(underscoredLine + System.lineSeparator()
                     + "\tThis task has already been marked completed." + System.lineSeparator() + underscoredLine);
             return;
         }
-        listInput[queryNumber-1].markAsDone();
+        listInput.get(queryNumber-1).markAsDone();
 
         String taskDoneMessage = "\tGreat job! I've marked this task as done:\n\t" + Integer.toString(queryNumber)
-                + ".[" + listInput[queryNumber-1].getStatusIcon() + "] " + listInput[queryNumber-1].getDescription();
+                + ".[" + listInput.get(queryNumber-1).getStatusIcon() + "] " + listInput.get(queryNumber-1).getDescription();
         System.out.println(underscoredLine + System.lineSeparator() + taskDoneMessage + System.lineSeparator()
                 + underscoredLine);
     }
 
-    private static void insertNewTask(Task[] taskList, String userInput, String[] tokenizedInput) throws
+    private static void insertNewTask(ArrayList<Task> taskList, String userInput, String[] tokenizedInput) throws
             IllegalKeywordException, NoDescriptionException, NoRemarkException {
         Task newTask;
         switch (tokenizedInput[0]) {
@@ -130,7 +143,7 @@ public class Duke {
             //break;
         }
 
-        taskList[taskCount] = newTask;
+        taskList.add(newTask);
         taskCount++;
 
         String taskAddedMessage = "\tGot it. I've added this task: \n\t" + newTask.toString() + System.lineSeparator()
@@ -139,17 +152,86 @@ public class Duke {
                 + underscoredLine);
     }
 
+    //TODO: load tasklist from file data/duke.txt on startup
+    private static void loadFileToTaskList(String filePath, ArrayList<Task> taskList) throws FileNotFoundException {
+        File f = new File(filePath);
+        if (!f.exists()) {
+            throw new FileNotFoundException();
+        }
+        Scanner s = new Scanner(f);
+        while (s.hasNext()) {
+            //add task (each line) to ArrayList taskList
+            //1. process each line first, construct new Todo/Event/Deadline object
+            String taskString = s.nextLine();
+            String[] tokenizedTaskString = taskString.split(" \\| ");
+            Task newTaskToLoad;
+            switch(tokenizedTaskString[0].toUpperCase()) {
+            case ("T"):
+                newTaskToLoad = new Todo(tokenizedTaskString[2], "");
+                break;
+            case ("E"):
+                newTaskToLoad = new Event(tokenizedTaskString[2], tokenizedTaskString[3]);
+                break;
+            case ("D"):
+                newTaskToLoad = new Deadline(tokenizedTaskString[2], tokenizedTaskString[3]);
+                break;
+            default:
+                newTaskToLoad = new Todo("hello", "world");
+                //break;
+            }
+            //if task was previously marked done already, make sure to mark it as done when loading to taskList
+            if (tokenizedTaskString[1].equals("1")) {
+                newTaskToLoad.markAsDone();
+            }
+            taskList.add(newTaskToLoad);
+            taskCount++;
+
+        }
+    }
+
+    //TODO: write loop to iterate through arraylist, write each task to file data/duke.txt on any changes
+    private static void saveTaskListToFile(String filePath, ArrayList<Task> taskList) throws IOException {
+        FileWriter fw;
+        try{
+            fw = new FileWriter(filePath); //overwrites existing file contents every time it is called
+        } catch (IOException e) {
+            throw new IOException();
+        }
+        //convert newTaskData to string format for storing in duke.txt
+        String newTaskString;
+        boolean hasDescription = false;
+
+        for (int i=0; i < taskList.size(); i++) {
+            Task newTaskData = taskList.get(i);
+            if (newTaskData instanceof Todo) {
+                newTaskString = newTaskData.getTaskData()[0] + " | " + newTaskData.getTaskData()[1] + " | "
+                        + newTaskData.getTaskData()[2] + System.lineSeparator();
+            } else {
+                newTaskString = newTaskData.getTaskData()[0] + " | " + newTaskData.getTaskData()[1] + " | "
+                        + newTaskData.getTaskData()[2] + " | " + newTaskData.getTaskData()[3] + System.lineSeparator();
+            }
+            fw.write(newTaskString);
+
+        }
+        fw.close();
+
+    }
+
     public static void main(String[] args) {
-        Task[] taskList = new Task[MAX_NO_OF_TASKS];
-        int taskCount = 0;
         String userInput;
         Scanner in = new Scanner(System.in);
         boolean isExitCommandInvoked = false;
+        ArrayList<Task> taskList = new ArrayList<>();
+        try {
+            loadFileToTaskList("src/main/java/data/duke.txt", taskList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
         sayIntro();
         //easier to identify lines input by user (per Python)
         System.out.print(">>>");
-        
+
         while (in.hasNextLine()) {
             userInput = in.nextLine();
             try {
@@ -162,19 +244,25 @@ public class Duke {
                 continue;
             }
             String[] tokenizedInput = userInput.split(" ");
-            switch (tokenizedInput[0]) {
+            switch (tokenizedInput[0].toLowerCase()) {
             case ("bye"):
                 isExitCommandInvoked = true;
                 break;
             case ("list"):
-                addTaskToList(taskList);
+                printTaskList(taskList);
                 break;
             case("done"):
                 updateTaskDone(tokenizedInput[1], taskList);
+                try {
+                    saveTaskListToFile("src/main/java/data/duke.txt", taskList);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 break;
             default:
                 try{
                     insertNewTask(taskList, userInput, tokenizedInput);
+                    saveTaskListToFile("src/main/java/data/duke.txt", taskList);
                 } catch (IllegalKeywordException e) {
                     System.out.println(underscoredLine);
                     System.out.println("\t\u2639 !!ERROR!! I'm sorry, but I don't know what that means :-(");
@@ -191,8 +279,11 @@ public class Duke {
                     System.out.println(underscoredLine);
                     System.out.println("\t\u2639 !!ERROR!! " + tokenizedInput[0] + " command is missing additional parameters.");
                     System.out.println(underscoredLine);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
+
 
             if (isExitCommandInvoked) {
                 break;
