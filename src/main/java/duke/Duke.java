6f0b7eb2fcba652de555f8ed97eaa323a98913f9
HAOYUN49 <e01261960@u.nus.edu>
2020-02-16 13:08:16 +0800

    add Delete command

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -8,13 +8,13 @@ public class Duke {
     public static final String GOODBYE = "Bye. Hope to see you again soon!";
     public static final String INVALID_COMMAND = "☹ OOPS!!! This is an invalid command, please type a valid command";
     public static final String GREETING = String.format("%s\n%s", "Hello, I'm duke.Duke!", "What can I do for you?");
-    public static final String TODO_DESCRIPTION = "todo <duke.task.Task Name>";
-    public static final String DEADLINE_DESCRIPTION = "deadline <duke.task.Task Name> /by <duke.task.Deadline>";
+    public static final String TODO_DESCRIPTION = "todo <Task Name>";
+    public static final String DEADLINE_DESCRIPTION = "deadline <Task Name> /by <Deadline>";
     public static final String INVALID_DONE = "# of the task is invalid\n";
-    public static final String DONE_DESCRIPTION = "done <duke.task.Task #>";
+    public static final String DONE_DESCRIPTION = "done <Task #>";
     public static final String CORRECT_FORMAT = "The correct format should be:\n";
     public static final String INVALID_DESCRIPTION = "The description of the task is invalid";
-    public static final String EVENT_DESCRIPTION = "event <duke.task.Task Name> /at <Timeslot>";
+    public static final String EVENT_DESCRIPTION = "event <Task Name> /at <Timeslot>";
 
     private TaskManager manager = new TaskManager();
 
@@ -32,39 +32,43 @@ public class Duke {
     public void runDuke() {
         Scanner in = new Scanner(System.in);
         String command;
-        while(in.hasNextLine()) {
+        while (in.hasNextLine()) {
             command = in.nextLine();
             String[] commands = command.split(" ", 2);
 
-            if(commands[0].equals("bye")) {
+            if (commands[0].equalsIgnoreCase("bye")) {
                 return;
-            } else if (commands[0].equals("list")){
+            } else if (commands[0].equalsIgnoreCase("list")) {
                 manager.listTask();
-            } else if (commands[0].equals("done")){
+            } else if (commands[0].equalsIgnoreCase("done")) {
                 try {
                     manager.markTask(Integer.parseInt(commands[1]));
-                } catch (IndexOutOfBoundsException e) {
-                    printFormat(INVALID_DONE+ CORRECT_FORMAT+ DONE_DESCRIPTION);
-                } catch (NumberFormatException e) {
-                    printFormat(INVALID_DONE+ CORRECT_FORMAT+ DONE_DESCRIPTION);
+                } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                    printFormat(INVALID_DONE + CORRECT_FORMAT + DONE_DESCRIPTION);
                 }
-            } else if (commands[0].equals("todo")){
+            } else if (commands[0].equalsIgnoreCase("todo")) {
                 try {
                     manager.addTodo(commands[1]);
-                }catch (IndexOutOfBoundsException e) {
-                    printFormat(INVALID_DESCRIPTION+CORRECT_FORMAT+TODO_DESCRIPTION);
+                } catch (IndexOutOfBoundsException e) {
+                    printFormat(INVALID_DESCRIPTION + CORRECT_FORMAT + TODO_DESCRIPTION);
                 }
-            } else if (commands[0].equals("deadline")) {
+            } else if (commands[0].equalsIgnoreCase("deadline")) {
                 try {
                     manager.addDeadline(commands[1]);
                 } catch (IndexOutOfBoundsException e) {
-                    printFormat(INVALID_DESCRIPTION+CORRECT_FORMAT+DONE_DESCRIPTION);
+                    printFormat(INVALID_DESCRIPTION + CORRECT_FORMAT + DEADLINE_DESCRIPTION);
                 }
-            } else if (commands[0].equals("event")) {
+            } else if (commands[0].equalsIgnoreCase("event")) {
                 try {
                     manager.addEvent(commands[1]);
                 } catch (IndexOutOfBoundsException e) {
-                    printFormat(INVALID_DESCRIPTION+CORRECT_FORMAT+ EVENT_DESCRIPTION);
+                    printFormat(INVALID_DESCRIPTION + CORRECT_FORMAT + EVENT_DESCRIPTION);
+                }
+            } else if (commands[0].equalsIgnoreCase("delete")) {
+                try {
+                    manager.delete(Integer.parseInt(commands[1]));
+                } catch (IndexOutOfBoundsException | NumberFormatException e) {
+                    printFormat(INVALID_DONE + CORRECT_FORMAT + DONE_DESCRIPTION.replace("done", "delete"));
                 }
             } else {
                 printFormat(INVALID_COMMAND);
@@ -72,7 +76,7 @@ public class Duke {
         }
     }
 
-    public void greet(){
+    public void greet() {
         printFormat(GREETING);
     }
 
--- src/main/java/duke/TaskManager.java
+++ src/main/java/duke/TaskManager.java
@@ -4,18 +4,20 @@ import duke.task.Deadline;
 import duke.task.Event;
 import duke.task.Task;
 import duke.task.Todo;
+import java.util.ArrayList;
 
 public class TaskManager {
-    public static final int MAXIMUM_TASK = 100;
     public static final String FORMAT_LINE = "------------------------------------";
     public static final String LISTING = "Here is the list of all tasks you have:";
-    public static final String DONE = "Good job! You have done this task:";
-    private Task[] tasks = new Task[MAXIMUM_TASK];
+    public static final String DONE_TASK = "Good job! You have done this task:";
+    public static final String ADD_TASK = "Got it! I have added this task into your list:";
+    public static final String DELETE_TASK= "Noted! I have deleted this task from the list:";
+    private ArrayList<Task> tasks = new ArrayList<>();
     private int numOfTasks = 0;
 
-    public void printAddedTask(String str) {
+    public void printCommand(String command, String str) {
         System.out.println(FORMAT_LINE);
-        System.out.println("Got it! I have added this task into your list:");
+        System.out.println(command);
         System.out.println("\t" + str);
         System.out.println("Now you have " + numOfTasks + (numOfTasks > 1? " tasks": " task") + " in the list");
         System.out.println("Remember to finish your tasks on time!");
@@ -24,34 +26,40 @@ public class TaskManager {
     }
 
     public void addTodo(String command) {
-        tasks[numOfTasks++] = new Todo(command);
-        printAddedTask(tasks[numOfTasks-1].print());
+        tasks.add(new Todo(command));
+        printCommand(ADD_TASK, tasks.get(numOfTasks++).print());
     }
 
     public void addDeadline(String command) {
         String[] commands = command.split("/", 2);
-        tasks[numOfTasks++] = new Deadline(commands);
-        printAddedTask(tasks[numOfTasks-1].print());
+        tasks.add(new Deadline(commands));
+        printCommand(ADD_TASK, tasks.get(numOfTasks++).print());
     }
 
     public void addEvent(String command) {
         String[] commands = command.split("/", 2);
-        tasks[numOfTasks++] = new Event(commands);
-        printAddedTask(tasks[numOfTasks-1].print());
+        tasks.add(new Event(commands));
+        printCommand(ADD_TASK, tasks.get(numOfTasks++).print());
     }
 
     public void listTask() {
         System.out.println(FORMAT_LINE);
         System.out.println(LISTING);
         for(int i = 0; i < numOfTasks; i++) {
-            System.out.println(i+1 + "." + tasks[i].print());
+            System.out.println(i+1 + "." + tasks.get(i).print());
         }
         System.out.println(FORMAT_LINE);
         System.out.println();
     }
 
     public void markTask(int taskNo) {
-        Duke.printFormat(DONE+"  [✓] " + tasks[taskNo-1].getName());
-        tasks[taskNo-1].changeStatus(true);
+        printCommand(DONE_TASK,"  [✓] " + tasks.get(taskNo-1).getName());
+        tasks.get(taskNo-1).changeStatus(true);
+    }
+
+    public void delete(int taskNo) {
+        numOfTasks--;
+        printCommand(DELETE_TASK, tasks.get(taskNo-1).print());
+        tasks.remove(taskNo-1);
     }
 }
