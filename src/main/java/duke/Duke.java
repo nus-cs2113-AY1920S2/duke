a531a4e968dea0e1395966393b46c44259f8d4ba
thanhduc2000 <nthanhduc2000@gmail.com>
2020-02-18 00:02:45 +0800

    Add "delete task" feature

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,6 +1,7 @@
 package duke;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -26,24 +27,22 @@ public class Duke {
     public static final String EMPTY_INPUT_MESSAGE = "\t☹ OOPS!!! Input cannot be empty";
     public static final String DUMMY_INPUT_MESSAGE = "\t☹ OOPS!!! I'm sorry, but I don't know what that means :-(";
     public static final String LIMIT_REACHED_MESSAGE = "\t☹ OOPS!!! duke.Task limit has reached";
-    public static final String WRONG_NUMBER_FORMAT_MESSAGE = "\t☹ OOPS!!! The task's index cannot be a float number";
+    public static final String WRONG_NUMBER_FORMAT_MESSAGE = "\t☹ OOPS!!! Wrong number format!";
     public static final String OUT_OF_BOUND_MESSAGE = "\t☹ OOPS!!! The task's index is out of bound or does not exist";
+    public static final String TASK_REMOVED_MESSAGE = "\tNoted. I've removed this task:";
+    public static final String DELETE = "delete";
 
-    public static void printList(Task[] tasks) {
+    public static void printList(ArrayList<Task> tasks) {
         System.out.println(TASK_LISTING);
-        for(int i = 0; i < TASK_LIMIT; i ++) {
-            if(tasks[i] == null) {
-                break;
-            }
-            System.out.println("\t" + (i + 1) + "." + tasks[i]);
+        for(int i = 0; i < tasks.size(); i ++) {
+            System.out.println("\t" + (i + 1) + "." + tasks.get(i));
         }
     }
 
 
     public static void main(String[] args) throws IOException, NullPointerException{
-        Task[] tasks = new Task[TASK_LIMIT];
+        ArrayList<Task> tasks = new ArrayList<Task>();
         welcomeMessage();
-        int listCount = 0;
         Scanner commandScanner = new Scanner(System.in);
         String command = commandScanner.nextLine();
         while(!command.equals(BYE)) {
@@ -54,15 +53,18 @@ public class Duke {
             } else {
                 String prefix = commandSplitter[0];
                 switch (prefix) {
+                case DELETE:
+                    deleteTaskMessage(tasks, commandSplitter[1]);
+                    break;
                 case DONE:
-                    markTaskMessage(tasks, commandSplitter[1], listCount);
+                    markTaskMessage(tasks, commandSplitter[1]);
                     break;
                 case EMPTY_STRING:
                 case TODO:
                 case DEADLINE:
                 case EVENT:
                 default:
-                    listCount = addTask(tasks, listCount, command, prefix);
+                    addTask(tasks, command, prefix);
                 }
             }
             System.out.println(LINE_SPLITTING);
@@ -71,7 +73,24 @@ public class Duke {
         byeMessage();
     }
 
-    private static int addTask(Task[] tasks, int listCount, String command, String prefix) {
+    private static void deleteTaskMessage(ArrayList<Task> tasks, String number) {
+        try {
+            System.out.println(TASK_REMOVED_MESSAGE);
+            int index = Integer.parseInt(number);
+            System.out.println("\t" + tasks.get(index - 1));
+            tasks.remove(tasks.get(index - 1));
+            String printTaskCount = "\tNow you have " + tasks.size() + " tasks in the list.";
+            System.out.println(printTaskCount);
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println(OUT_OF_BOUND_MESSAGE);
+        }
+        catch (NumberFormatException e) {
+            System.out.println(WRONG_NUMBER_FORMAT_MESSAGE);
+        }
+    }
+
+    private static void addTask(ArrayList<Task> tasks, String command, String prefix) {
         try {
             int splitIndex = command.indexOf("/");
             switch (prefix) {
@@ -79,25 +98,28 @@ public class Duke {
                 throw new NullPointerException();
             case TODO:
                 String activity = command.substring(TODO_WORD_LENGTH + 1);
-                tasks[listCount++] = new ToDo(activity);
+                tasks.add(new ToDo(activity));
                 break;
             case DEADLINE:
                 String activity2 = command.substring(DEADLINE_WORD_LENGTH + 1, splitIndex - 1);
                 String date = command.substring(splitIndex + 4);
-                tasks[listCount++] = new Deadline(activity2, date);
+                tasks.add(new Deadline(activity2, date));
                 break;
             case EVENT:
                 String activity3 = command.substring(EVENT_WORD_LENGTH + 1, splitIndex - 1);
                 String time = command.substring(splitIndex + 4);
-                tasks[listCount++] = new Event(activity3, time);
+                tasks.add(new Event(activity3, time));
                 break;
             default:
                 throw new IOException();
             }
+            if(tasks.size() > TASK_LIMIT) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
             System.out.println(ADDED_TASK_MESSAGE);
-            String printTask = "\t" + tasks[listCount - 1];
+            String printTask = "\t" + tasks.get(tasks.size() - 1);
             System.out.println(printTask);
-            String printTaskCount = "\tNow you have " + listCount + " tasks in the list.";
+            String printTaskCount = "\tNow you have " + tasks.size() + " tasks in the list.";
             System.out.println(printTaskCount);
         }
         catch (NullPointerException e) {
@@ -114,19 +136,18 @@ public class Duke {
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println(LIMIT_REACHED_MESSAGE);
         }
-        return listCount;
     }
 
-    private static void markTaskMessage(Task[] tasks, String word, int listCount) throws ArrayIndexOutOfBoundsException{
+    private static void markTaskMessage(ArrayList<Task> tasks, String word) throws ArrayIndexOutOfBoundsException{
         try {
             int index = Integer.parseInt(word);
-            if(index >= listCount) {
+            if(index >= tasks.size()) {
                 throw new ArrayIndexOutOfBoundsException();
             }
-            tasks[index - 1].markAsDone();
+            tasks.get(index - 1).markAsDone();
             System.out.println(TASK_MARKING_MESSAGE);
             String printTask = "\t%s\n";
-            System.out.printf(printTask, tasks[index - 1]);
+            System.out.printf(printTask, tasks.get(index - 1));
         }
         catch (NumberFormatException e) {
             System.out.println(WRONG_NUMBER_FORMAT_MESSAGE);
