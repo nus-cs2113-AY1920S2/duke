b201292ac255250d680c70856f2bf6c39520dfeb
Janicetyy <janice.tohy2@gmail.com>
2020-02-18 11:54:47 +0800

    add feature level 6 and minor changes to implementation due to change to arraylist

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,9 @@
 public class Task {
-    protected int taskNum;
     protected String description;
     protected String type;
     protected boolean isDone;
 
-    private static int totalTask = 0;
-
     public Task(String description, String type) {
-        totalTask++;
-        this.taskNum = totalTask;
         this.description = description;
         this.type = type;
     }
@@ -17,24 +12,12 @@ public class Task {
         return description;
     }
 
-    public int getTaskNum() {
-        return taskNum;
-    }
-
-    public void setTaskNum(int taskNum) {
-        this.taskNum = taskNum;
-    }
-
     public void setDone(boolean isDone) {
         this.isDone = isDone;
     }
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
-    }
-
-    public static int getTotalTask() {
-        return totalTask;
     }
 
     @Override
