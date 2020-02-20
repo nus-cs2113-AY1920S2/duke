0834a662928e23441685d3105ff019c4e657c4cd
alwayshuizhen <alwayshuizhen@gmail.com>
2020-02-20 18:52:03 +0800

    Dates and Times

--- src/main/java/TaskList/Deadline.java
+++ src/main/java/TaskList/Deadline.java
@@ -1,15 +1,30 @@
 package TaskList;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     protected String by;
-
+    private LocalDate deadline;
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
         this.itemType = 'D';
     }
 
+    public void convertDeadlineFormat(String oldFormat) {
+        try {
+            deadline = LocalDate.parse(oldFormat);
+            by = deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) {
+            by = oldFormat;
+        }
+    }
+
     @Override
     public String printObject() {
+        convertDeadlineFormat(by);
         return ("[" + itemType + "][" + getStatusIcon() + "] "+ description + " (by: " + by + ")");
     }
 
