d515581cb191d5fc048f50654b345da06956df09
kokjoon97 <e0309136@u.nus.edu>
2020-02-21 22:42:31 +0800

    Add JavaDoc

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -20,16 +20,29 @@ import static duke.common.Messages.INDEX_PROBLEM_MESSAGE;
 import static duke.common.Messages.FILE_ERROR_MESSAGE;
 import static duke.common.Messages.FORMAT_PROBLEM_MESSAGE;
 
+/**
+ * Entry point of the Duke application.
+ * Manages interactions between storage, UI and the list of tasks.
+ */
 public class Duke {
 
+    /** Storage object that loads tasks from file and saves tasks to file */
     private static Storage storage;
 
+    /** List of tasks */
     private static TaskList tasks;
 
+    /** UI that interacts with the user */
     private static Ui ui;
 
+    /** Flag that indicates whether an exit command has been encountered */
     private static boolean isExit;
 
+    /**
+     * Runs the application.
+     *
+     * @param args Not significant in this case.
+     */
     public static void main(String[] args) {
         initialiseDuke();
         try {
@@ -63,6 +76,9 @@ public class Duke {
         }
     }
 
+    /**
+     * Initialises the application, including all required resources.
+     */
     private static void initialiseDuke() {
         tasks = new TaskList();
         ui = new Ui();
