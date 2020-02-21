3e86976ee68dfc829c7e1962cb99eed76c5dd923
NizarMohd <mohdnizar97@gmail.com>
2020-02-21 22:03:08 +0800

    Add more Java Doc comments

--- src/main/java/Asset/Ui.java
+++ src/main/java/Asset/Ui.java
@@ -57,32 +57,50 @@ public class Ui  {
         out.println("\t" + "What can I do for you?");
         out.println(LINE);
     }
-
+    /**
+     * This method prints constant LINE
+     */
     public void printLine(){
         out.println(LINE);
     }
-
+    /**
+     * This method prints all the error message thrown in Duke.run().
+     */
     public void printError(String message){
         out.println(message);
     }
+    /**
+     * This method informs User that a new Task class has been created and added to list
+     */
     public void printAddTask(ArrayList<Task> l1, Task task){
         out.println("\t Got it. I've added this task:");
         out.println("\t   "+task.toString());
         out.println("\t Now you have " + l1.size()+ " tasks in the list.");
     }
-
+    /**
+     * This method prints good bye message to User.
+     */
     public void printGoodByeMessage(){
         out.println("\t Bye.Hope to see you again soon!");
     }
+    /**
+     * This method informs User that stated Task has been marked as done.
+     */
     public void printDone(Task task){
         out.println("\t Nice! I've marked this task as done:");
         out.println("\t   "+task.toString());
     }
+    /**
+     * This method informs User that stated Task has been deleted.
+     */
     public void printDelete(Task task, ArrayList<Task> l1){
         out.println("\t Noted. I've removed this task: ");
         out.println("\t   " + task.toString());
         out.println("\t Now you have " + l1.size() + " tasks in the list.");
     }
+    /**
+     * This method informs User that all Tasks has been marked as done.
+     */
     public void printDoneAll(ArrayList<Task> l1){
         out.println("\t All tasks have been marked as done!");
             for (int i = 0; i < l1.size(); i++) {
@@ -91,19 +109,31 @@ public class Ui  {
                 out.println("\t " + count + "." + task.toString());
             }
     }
-
+    /**
+     * This method informs User that all Tasks has been deleted.
+     */
     public void printDeleteAll(){
         out.println("\t All tasks have been deleted!");
     }
-
+    /**
+     * This method confirms with User if they want all Tasks to be deleted.
+     */
     public void confirmDeleteAll(){
         out.println("\t Are you sure that you want ALL tasks to be deleted?");
         out.println("\t Type Y for Yes and N for No");
     }
-
+    /**
+     * This method receives confirmation with User if they want all Tasks to be deleted.
+     */
     public String receiveDeleteAllConfirmation(){
         return in.nextLine();
     }
+    /**
+     * This method shows User of all available Tasks.
+     * @param l1 is the current list of available Tasks.
+     * If l1 is empty, method will inform user that no Tasks
+     * are available.
+     */
     public void printList(ArrayList<Task> l1){
         if(l1.isEmpty()){
             out.println(LIST_EMPTY);
@@ -116,16 +146,31 @@ public class Ui  {
             }
         }
     }
-
+    /**
+     * This method informs User that command entered is not supported by Duke.
+     */
     public void printWrongInput(){
         out.println(WRONG_INPUT);
     }
+    /**
+     * This method lists the supported commands for User.
+     */
     public void printHelp(){
         out.println("\t "+ Arrays.toString(COMMAND));
     }
+    /**
+     * This method informs User that data.txt is missing.
+     */
     public void showLoadingError(){
         out.println(MISSING_FILE);
     }
+    /**
+     * This method shows User the list of Tasks that matches with
+     * User search key.
+     *
+     * @param l1 is the list of all Tasks.
+     * @param l2 is the list of matching Tasks.
+     */
     public void printFindList(ArrayList<Task> l1, ArrayList<Integer> l2){
         if(l2.isEmpty()){
             out.println(NO_MATCHING_TASK);
