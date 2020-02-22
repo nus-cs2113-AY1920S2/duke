c0d5924de518a83fc4fb0b8baa0354774d4f4964
Keith-JK <keithcjk123@gmail.com>
2020-02-23 00:06:28 +0800

    Change to importing specifics instead of *

--- src/main/java/Storage/Storage.java
+++ src/main/java/Storage/Storage.java
@@ -7,7 +7,12 @@ import Task.Deadline;
 import Task.Events;
 import TaskList.TaskList;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
