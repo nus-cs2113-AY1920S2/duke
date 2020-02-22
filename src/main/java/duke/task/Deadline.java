bb367efeaa6384bae27230c9474c029c93b6c025
lwxymere <chaoscold@hotmail.com>
2020-02-22 16:35:04 +0800

    Add date handling

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,40 +1,43 @@
 package duke.task;
 
 import duke.DukeException;
+import duke.Parser;
+
+import java.time.LocalDateTime;
 
 public class Deadline extends Task {
     public static final char DEADLINE_ICON = 'D';
 
-    protected String by;
+    protected LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, LocalDateTime by) {
         super(description);
         this.by = by;
     }
 
     @Override
     public String encodeTask() {
-        return String.format("%s|%s|%s|%s", DEADLINE_ICON, isDone, description, by);
+        return String.format("%s|%s|%s|%s",
+                DEADLINE_ICON, isDone, description, by.format(Parser.INPUT_DATE_FORMAT)
+        );
     }
 
     public static Deadline decodeTask(String encodedTask) {
         String[] tokens = encodedTask.split("\\" + DELIMITER);
         boolean isDone = Boolean.parseBoolean(tokens[1]);
         String description = tokens[2];
-        String by = tokens[3];
+        LocalDateTime by = Parser.parseDate(tokens[3]);
         Deadline deadline = new Deadline(description, by);
         if (isDone) {
-            try {
-                deadline.markAsDone();
-            } catch (DukeException de) {
-                // user feedback not required
-            }
+            deadline.markAsDone();
         }
         return deadline;
     }
 
     @Override
     public String toString() {
-        return "[" + DEADLINE_ICON + "]" + super.toString() + " (by: " + by + ")";
+        return "[" + DEADLINE_ICON + "]"
+                + super.toString()
+                + " (by: " + by.format(Parser.PRINT_DATE_FORMAT) + ")";
     }
 }
