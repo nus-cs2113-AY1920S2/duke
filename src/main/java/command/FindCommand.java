a41e35bc30977c9e285caa8df5c93a23b238594f
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:25:02 +0800

    Refactor FindCommand class and added Javadocs documentations.

--- src/main/java/command/FindCommand.java
+++ src/main/java/command/FindCommand.java
@@ -1,15 +1,37 @@
 package command;
 
+import static misc.Messages.MESSAGE_COMMAND_RESULT_SUCCESS;
+import static misc.Messages.MESSAGE_FIND_COMMAND_MISSING_KEYWORD;
+
+import java.util.Optional;
+
 import duke.Duke;
 
-import static misc.Messages.MESSAGE_COMMAND_RESULT_SUCCESS;
+/**
+ * Represents the 'Find' functionality of a command. A FindCommand contains
+ * a keyword that will be pass onto Duke to find tasks that contains this
+ * keyword.  
+ * 
+ */
 
 public class FindCommand extends Command {
   
     private final String keyword;
     
-    public FindCommand(String keyword) {
-        this.keyword = keyword;
+    /**
+     * Constructor of a FindCommand.
+     * 
+     * @param keyword A keyword that is used to find tasks that contains this word. 
+     * @throws InvalidCommandException If the given keyword is a blank entry.
+     */
+    public FindCommand(Optional<String> keyword) {
+        boolean hasKeyword = keyword.get().isBlank();
+
+        if (!hasKeyword) {
+            this.keyword = keyword.get();
+        } else {
+            throw new InvalidCommandException(MESSAGE_FIND_COMMAND_MISSING_KEYWORD);
+        }
     }
     
     @Override
