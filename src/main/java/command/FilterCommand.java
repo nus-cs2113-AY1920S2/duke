cf9c41c7826573406f560082544848a3042be65a
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:24:54 +0800

    Refactor FindCommand class and added Javadocs documentations.

--- src/main/java/command/FilterCommand.java
+++ src/main/java/command/FilterCommand.java
@@ -1,15 +1,37 @@
 package command;
 
+import static misc.Messages.MESSAGE_COMMAND_RESULT_SUCCESS;
+import static misc.Messages.MESSAGE_FILTER_COMMAND_MISSING_DATE;
+
+import java.util.Optional;
+
 import duke.Duke;
 
-import static misc.Messages.MESSAGE_COMMAND_RESULT_SUCCESS;
-
+/**
+ * Represents the 'Filter' functionality of a command. A FilterCommand contains
+ * a date that will be pass onto Duke to filter tasks based on the given date.  
+ * 
+ */
 public class FilterCommand extends Command {
     
+    /** A String representation of a date that serves as a predicate. */
     private final String date;
   
-    public FilterCommand(String date) {
-        this.date = date;
+    /**
+     * Constructor for a FilterCommand.
+     * 
+     * @param date A date to be used as a predicate for filtering tasks.
+     * @throws InvalidCommandException If the given date is a blank entry.
+     */
+    public FilterCommand(Optional<String> date) {
+        boolean hasDate = date.get().isBlank();
+
+        if (!hasDate) {
+            this.date = date.get();
+        } else {
+            throw new InvalidCommandException(
+                    MESSAGE_FILTER_COMMAND_MISSING_DATE);
+        }
     }
     
     @Override
