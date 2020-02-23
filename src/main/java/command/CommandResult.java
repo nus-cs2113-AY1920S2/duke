ab0c8ab6231e3c9ea8a05b3aa70772801212e620
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:23:44 +0800

    Refactor CommandResult class and added Javadocs documentations.

--- src/main/java/command/CommandResult.java
+++ src/main/java/command/CommandResult.java
@@ -3,32 +3,32 @@ package command;
 import duke.Duke;
 
 /**
- * Represents the result after executing a command.
+ * Represents the result after executing a command. A CommandResult
+ * contains a string description of a result that will be used by the
+ * Ui to display to the user. As CommandResult is the final stage of 
+ * executing a command, an IllegalStateException will be thrown when 
+ * executing a CommandResult.
+ * 
  */
 public class CommandResult extends Command {
     
     /** String representation of a result after executing. */
-    public final String commandOutput;
+    public final String commandResult;
     
     /** 
      * Constructor for the CommandResult.
      * 
-     * @param commandOutput
+     * @param commandResult A String description of a command result after
+     * a command is being executed. 
      */
-    public CommandResult(String commandOutput) {
-        this.commandOutput = commandOutput;
+    public CommandResult(String commandResult) {
+        this.commandResult = commandResult;
     }
     
-    public String getCommandOutput() {
-        return this.commandOutput;
+    public String getCommandResult() {
+        return this.commandResult;
     }
     
-    /**
-     * CommandResult is the final stage after executing a command. It cannot be 
-     * executed again.
-     * 
-     * @throws IllegalStateException.
-     */
     @Override
     public CommandResult execute(Duke duke) {
         throw new IllegalStateException("Cannot execute a Command Result");
