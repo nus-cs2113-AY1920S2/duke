5aa8d55614059cbf9e3f8b536324e5a447d950c1
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:26:33 +0800

    Refactor Storage class and added Javadocs documentations.

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -2,6 +2,7 @@ package storage;
 
 import static misc.Messages.MESSAGE_READ_WRITE_FAILURE;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -20,10 +21,11 @@ import storage.InvalidStorageFilePathException;
 /**
  * Represents the storage that stores all information pertaining
  * to tasks in a storage.txt file.
+ * 
  */
 public class Storage {
     
-    /** The default file name that is to be read or write by the program. */
+    /** The default file name that is to be read or written by the program. */
     public static final String DEFAULT_STORAGE_FILEPATH = "storage.txt";
     
     public final Path path;
@@ -35,10 +37,10 @@ public class Storage {
     
     /** 
      * Constructor of a Storage retrieved from Path.
-     * Throws an exception when there is no file that matches "storage.txt".
      * 
      * @param filePath
-     * @throws InvalidStorageFilePathException
+     * @throws InvalidStorageFilePathException  If the storage file does not match
+     *                                          a .txt extension.
      */
     public Storage(String filePath) {
         this.path = Paths.get(filePath);
@@ -50,50 +52,68 @@ public class Storage {
     }
     
     /** 
-     * Loads all data from the storage file.
+     * Silently loads all data from the storage text file. This method first
+     * tries to read from a storage.txt file. If this file is not present in
+     * the same directory as the program, it will create a new file that is
+     * named after 'storage.txt' as well.
      * 
-     * @return
-     * @throws IOException
+     * @return A TaskList with all the data of tasks stored in it.
+     * @throws StorageReadWriteException  If the current storage file cannot be
+     *                                    loaded.  
      */
-    public TaskList load() throws IOException {
-        List<String> lines = new ArrayList<>();
+    public TaskList load() throws StorageReadWriteException {
+        List<String> lines = new ArrayList<>();        
         
-        // Find an existing storage.txt file first, else create a new storage.txt.
-        File f = new File(this.path.toString()); 
-        f.createNewFile();
-        FileOutputStream outputFile = new FileOutputStream(f, false);
-        outputFile.close();
-        
+        File f = new File(this.path.toString());        
+        try {
+            f.createNewFile(); 
+            
+            FileOutputStream outputFile = new FileOutputStream(f, true);
+            BufferedOutputStream bufferedOutputFile = 
+                    new BufferedOutputStream(outputFile, 1024);
+            
+            bufferedOutputFile.close();
+        } catch (IOException e) {
+            throw new StorageReadWriteException(
+                    MESSAGE_READ_WRITE_FAILURE);
+        }
+                
         try {
             String line;                       
-            BufferedReader br = new BufferedReader(new FileReader(this.path.toString()));
+            BufferedReader br = new BufferedReader(
+                    new FileReader(this.path.toString()));
+            
             while ((line = br.readLine()) != null) {
                 lines.add(line);
             }
         } catch (IOException e) {
-            throw new StorageReadWriteException(MESSAGE_READ_WRITE_FAILURE);
+            throw new StorageReadWriteException(
+                    MESSAGE_READ_WRITE_FAILURE);
         }
-        return TaskListDecoder.decodeTaskList(lines);
+        return StorageDecoder.decodeStorageFile(lines);
     }
     
     /**
-     * Write data into the storage file.
-     * Throws an exception if the file cannot be read or written.
+     * Writes data into the storage text file. Throws an exception if the file 
+     * cannot be read or written.
      * 
-     * @param filePath
-     * @param taskList
-     * @throws IOException
-     * @throws StorageReadWriteException
+     * @throws StorageReadWriteException  If the current storage file cannot be
+     *                                    overwritten.  
      */
-    public void save(String filePath, TaskList taskList) throws IOException {      
+    public void save(String filePath, TaskList taskList) 
+            throws StorageReadWriteException {
+        
         try {
             FileWriter fw = new FileWriter(filePath);
             BufferedWriter bw = new BufferedWriter(fw);
-            String encodedTaskList = TaskListEncoder.encodeTaskList(taskList); 
+            String encodedTaskList = StorageEncoder.
+                    encodeTaskList(taskList); 
+            
             bw.write(encodedTaskList);
             bw.close();
         } catch (IOException e) {
-            throw new StorageReadWriteException(MESSAGE_READ_WRITE_FAILURE);
+            throw new StorageReadWriteException(
+                    MESSAGE_READ_WRITE_FAILURE);
         }        
     }
     
@@ -101,7 +121,7 @@ public class Storage {
         return this.path.toString();
     }
     
-    /** Test if a given file has a .txt extension. */
+    /** Returns true if a given file has a .txt extension. */
     public boolean isValidPath(Path filePath) {
         return filePath.toString().endsWith(".txt");
     }   
