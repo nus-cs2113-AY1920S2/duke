977b45019e10b77e4453475d7032f2567936af9a
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:26:05 +0800

    Refactor Main class and added Javadocs documentations.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,7 @@
+import static misc.Messages.MESSAGE_END_OF_INPUT_FILE;
+
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 import command.CommandResult;
 import command.ExitCommand;
@@ -36,10 +39,11 @@ public class Main {
     }
     
     /** 
-     * Initialize the required classes, load any data from storage.txt and 
+     * Initializes the required classes, loads any data from storage text file and 
      * prints a welcome message. 
      * 
-     * @param args arguments to be provided by the user at program launch
+     * @param args arguments to be provided by the user at program launch.
+     * @throws throws a RuntimeException when if the storage text file is corrupted.
      */
     public void start(String[] args) {
         try {
@@ -47,50 +51,52 @@ public class Main {
             this.storage = new Storage();           
             this.duke = new Duke(storage.load());  
             ui.displayWelcomeMessage();
-        } catch (InvalidStorageFilePathException e) {
-            ui.displayErrorMessage(e.getMessage());
-        } catch (StorageReadWriteException e) {
+        } catch (InvalidStorageFilePathException | StorageReadWriteException e) {
             ui.displayErrorMessage(e.getMessage());
             throw new RuntimeException(e);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
         } 
     }
     
-    /** Process user command and execute until an exit command is given. */
+    /** Processes user command and execute until an exit command is given. */
     private void runCommandUntilExit() {                     
         while (true) {
-            Command command;
-            String commandText = ui.getCommand();
-            ui.displayBorder();
-            
-            try {  
+            Command command;                          
+            try {        
+                String commandText = ui.getCommand();                 
+                ui.displayBorder();
                 command = new Parser().parseCommand(commandText);
-                CommandResult output = command.execute(this.duke);
                 
-                try {
-                    storage.save(storage.getFilePath(), duke.getTaskList());
-                } catch (IOException ioe) {
-                    ioe.printStackTrace();
-                } 
-                
-                ui.displayOutputMessage(output);             
                 if (ExitCommand.isExit(command)) {
                     break;
                 }
-
+                
+                CommandResult output = executeCommand(command, this.duke);                 
+                ui.displayOutputMessage(output);             
             } catch (InvalidTaskArgumentException e) {                
                 ui.displayErrorMessage(e.getMessage());
             } catch (IndexOutOfBoundsException e) {
                 ui.displayErrorMessage(e.getMessage());
             } catch (InvalidCommandException e) {
                 ui.displayErrorMessage(e.getMessage());
-            } catch (IllegalArgumentException e) {
-                ui.displayErrorMessage(e.getMessage());
+            } catch (NoSuchElementException nsee) {
+                // Used only for testing runtest.bat reads from an input file.
+                ui.displayErrorMessage(MESSAGE_END_OF_INPUT_FILE);
+                exit();
             } finally {
                 ui.displayBorder();
             }
         }
+    }
+    
+    /** Executes a command and save the result onto storage text file. */
+    private CommandResult executeCommand(Command command, Duke duke) {
+        try {
+            CommandResult output = command.execute(this.duke);
+            storage.save(storage.getFilePath(), duke.getTaskList());
+            return output;
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        } 
     }
     
     /** Exit the program with a goodbye message. */
