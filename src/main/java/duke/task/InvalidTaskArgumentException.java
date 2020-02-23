a1644004494021f2beea58ae8edcfcb2275f8a35
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:25:52 +0800

    Refactor InvalidTaskArgumentException class.

--- src/main/java/duke/task/InvalidTaskArgumentException.java
+++ src/main/java/duke/task/InvalidTaskArgumentException.java
@@ -4,14 +4,10 @@ package duke.task;
  * Represents an exception pertaining to invalid task arguments.
  * This exception is thrown when arguments of a task are not in the correct 
  * format specified by the Task constructor.
+ * 
  */
 public class InvalidTaskArgumentException extends IllegalArgumentException {
     
-    /** 
-     * Constructor for an InvalidTaskArgument.
-     * 
-     * @param message
-     */
     public InvalidTaskArgumentException(String message) {
         super(message);
     }
