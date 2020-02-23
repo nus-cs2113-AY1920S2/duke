559a31febbd5070e697d2452bf73626338460deb
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:24:01 +0800

    Refactor DeleteCommand class and added Javadocs documentations.

--- src/main/java/command/DeleteCommand.java
+++ src/main/java/command/DeleteCommand.java
@@ -1,11 +1,17 @@
 package command;
 
 import static misc.Messages.MESSAGE_COMMAND_RESULT_SUCCESS;
+import static misc.Messages.MESSAGE_DELETE_COMMAND_MISSING_TASKID;
+
+import java.util.Optional;
 
 import duke.Duke;
 
 /**
- * Represents the 'delete' function of a command.
+ * Represents the 'Delete' functionality of a command. A DeleteCommand contains
+ * a TaskID that will be pass onto Duke to delete a task that matches this
+ * TaskID. 
+ * 
  */
 public class DeleteCommand extends Command {
     
@@ -15,18 +21,20 @@ public class DeleteCommand extends Command {
     /** 
      * Constructor of the DeleteCommand. 
      * 
-     * @param taskId
+     * @param taskId The ID of a task that is to be deleted.
+     * @throws InvalidCommandException If a taskId is a blank entry.
      */
-    public DeleteCommand(int taskId) {
-        this.taskId = taskId;
+    public DeleteCommand(Optional<String> taskId) {
+        boolean hasTaskId = taskId.get().isBlank();
+
+        if (!hasTaskId) {
+            this.taskId = Integer.parseInt(taskId.get());
+        } else {
+            throw new InvalidCommandException(
+                    MESSAGE_DELETE_COMMAND_MISSING_TASKID);
+        }
     }
     
-    /**
-     * Executes the delete function.
-     * 
-     * @param duke Takes in duke to process the command.
-     * @return Returns a result of a command after execution.
-     */
     @Override
     public CommandResult execute(Duke duke) {
         duke.executeDeleteCommand(this.taskId);
