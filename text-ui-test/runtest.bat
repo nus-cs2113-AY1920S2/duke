d93768907aabdaf7f9d15b437071b9c04ad0cfed
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:27:08 +0800

    Add 'del storage.txt' after testing.

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,11 +7,11 @@ REM delete output from previous run
 del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src -Xlint:none -d ..\bin..\src\main\java\duke\Duke.java
-..\src\main\java\duke\exception\*.java ..\src\main\java\duke\task\*.java
-..\src\main\java\command\*.java ..\src\main\java\misc\*.java
-..\src\main\java\parser\*.java ..\src\main\java\ui\*.java
-..\src\main\java\Main.java
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\main\java\misc\Messages.java
+..\src\main\java\command\*.java ..\src\main\java\duke\Duke.java
+..\src\main\java\duke\task\*.java ..\src\main\java\parser\Parser.java
+..\src\main\java\storage\*.java ..\src\main\java\ui\Ui.java
+..\src\main\java\Main.java ..\storage.txt
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
@@ -23,3 +23,6 @@ java -classpath ..\bin Main < input.txt > ACTUAL.TXT
 
 REM compare the output to the expected output
 FC ACTUAL.TXT EXPECTED.TXT
+
+REM delete storage.txt file after checking for differences. This is to prevent side effect of duke reading from an existing storage file which affects the expected output (based on no storage file existing at first).
+del storage.txt
