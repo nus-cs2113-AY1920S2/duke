655c1f080e4d98a72b44bdbeb2c05ec1f6181e4e
Joseph Lim <limweijie@live.com.sg>
2020-02-23 16:25:59 +0800

    Refactor ListCommand class and added Javadocs documentations.

--- src/main/java/command/ListCommand.java
+++ src/main/java/command/ListCommand.java
@@ -5,19 +5,20 @@ import static misc.Messages.MESSAGE_COMMAND_RESULT_SUCCESS;
 import duke.Duke;
 
 /** 
- * Represents the 'list' function for a command.
+ * Represents the 'List' functionality of a command. A ListCommand does
+ * not take in any parameter. It's function is to instruct duke to
+ * list all current outstanding tasks that are stored in the storage file.  
+ * 
  */
 public class ListCommand extends Command {
   
+    /** 
+     * An empty constructor for a ListCommand that is needed for the 
+     * parseCommand() in the Parser class.  
+     */ 
     public ListCommand() {
     }
     
-    /**
-     * Executes the 'list' function.
-     *
-     * @param duke Takes in duke to process the command.
-     * @return Returns a result of a command after execution.
-     */
     @Override
     public CommandResult execute(Duke duke) {
         duke.executeListCommand();
