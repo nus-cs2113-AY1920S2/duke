329917bb890d620c0eb300feb669fe99e7e38c32
GanapathySanathBalaji <e0311091@u.nus.edu>
2020-02-25 17:43:27 +0800

    Add JavaDoc comments to Duke.java

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -9,16 +9,29 @@ import duke.ui.Ui;
 
 import java.util.Scanner;
 
+/**
+ * Main Class of the application
+ */
 public class Duke {
 
     private static final String FILE_PATH = "TaskList.txt";
     private static final String BYE_COMMAND = "bye";
 
+    /** Used for storing and loading tasks */
     private static Storage storage;
+    /** Used for interacting with the user */
     private static Ui ui;
+    /** Used for parsing user commands */
     private static Parser parser;
+    /** Used to store and manipulate the list of tasks */
     private static TaskList taskList;
 
+    /**
+     * Constructor for Duke Class.
+     * It creates a new Duke Object initialised with task list at the specified file path.
+     *
+     * @param filePath The location of the data file where tasks were previously stored at.
+     */
     public Duke(String filePath) {
         storage = new Storage(filePath);
         ui = new Ui();
@@ -26,6 +39,16 @@ public class Duke {
         taskList = new TaskList(storage.loadData());
     }
 
+    /**
+     * Runs the entire application and stores the task list before exit by calling relevant functions such as
+     * {@link Ui#printWelcomeMessage()}, {@link #parseAndExecuteCommands()}, {@link Storage#storeTasksToFile(TaskList)}
+     * and {@link Ui#printByeMessage()}.
+     *
+     * @see Ui#printWelcomeMessage()
+     * @see #parseAndExecuteCommands()
+     * @see Storage#storeTasksToFile(TaskList)
+     * @see Ui#printByeMessage()
+     */
     public void run() {
         ui.printWelcomeMessage();
         parseAndExecuteCommands();
@@ -33,6 +56,10 @@ public class Duke {
         ui.printByeMessage();
     }
 
+    /**
+     * Parses the user commands and executes them till the bye command is entered. After which it stops and returns the
+     * control back to {@link #run()}.
+     */
     private void parseAndExecuteCommands() {
         Scanner sc = new Scanner(System.in);
         String fullCommand;
@@ -51,6 +78,13 @@ public class Duke {
         }
     }
 
+    /**
+     * Main function of the entire application.
+     * Starts the application by creating a Duke object, initializing it
+     * and then calling the {@link #run()} function of the created Duke Object.
+     *
+     * @param args Command Line Arguments, Not used in the application.
+     */
     public static void main(String[] args) {
         new Duke(FILE_PATH).run();
     }
