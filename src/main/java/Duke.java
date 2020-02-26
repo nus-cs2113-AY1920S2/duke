43429fa8e9563013c4cc4e18cc2b91a30bf43105
kezia <keziakew98@gmail.com>
2020-02-26 13:17:25 +0800

    remove comments

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,24 +1,11 @@
-import java.io.*;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 import exceptions.DukeException;
 import tasks.Task;
-import tasks.Deadline;
-import tasks.Event;
-import tasks.TaskType;
-
-// TODO: Extract existing error handling to exceptions.DukeException where appropriate
-// TODO: Assess if there is a better way to implement task adding i.e. with splitString() or otherwise
-// TODO: Check for further modularisation
-// TODO: A general add to arrayList method? reduce LOC
 
 public class Duke {
     private static ArrayList<Task> taskArrList = new ArrayList<>();
 
-    /** Main method Start **/
     public static void main(String[] args) throws DukeException {
         Scanner sc = new Scanner(System.in);
         boolean continueRun = true;
