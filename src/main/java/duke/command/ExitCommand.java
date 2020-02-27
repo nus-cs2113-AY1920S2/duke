51dc5412c0670b9be5cbd6292de681afffaac509
JWang2000 <JTWang2000@users.noreply.github.com>
2020-02-27 15:21:22 +0800

    Change command to be abstract class and modify the exitcommand.

--- src/main/java/duke/command/ExitCommand.java
+++ src/main/java/duke/command/ExitCommand.java
@@ -1,5 +1,12 @@
 package duke.command;
 
+import duke.common.DukeException;
+import duke.storage.Storage;
+import duke.taskList.TaskList;
+import duke.ui.Ui;
+
+import java.io.IOException;
+
 public class ExitCommand extends Command{
     public ExitCommand() {
 
@@ -8,5 +15,10 @@ public class ExitCommand extends Command{
     @Override
     public Boolean isExit() {
         return true;
+    }
+
+    @Override
+    public void execute (TaskList tasks, Ui ui, Storage storage) throws IOException, DukeException {
+        ui.showGoodByeMessage();
     }
 }
