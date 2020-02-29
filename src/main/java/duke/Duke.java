c11c116c28b61ec15de59a02a50c40b4a28cc5bf
dejunnn <djplanners@hotmail.com>
2020-02-29 15:30:54 +0800

    Minor edits to saving and created separate branch for Level 9

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,6 +7,10 @@ import duke.storage.Storage;
 import duke.tasks.TaskList;
 import duke.ui.UI;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Duke {
 
     public static void main(String[] args) {
@@ -16,6 +20,29 @@ public class Duke {
         Storage storage = new Storage();
 
         ui.displayWelcomeMessage();
+
+        try {
+            File dukeData = new File("data" + File.separator + "duke.txt");
+            if (dukeData.createNewFile()) {
+                System.out.println("File created: " + dukeData.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+/*
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+*/
 
         while (!ui.isExitStatus()) {
             try {
--- src/main/java/duke/storage/Storage.java
+++ src/main/java/duke/storage/Storage.java
@@ -1,5 +1,11 @@
 package duke.storage;
 
+import duke.tasks.Task;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Storage {
 
 }
