03ed5041c79bee5df12646068641b2d394458da6
thanhduc2000 <nthanhduc2000@gmail.com>
2020-03-02 02:00:31 +0800

    Updating JavaDoc #5

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -4,9 +4,17 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Deadline class extend from Task class to deal with Deadline
+ */
 public class Deadline extends Task {
     protected LocalDate time;
 
+    /**
+     * Deadline constructor with given description and date
+     * @param description
+     * @param time date that task needs to be done by
+     */
     public Deadline(String description, String time) {
         super(description);
         try {
@@ -16,21 +24,41 @@ public class Deadline extends Task {
         }
     }
 
+    /**
+     * Override of getTime method in Task
+     *
+     * @return time of the deadline
+     */
     @Override
     public LocalDate getTime() {
         return time;
     }
 
+    /**
+     * Override of the time's format method
+     *
+     * @return time format in human readable way
+     */
     @Override
     public String getTimeFormatted() {
         return this.time.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
     }
 
+    /**
+     * Override of getType method
+     *
+     * @return first letter of deadline "D"
+     */
     @Override
     public String getType() {
         return "D";
     }
 
+    /**
+     * Override of string method in Task
+     *
+     * @return format of a Deadline's task
+     */
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + this.getTimeFormatted() + ")";
--- src/main/java/duke/task/ToDo.java
+++ src/main/java/duke/task/ToDo.java
@@ -1,15 +1,34 @@
 package duke.task;
 
+/**
+ * Todo class extend from Task class to deal with Todo
+ */
 public class ToDo extends Task {
+    /**
+     * Todo constructor given a description from user
+     *
+     * @param description
+     */
     public ToDo(String description) {
         super(description);
     }
 
+    /**
+     * Overriding of getType in Task
+     * return first letter of Task
+     *
+     * @return "T"
+     */
     @Override
     public String getType() {
         return "T";
     }
 
+    /**
+     * Override of string method in Task
+     *
+     * @return format of the task in output
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
