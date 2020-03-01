5fd1a3aa2a35eb10bb7b011c853dff2b8a56fe9a
thanhduc2000 <nthanhduc2000@gmail.com>
2020-03-02 00:55:30 +0800

    Updating JavaDoc #4

--- src/main/java/duke/Storage.java
+++ src/main/java/duke/Storage.java
@@ -53,6 +53,13 @@ public class Storage {
         return tasks;
     }
 
+    /**
+     * Method to save the changes in the task lists after adjustment from
+     * users and create new file if it does not exist yet
+     *
+     * @param tasks the list of tasks that is passed after adjustmenr
+     * @throws IOException in case exception in file
+     */
     public void save(TaskList tasks) throws IOException{
         Path directoryPath = Paths.get("data");
         boolean isFileExists = Files.exists(directoryPath);
