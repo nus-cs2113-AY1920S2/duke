f171194466d74bcc9eaf74f821933eacfdc10a03
Riccardo Di Maio <35903974+rdimaio@users.noreply.github.com>
2020-03-05 01:35:41 +0800

    Adjust bracing style in for loop

--- src/main/java/duke/storage/StorageHandler.java
+++ src/main/java/duke/storage/StorageHandler.java
@@ -67,8 +67,7 @@ public class StorageHandler {
      */
     private static void updateIndexes(List<String> fileContent, int removedIndex, String dataFilePath) throws IOException {
         // Update indexes of subsequent tasks (e.g. if you remove task 2, task 3 becomes task 2)
-        for (int i = removedIndex; i < fileContent.size(); i++)
-        {
+        for (int i = removedIndex; i < fileContent.size(); i++) {
             // Get current comma separated string
             String currString = fileContent.get(i);
 
